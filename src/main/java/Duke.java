356e3886d2e74406885b9757ee32fea86d955372
Ethan <ethanhuang03@gmail.com>
2023-08-19 02:23:52 +0800

    Rename bot name, add greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printLines();
+        System.out.println("Hello! I'm Remy");
+        System.out.println("What can I do for you?");
+        printLines();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLines();
+    }
+    
+    static void printLines() {
+    	System.out.println("____________________________________________________________");
     }
 }
