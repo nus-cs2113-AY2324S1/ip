e2c4bb8e3b1c71f059e56f47bd34c0d840dbb23e
TeoHaoZhi <e0957449@u.nus.edu>
2023-08-18 21:54:51 +0800

    Add edited Duke.java with updated name,greetings and goodbye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        //Greetings & renaming
+        System.out.println("Hello! I'm JARVIS");
+        System.out.println("What can I do for you?");
+
+        //Print newline to seperate greetings and goodbye
+        System.out.println();
+
+        //Goodbye and exit
+        System.out.println("Bye. Hope to see you again soom!");
     }
 }
