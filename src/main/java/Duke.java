edf1ad49c4bdc956c0511d7b078ddcc204dd366a
Jason <jsong2506@gmail.com>
2023-08-19 14:23:50 +0800

    Add greeting for Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 public class Duke {
+    public static final String BOT_NAME = "JS";
+    public static final String LINE_DIVIDER = "----------------------------------------";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +8,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(LINE_DIVIDER);
+        System.out.println("Hello! I'm " + BOT_NAME);
+        System.out.println("What can I do for you?");
+        System.out.println(LINE_DIVIDER);
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
