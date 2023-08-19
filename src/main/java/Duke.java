fc07eac6cf69d2629f534fee4364a7a6bfdf4a7d
000verflow <ashokcoolboy5@gmail.com>
2023-08-19 14:18:42 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     static String chatbotName="Andrew Tate";
 
     public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm " + chatbotName);
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        while (true){
+            String commandGiven = myScanner.nextLine();
+            if (commandGiven.equals("bye")) {
+                break;
+            }else{
+                System.out.println("____________________________________________________________");
+                System.out.println(commandGiven);
+                System.out.println("____________________________________________________________");
+            }
+        }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
