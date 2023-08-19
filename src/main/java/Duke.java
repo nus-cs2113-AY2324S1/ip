d966d7ffa7d6a230b53fa05043c35bd76ee93c42
lctxct <caitinglien@hotmail.com>
2023-08-19 18:32:28 +0800

    Update chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String line = "____________________________________________________________";
+        String bob = "   ___    ___  ___ \n"
+                + "  / __\\  /___\\/ __\\\n"
+                + " /__\\// //  //__\\/\\\n"
+                + "/ \\/  \\/ \\_// \\/  \\\n"
+                + "\\_____/\\___/\\_____/\n";
+
+        System.out.println(line);
+        System.out.println("Hello! I'm\n" + bob);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+
     }
 }
