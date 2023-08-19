94a4a1978b4761c7b5149b7d0facf0e4d97d23dc
ICubE- <jehoyeon01@gmail.com>
2023-08-19 15:38:28 +0800

    Complete Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ___    _       _        \n"
+                + "|   \\  | |_   _| | _____ \n"
+                + "| |\\ \\ | | | | | |/ / _ \\\n"
+                + "| | \\ \\| | |_| |   <  __/\n"
+                + "|_|  \\___|\\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println();
+        System.out.println("Hello! I'm Nuke.");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
