e292cfa607715ad1fe92fc2611fa782aac62dc4b
NeoMinWei <>
2023-08-20 15:30:57 +0800

    Add greet and exit

--- src/main/java/Neo.java
+++ src/main/java/Neo.java
@@ -1,5 +1,8 @@
 public class Neo {
     public static void main(String[] args) {
-
+        System.out.println("Hello! I'm Neo.");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
