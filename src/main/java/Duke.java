e122d8b915fde7ae4b0f4476c6e91dc0e4a5dd68
AlWo223 <alexander.wolters@tum.de>
2023-08-20 13:39:43 +0800

    Rename Bot and implement greeting and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,18 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        
+        String name = "Lexi";
+        final String horizontal_lines = "--------------------------------------------";
+
+        System.out.println(logo);
+
+        System.out.println(horizontal_lines);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontal_lines);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontal_lines);
+
     }
 }
