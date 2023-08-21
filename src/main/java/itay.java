51a8a577a6b8acadf363cddff9758721c473230c
itayrefaely <refaelyitay@gmail.com>
2023-08-21 19:34:25 +0800

    greeting

---
+++ src/main/java/itay.java
@@ -0,0 +1,15 @@
+public class itay {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+
+        // greet
+        System.out.println("Hello! I'm Itay");
+        System.out.println("What can i do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
