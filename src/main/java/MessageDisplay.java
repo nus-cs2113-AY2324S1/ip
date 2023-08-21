decb2cd760fe3160bb3e0e093a6cbf4a16cfa61c
spinoandraptos <spinoandraptos@gmail.com>
2023-08-21 20:36:51 +0800

    First commit - week 2

---
+++ src/main/java/MessageDisplay.java
@@ -0,0 +1,16 @@
+public class MessageDisplay {
+    static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    public static void displayMessage(){
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Spino & Raptos");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
