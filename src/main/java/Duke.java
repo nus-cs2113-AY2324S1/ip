8fa65450bc6d3d8d860092fbbcba873ec7feccc0
choonsiang <choonsiang_ccs@hotmail.com>
2023-08-22 23:08:08 +0800

    Rename bot and greet user.
    Signed-off-by: choonsiang <choonsiang_ccs@hotmail.com>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        final String BOT_NAME= "Elgin";
+        final String SEPARATOR = "____________________________________________________________";
+
+        System.out.println(SEPARATOR);
+        System.out.println("Hello! I'm " + BOT_NAME);
+        System.out.println("What can I do for you?");
+        System.out.println(SEPARATOR);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(SEPARATOR);
+
+
     }
 }
