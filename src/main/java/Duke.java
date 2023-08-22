ed3efe1f97da730965a4659f0349a7ec949328d4
aaronxujiachen <1943999635x@gmail.com>
2023-08-22 18:27:33 +0800

    Add Dude Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+
+    public static void drawLine() {  // Draw horizontal lines
+        for (int i = 0; i < 30; i++) {
+            System.out.print("_");
+        }
+        System.out.println();
+    }
+
+    public static void hiDude() {  // Print logo and hello message
+        String logo = "###            #        \n"
+                + "#  #           #        \n"
+                + "#  #  #  #   ###   ##   \n"
+                + "#  #  #  #  #  #  # ##  \n"
+                + "#  #  #  #  #  #  ##    \n"
+                + "###    ###   ###   ##   \n";
         System.out.println("Hello from\n" + logo);
+        drawLine();
+        System.out.println("Hello! I'm your best Dude");
+        System.out.println("What can I do for you?");
+        drawLine();
+    }
+
+    public static void byeDude() {  // Print goodbye message
+        System.out.println("Bye. Hope to see you again soon!");
+        drawLine();
+    }
+
+    public static void main(String[] args) {
+        hiDude();
+        byeDude();
     }
 }
