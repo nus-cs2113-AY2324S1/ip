9482154c4c7522220f54503a901acf24ca09cf46
TongZhengHong <zhtong315@gmail.com>
2023-08-22 19:29:58 +0800

    Update welcome sentence

--- src/main/java/Dawson.java
+++ src/main/java/Dawson.java
@@ -1,10 +1,12 @@
 public class Dawson {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcomeText = "____________________________________________________________\n" + 
+                " Hello! I'm Dawson!\n" + 
+                " What can I do for you?\n" + 
+                "____________________________________________________________\n" + 
+                " Bye. Hope to see you again soon!\n" + 
+                "____________________________________________________________";
+
+        System.out.println(welcomeText);
     }
 }
