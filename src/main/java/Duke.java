4a14bc13a1124556836973094bb0c6851a582b4a
lctxct <caitinglien@hotmail.com>
2023-08-22 18:39:58 +0800

    Add echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static final String LINE = "\t____________________________________________________________";
+
+    public static void printWelcome() {
+
+        String bob = "\t   ___    ___  ___ \n"
+                + "\t  / __\\  /___\\/ __\\\n"
+                + "\t /__\\// //  //__\\/\\\n"
+                + "\t/ \\/  \\/ \\_// \\/  \\\n"
+                + "\t\\_____/\\___/\\_____/\n";
+
+        System.out.println(LINE);
+        System.out.println("\tHello! I'm\n" + bob);
+        System.out.println("\tWhat can I do for you?");
+        System.out.println(LINE);
+    }
+
+    public static void printFarewell() {
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
 
-        String line = "____________________________________________________________";
-        String bob = "   ___    ___  ___ \n"
-                + "  / __\\  /___\\/ __\\\n"
-                + " /__\\// //  //__\\/\\\n"
-                + "/ \\/  \\/ \\_// \\/  \\\n"
-                + "\\_____/\\___/\\_____/\n";
+        printWelcome();
 
-        System.out.println(line);
-        System.out.println("Hello! I'm\n" + bob);
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        line = in.nextLine().trim();
+        while (line.equals("bye") == false) {
+            System.out.println(LINE);
+            System.out.println("\t" + line);
+            System.out.println(LINE);
 
+            line = in.nextLine().trim();
+        }
+
+        printFarewell();
     }
 }
