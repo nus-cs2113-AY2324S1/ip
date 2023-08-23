d169c94d5b4d79e764826f87b019315e17b1ea5b
woodenclock <luckyisland3710@gmail.com>
2023-08-23 17:21:13 +0800

    Update Duke.java for Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
+    public static void printHorizontalLines() {
+        for (int i = 0; i < 60; i++) {
+            System.out.print("_");
+        }
+        System.out.print("\n");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        printHorizontalLines();
+        System.out.println("Hello! I'm ELVIS\nWhat can I do for you?");
+        printHorizontalLines();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLines();
     }
 }
