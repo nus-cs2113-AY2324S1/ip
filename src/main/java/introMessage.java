d45747e832a71e41a36dbb273d443ae430499a69
StevenGX12 <guanxiao2000@gmail.com>
2023-08-23 12:22:13 +0800

    no message

---
+++ src/main/java/introMessage.java
@@ -0,0 +1,9 @@
+public class introMessage {
+    public static void displayIntroMessage() {
+        String line = "____________________________________________________________\n";
+        System.out.println(line 
+        + "Hello! I'm notChatGPT :)\n" 
+        + "What can I do for you?\n"
+        + line + "Bye. Hope to see you soon!\n" + line);
+    }
+}
---
+++ src/main/java/notChatGPT.java
@@ -0,0 +1,5 @@
+public class notChatGPT {
+    public static void main(String[] args) {
+        introMessage.displayIntroMessage();
+    }
+}
