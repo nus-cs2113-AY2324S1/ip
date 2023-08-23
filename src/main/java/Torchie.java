95875a61c6e6eef0dfee599459ed9d6a0fdb744d
kai jie <leowkaijie@gmail.com>
2023-08-23 22:43:53 +0800

    Update main to do rename, greet, exit

---
+++ src/main/java/Torchie.java
@@ -0,0 +1,7 @@
+public class Torchie {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Torchie!");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
