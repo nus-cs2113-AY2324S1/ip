a4d6de2ea8e8b9586b88e24651faf7ddd888e583
vvhuiling <huanghuiling2022@outlook.com>
2023-08-24 14:03:57 +0800

    Add greeting and exit

---
+++ src/main/java/Sun.java
@@ -0,0 +1,19 @@
+public class Sun {
+    public static void main(String[] args) {
+        String logo = " ____               \n"
+                    + "| ___| _   _  ______ \n"
+                    + "| \\__ | | | || /--\\ |\n"
+                    + " \\___|| |_| || |  | |\n"
+                    + "/____/ \\__,_||_|  |_|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Sun!");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        //
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
