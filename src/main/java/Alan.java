e3f01fab05b620aae891073d626a8ffae648f870
Dexter Hoon <e0727166@u.nus.edu>
2023-08-24 23:16:00 +0800

    Add greeting and exit functions

--- src/main/java/Alan.java
+++ src/main/java/Alan.java
@@ -1,10 +1,27 @@
 public class Alan {
+    public static void printGreet() {
+        String man = " @/\n" +
+                     "/| \n" +
+                     "/ \\";
+
+        System.out.println(man);
+        System.out.println("Hello! I'm Alan");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void printExit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printHorizontalLine() {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printHorizontalLine();
+        printGreet();
+        printHorizontalLine();
+        printExit();
+        printHorizontalLine();
     }
 }
