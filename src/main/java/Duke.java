c4febfab81d799c1206062c6a7f3497617144352
DavinciDelta <110587475+DavinciDelta@users.noreply.github.com>
2023-08-25 13:56:25 -0400

    Add Increments: Level-1
    Allow chatbot to echo user input, provide characteristics to chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello I'm Rias!");
-        System.out.println("What can I do for you?");
+        System.out.println("Hello I'm Rias-chan!");
+        System.out.println("Welcome back goshujin-sama, what can I do for you?");
+        waitForResponse();
         System.out.println("Bye. Hope to see you again soon!");
+    }
+    public static void waitForResponse() {
+        Scanner scanner = new Scanner(System.in);
+        String response = "";
+        do {
+            response = scanner.nextLine();
+            if (!"bye".equalsIgnoreCase(response)) {
+                process(response);
+            }
+        } while (!"bye".equalsIgnoreCase(response));
+
+        scanner.close();
+    }
+    public static void process(String response) {
+        System.out.println(response);
     }
 }
