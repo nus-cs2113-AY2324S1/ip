b8a222a35b0dffe797b5fd7266c484e317159bcc
TongZhengHong <zhtong315@gmail.com>
2023-08-25 14:27:56 +0800

    Update Dawson welcome text with new text art

--- src/main/java/dawson/Dawson.java
+++ src/main/java/dawson/Dawson.java
@@ -1,14 +1,32 @@
 package dawson;
 
 public class Dawson {
-    public static void main(String[] args) {
-        String welcomeText = "____________________________________________________________\n" + 
-                " Hello! I'm Dawson!\n" + 
-                " What can I do for you?\n" + 
-                "____________________________________________________________\n" + 
-                " Bye. Hope to see you again soon!\n" + 
-                "____________________________________________________________";
+    /**
+     * Print given input text together with a line as separator
+     * 
+     * @param text
+     */
+    public static void printText(String text) {
+        System.out.println(text);
+        System.out.println("____________________________________________________________\n");
+    }
 
-        System.out.println(welcomeText);
+    public static void main(String[] args) {
+
+        String dawsonText = 
+            "██████╗░░█████╗░░██╗░░░░░░░██╗░██████╗░█████╗░███╗░░██╗\n" +
+            "██╔══██╗██╔══██╗░██║░░██╗░░██║██╔════╝██╔══██╗████╗░██║\n" +
+            "██║░░██║███████║░╚██╗████╗██╔╝╚█████╗░██║░░██║██╔██╗██║\n" +
+            "██║░░██║██╔══██║░░████╔═████║░░╚═══██╗██║░░██║██║╚████║\n" +
+            "██████╔╝██║░░██║░░╚██╔╝░╚██╔╝░██████╔╝╚█████╔╝██║░╚███║\n" +
+            "╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═════╝░░╚════╝░╚═╝░░╚══╝";
+
+        String welcomeText = " What can I do for you?";
+        String exitString = " Bye. Hope to see you again soon!";
+
+        printText("");
+        printText("Hello! My name is \n" + dawsonText);
+        printText(welcomeText);
+        printText(exitString);
     }
 }
