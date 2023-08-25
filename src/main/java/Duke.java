b199946619a54556c9446e6ec72a7c899ab82bcb
lisizhuang-0121 <sizhuangli6@gmail.com>
2023-08-26 00:27:45 +0800

    Add Echo Function in Chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void echo(String line) {
+        Scanner textIn = new Scanner(System.in);
+        while (true) {
+            String command = textIn.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println(line);
+            System.out.println(command);
+            System.out.println(line);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,6 +24,8 @@ public class Duke {
         String line = "____________________________________________________________";
         System.out.println(line);
         System.out.println("Hello! I'm soccat!\nWhat can I do for you?");
+        System.out.println(line);
+        echo(line);
         System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
