bb958c9d2087092175be1433a884ca5a9f4293b6
J0shuaLeong <joshualeong2607@gmail.com>
2023-08-25 21:53:14 +0800

    Add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,35 +1,52 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
-    public static void main(String[] args) {
-        int length = 10;
-
-        // Print a horizontal line of hyphens
+    public static void printHorizontalLines(int length) {
         for (int i = 0; i < length; i++) {
             System.out.print("-");
         }
         System.out.println();
+    }
+    public static void printList(List<String> list, int length){
+        if (list.isEmpty()) {
+            return;
+        } else {
+            printHorizontalLines(length);
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println((i + 1) + ". " + list.get(i));
+            }
+            printHorizontalLines(length);
+        }
+    }
+    public static void main(String[] args) {
+        int length = 30;
+        //String[] textList = new String[100];
+        List<String> textList = new ArrayList<>();
+
+        printHorizontalLines(length);
         System.out.println("Hi! I'm Joshua");
         System.out.println("What can I do for you?");
-        for (int i = 0; i < length; i++) {
-            System.out.print("-");
-        }
-        System.out.println();
+        printHorizontalLines(length);
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            String input = scanner.nextLine().trim().toLowerCase();
-
-            if (input.equals("bye")) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                printHorizontalLines(length);
                 System.out.println("Bye! See you soon.");
-                break; // Exit the loop and terminate the program
+                printHorizontalLines(length);
+                scanner.close();
+                break;
+            } else if (input.equalsIgnoreCase("list")) {
+                printList(textList, length);
+            } else {
+                textList.add(input);
+                printHorizontalLines(length);
+                System.out.println("added: " + input);
+                printHorizontalLines(length);
             }
         }
-        scanner.close();
-
-        for (int i = 0; i < length; i++) {
-            System.out.print("-");
-        }
-        System.out.println();
     }
 }
