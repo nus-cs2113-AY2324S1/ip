a9ff8ed9a1e9bf696baa1193795bd6329d9e6546
TeoHaoZhi <e0957449@u.nus.edu>
2023-08-26 22:44:07 +0800

    Remove line seperator

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,10 +4,7 @@ public class Duke {
         System.out.println("Hello! I'm JARVIS");
         System.out.println("What can I do for you?");
 
-        //Print newline to seperate greetings and goodbye
-        System.out.println();
-
-        //Goodbye and exit
-        System.out.println("Bye. Hope to see you again soom!");
+        Echo respond = new Echo();
+        respond.echoBack();
     }
 }
