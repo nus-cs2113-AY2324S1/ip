693da2e3f2e75a7b52e39e10492dc21ea53e033f
NeoMinWei <>
2023-08-27 00:26:32 +0800

    Add echo function

--- src/main/java/Neo.java
+++ src/main/java/Neo.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 public class Neo {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Neo.");
         System.out.println("What can I do for you?");
-        System.out.println();
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(line);
+            line = in.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
