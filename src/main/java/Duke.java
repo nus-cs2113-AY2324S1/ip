a14a7288cd1909c6df573d2fbe16374d32b701aa
Bryan Lee <e0725090@u.nus.edu>
2023-08-26 16:14:00 +0800

    Update Duke.java
    renamed chatbot from duke to Dom, added the various lines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "  ____                  \n"
+                + " |  _ \\  ___  _ __ ___  \n"
+                + " | | | |/ _ \\| '_ ` _ \\ \n"
+                + " | |_| | (_) | | | | | |\n"
+                + " |____/ \\___/|_| |_| |_|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Dom");
+        System.out.println("What can I do for you?");
+
+        System.out.println(("Bye. Hope to see you again soon"));
     }
 }
