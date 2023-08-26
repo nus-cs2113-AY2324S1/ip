6b66b4618a8fa5a370707eece07bda66d7af3704
wwweert123 <puachengxuan.fp@gmail.com>
2023-08-26 11:09:20 +0800

    Update Fredbot functionality and skeleton
    echos commands entered by the user and exits when the user types the command "bye"

--- src/main/java/Fredbot.java
+++ src/main/java/Fredbot.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Fredbot {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,13 +8,24 @@ public class Fredbot {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         // System.out.println("Hello from\n" + logo);
-        String greeting = "____________________________________________________________\n" +
-                " Hello! I'm Fredbot\n" +
-                " What can I do for you?";
-        String farewell = "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+        String greeting = "    ____________________________________________________________\n" +
+                "     Hello! I'm Fredbot\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________\n";
+        String farewell = "    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________";
         System.out.println(greeting);
+        String line;
+        String divider = "    ____________________________________________________________";
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(divider);
+            System.out.println("    " + line);
+            System.out.println(divider + '\n');
+            line = in.nextLine();
+        }
         System.out.println(farewell);
     }
 }
