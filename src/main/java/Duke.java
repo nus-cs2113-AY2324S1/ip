24f7d0dc0b5639f6221c465d5951338660f378ae
Jason <jsong2506@gmail.com>
2023-08-26 20:53:39 +0800

    Add list and add feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Duke {
     public static final String BOT_NAME = "JS";
     public static final String LINE_DIVIDER = "----------------------------------------";
     public static void main(String[] args) {
+        ArrayList<String> toDoList = new ArrayList<String>();
+        Iterator<String> toDoListIter = toDoList.iterator();
         System.out.println(LINE_DIVIDER);
         System.out.println("Hello! I'm " + BOT_NAME);
         System.out.println("What can I do for you?");
@@ -12,7 +16,14 @@ public class Duke {
         String userInput = input.nextLine();
         while(!(userInput.equals("bye"))) {
             System.out.println(LINE_DIVIDER);
-            System.out.println(userInput);
+            if(userInput.equals("list")) {
+                toDoListIter = toDoList.iterator();
+                for(int i = 1; toDoListIter.hasNext() ; i++) {
+                    System.out.println(i + ". " + toDoListIter.next());
+                }
+            }
+            System.out.println("added: " + userInput);
+            toDoList.add(userInput);
             System.out.println(LINE_DIVIDER);
             userInput = input.nextLine();
         }
