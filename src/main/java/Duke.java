158e5ab902d3a44a45f7c0daac9cdd1959c0b3a9
Jason <jsong2506@gmail.com>
2023-08-26 20:37:17 +0800

    Add echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static final String BOT_NAME = "JS";
     public static final String LINE_DIVIDER = "----------------------------------------";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
         System.out.println(LINE_DIVIDER);
         System.out.println("Hello! I'm " + BOT_NAME);
         System.out.println("What can I do for you?");
         System.out.println(LINE_DIVIDER);
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+        while(!(userInput.equals("bye"))) {
+            System.out.println(LINE_DIVIDER);
+            System.out.println(userInput);
+            System.out.println(LINE_DIVIDER);
+            userInput = input.nextLine();
+        }
+        System.out.println(LINE_DIVIDER);
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE_DIVIDER);
     }
 }
