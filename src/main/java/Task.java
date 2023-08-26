95bd1c2fe04c6c9b720472df6e8cbed46eaf15d1
wwweert123 <puachengxuan.fp@gmail.com>
2023-08-26 12:20:37 +0800

    Add a Task class
    While it is possible to represent a task list as a multi-dimensional array containing primitive values, the more natural approach is to use a Task class to represent tasks.
    
    The task class contains taskdescription and done instance level members and numTask class level member

---
+++ src/main/java/Task.java
@@ -0,0 +1,35 @@
+public class Task {
+    private String taskDesc;
+    private boolean done;
+
+    private static int numTask;
+
+    public Task(String taskDesc) {
+        this.taskDesc = taskDesc;
+        this.done = false;
+    }
+
+    public String getTaskDesc() {
+        return taskDesc;
+    }
+
+    public void setTaskDesc(String taskDesc) {
+        this.taskDesc = taskDesc;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public static int getNumTask() {
+        return numTask;
+    }
+
+    public static void setNumTask(int numTask) {
+        Task.numTask = numTask;
+    }
+}
