b1dda71992a708596a06656a46ad4309f2172f9c
WooKaiNing <e0968128@u.nus.edu>
2023-08-27 01:37:11 +0800

    Add Echo to LinguoBot, Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,24 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello I'm LinguoBot");
         System.out.println("What can I do for you?");
-        System.out.println("-------------------------");
-        System.out.println("Bye. Hope to see you again soon.");
-        System.out.println("-------------------------");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            line = in.nextLine();
+            System.out.println("-------------------------");
+            System.out.println(line);
+            System.out.println("-------------------------");
+
+            if (line.equals("bye")) {
+                System.out.println("-------------------------");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("-------------------------");
+                break;
+            }
+        }
     }
-//    public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-//    }
 }
