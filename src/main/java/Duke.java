4b7e598968f722d3e8fd4c8cd1765321cad8c386
choonsiang <choonsiang_ccs@hotmail.com>
2023-08-27 19:11:38 +0800

    Echo user command and exit when user input bye.
    Signed-off-by: choonsiang <choonsiang_ccs@hotmail.com>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,52 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    final static String BOT_NAME= "Elgin";
+    final static String SEPARATOR = "____________________________________________________________";
+
+    public static void formatPrint(String[] lines){
+        System.out.println("\t"+SEPARATOR);
+        for(String line:lines){
+            System.out.println("\t"+line);
+        }
+        System.out.println("\t"+SEPARATOR);
+    }
+
+    public static void formatPrint(String line){
+        System.out.println("\t"+SEPARATOR);
+        System.out.println("\t"+line);
+        System.out.println("\t"+SEPARATOR);
+    }
+    public static void sayGreeting(){
+        formatPrint(new String[]{"Hello! I'm " + BOT_NAME, "What can I do for you?"});
+    }
+
+    public static void sayBye(){
+        formatPrint("Bye. Hope to see you again soon!");
+    }
+
+    public static boolean handleCommand(String command){
+        switch(command.toLowerCase()){
+        case "bye":
+            sayBye();
+            return false;
+        default:
+            formatPrint(command);
+        }
+        return true;
+    }
+
+
     public static void main(String[] args) {
-        final String BOT_NAME= "Elgin";
-        final String SEPARATOR = "____________________________________________________________";
 
-        System.out.println(SEPARATOR);
-        System.out.println("Hello! I'm " + BOT_NAME);
-        System.out.println("What can I do for you?");
-        System.out.println(SEPARATOR);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(SEPARATOR);
+        Scanner scanner = new Scanner(System.in);
+        sayGreeting();
+        String command;
 
+        do {
+            command= scanner.nextLine();
+        } while(handleCommand(command));
 
     }
 }
