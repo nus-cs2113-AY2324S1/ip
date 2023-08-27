8f50b9222a58859c9f13d616d7c7ba24b024bf56
Wendelin Wemhöner <wendelin.wemhoener@gmail.com>
2023-08-27 22:07:37 +0800

    Implement Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        printIntroduction();
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+
+            printHorizontalLine();
+            System.out.println("    " + input);
+            printHorizontalLine();
+        }
+
+        printFarewell();
+    }
+
+    public static void printIntroduction() {
         printHorizontalLine();
-        System.out.println("Hello! I'm Careo");
-        System.out.println("What can I do for you?");
+        System.out.println("    Hello! I'm Careo");
+        System.out.println("    What can I do for you?");
         printHorizontalLine();
-        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printFarewell() {
+        printHorizontalLine();
+        System.out.println("    Bye. Hope to see you again soon!");
         printHorizontalLine();
     }
 
     public static void printHorizontalLine() {
-        System.out.println("-".repeat(80));
+        System.out.println("    " + "-".repeat(76));
     }
 }
