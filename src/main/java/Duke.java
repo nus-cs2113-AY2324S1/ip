0726153cf5fbb5b03f2fd33c2443134328ec08dc
WooKaiNing <e0968128@u.nus.edu>
2023-08-27 10:24:25 +0800

    Add List to LinguoBot, Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,17 +7,33 @@ public class Duke {
         String line;
         Scanner in = new Scanner(System.in);
 
+        String[] list;
+        list = new String[100];
+        int itemCount = 0;
+
         while (true) {
             line = in.nextLine();
-            System.out.println("-------------------------");
-            System.out.println(line);
-            System.out.println("-------------------------");
+            if (line.equals("list")) {
+                System.out.println("-------------------------");
+                for (int i = 0 ; i<itemCount; i++) {
+                    System.out.println(i + 1 + "." + list[i]);
+                }
+                System.out.println("-------------------------");
+            }
 
-            if (line.equals("bye")) {
+            else if (line.equals("bye")) {
                 System.out.println("-------------------------");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("-------------------------");
                 break;
+            }
+
+            else {
+                System.out.println("-------------------------");
+                System.out.println("added: " + line);
+                System.out.println("-------------------------");
+                list[itemCount] = line;
+                itemCount ++;
             }
         }
     }
