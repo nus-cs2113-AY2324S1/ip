6c5ea819482bfd5c0d527f776bf30c3392321f9c
TeoHaoZhi <e0957449@u.nus.edu>
2023-08-27 11:46:26 +0800

    Create new class for task addition

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    public  String toBeDone;
+    private boolean completed;
+    public String[] markAsDone;
+
+
+    public Task(String newTask) {
+        toBeDone = newTask;
+        completed = false;
+        markAsDone = new String[1];
+        markAsDone[0] = " ";
+    }
+    public void setDone(){
+        completed = true;
+        markAsDone[0] ="X";
+    }
+
+
+    public void setNotDone(){
+        completed = false;
+        markAsDone[0] =" ";
+    }
+}
