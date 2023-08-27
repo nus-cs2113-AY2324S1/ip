cb300a566c5260ce47a2df925e383f92be903a16
Ethan <ethanhuang03@gmail.com>
2023-08-27 15:19:56 +0800

    Update .gitignore Add Echo to bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        printLines();
-        System.out.println("Hello! I'm Remy");
-        System.out.println("What can I do for you?");
-        printLines();
-        System.out.println("Bye. Hope to see you again soon!");
-        printLines();
+    	greetUser();
+        Scanner input = new Scanner(System.in);
+        String command = input.nextLine();
+        
+        while (!command.equals("bye")) {
+        	respond(command);
+        	command = input.nextLine();
+        }
+        sayGoodbye();
     }
     
-    static void printLines() {
-    	System.out.println("____________________________________________________________");
+    public static void greetUser() {
+    	 printLines();
+         System.out.println("    Hello! I'm Remy");
+         System.out.println("    What can I do for you?");
+         printLines();
+    }
+    
+    public static void printLines() {
+    	System.out.println("    ____________________________________________________________");
+    }
+    
+    public static void respond(String response) {
+    	printLines();
+    	System.out.println("    " + response);
+    	printLines();
+    	System.out.println();
+    }
+    
+    public static void sayGoodbye() {
+    	printLines();
+        System.out.println("    Bye. Hope to see you again soon!");
+        printLines();
     }
 }
