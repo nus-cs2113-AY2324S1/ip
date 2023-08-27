da9144b7b99a8ce0e0ea327f06fb9478e908202a
nihalzp <81457724+nihalzp@users.noreply.github.com>
2023-08-27 18:57:54 +0800

    Rename bot and implement basic greet, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Arrays;
+
 public class Duke {
+    final private static String BOT_NAME = "Python";
+    final private static int HORIZONTAL_LINE_LENGTH = 80;
+
+    private static void printHorizontalLine() {
+        char[] horizontalLine = new char[HORIZONTAL_LINE_LENGTH];
+        Arrays.fill(horizontalLine, '—');
+        System.out.println(new String(horizontalLine));
+    }
+    
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printHorizontalLine();
+        System.out.printf("Hello! I am a short Java Bot %s!\n", BOT_NAME);
+        System.out.println("What can I do for you?");
+        printHorizontalLine();
+        System.out.println("Bye. See you again when you run the program again!");
+        printHorizontalLine();
     }
 }
