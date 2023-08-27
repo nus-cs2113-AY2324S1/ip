8bc161abe29f3e61808e731985644dfe66efec76
yingx9 <lyingx99@gmail.com>
2023-08-27 14:45:53 +0800

    Add Level-1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void echoInput(){
+
+
+        String input = "";
+        Scanner in = new Scanner(System.in);
+
+        input = in.nextLine();
+
+        while(!(input.equals("bye"))){
+
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________\n");
+
+            input = in.nextLine();
+        }
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________\n");
+
+
+
+    }
+
+
+
     public static void main(String[] args) {
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Magpie");
         System.out.println("What can I do for you?\n");
-        System.out.println("____________________________________________________________");
-
-        System.out.println("Bye. Hope to see you again soon!\n");
         System.out.println("____________________________________________________________\n");
 
+
+        echoInput();
+
     }
-    
+
 }
