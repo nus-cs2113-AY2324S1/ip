2c369866fb656c5459f52871ac9446780730675e
TeoHaoZhi <e0957449@u.nus.edu>
2023-08-27 08:14:32 +0800

    Update Echo class to use toDoList class and methods

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,26 +1,38 @@
 import java.util.Scanner;
 
+
 public class Echo {
     private String bye;
     private String text;
 
 
+
 public Echo() {
     bye ="bye";
+
 }
 
+toDoList newList  = new toDoList();
+
 public void echoBack(){
+
     Scanner in = new Scanner(System.in);
     text = in.nextLine();
     if(text.equals(bye)){
         System.out.println("Bye. Hope to see you again soom!");
     }
+    else if(text.equals(newList.checker)){
+        newList.listTask();
+        echoBack();
+    }
     else {
-        System.out.println(text);
+       newList. addTask(text);
         echoBack();
 
     }
 }
+
+
 
 
 }
