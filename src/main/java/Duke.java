dce66a8bcab1a3d791f1f56d2427a62c9ee33067
Stanley Wijaya <wijayastanley00@gmail.com>
2023-08-28 16:09:55 +0800

    Add horizontal lines and an exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
         final String logo = "MudMud";
+
+        System.out.println("____________________________________________________________");
         System.out.println("Oh hello! I'm " + logo + ".");
         System.out.println("How can I help you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Goodbye! I am going to sleep now.");
+        System.out.println("____________________________________________________________");
     }
 }
