791969c60bcf5a182cdd0d1bf090cf8d99960fe9
Xuan127 <shanyuxuan2001@gmail.com>
2023-08-29 02:54:26 +0800

    Add echo function

--- src/main/java/Zen.java
+++ src/main/java/Zen.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Zen {
     public static void main(String[] args) {
-        System.out.println("\n        ,----,                   ,--.                 ,---._                             ,/   .`|                     ");
+        System.out.println(System.lineSeparator() + "        ,----,                   ,--.                 ,---._                             ,/   .`|                     ");
         System.out.println("      .'   .`|    ,---,.       ,--.'|               .-- -.' \\     ,---,.  .--.--.      ,`   .'  :   ,---,.,-.----.    ");
         System.out.println("   .'   .'   ;  ,'  .' |   ,--,:  : |               |    |   :  ,'  .' | /  /    '.  ;    ;     / ,'  .' |\\    /  \\   ");
         System.out.println(" ,---, '    .',---.'   |,`--.'`|  ' :               :    ;   |,---.'   ||  :  /`. /.'___,/    ,',---.'   |;   :    \\  ");
@@ -13,12 +15,25 @@ public class Zen {
         System.out.println("./__;       : |   |    \\|   | '`--'          /  ../  `..-    ,|   |    \\'--'.     /    ;   |.'  |   |    \\:   ' | \\.' ");
         System.out.println("|   :     .'  |   :   .''   : |              \\    \\         ; |   :   .'  `--'---'     '---'    |   :   .':   : :-'   ");
         System.out.println(";   |  .'     |   | ,'  ;   |.'               \\    \\      ,'  |   | ,'                          |   | ,'  |   |.'     ");
-        System.out.println("`---'         `----'    '---'                  ---....--'    `----'                            `----'    `---'       \n");
-        System.out.println("____________________________________________________________\r\n" + //
-                            " Greetings, dear traveler! I am ZEN JESTER\r\n" + //
-                            " How may I bring mirth to your day?\r\n" + //
-                            "____________________________________________________________\r\n" + //
-                            " Farewell, my friend! Until our laughter intertwines again\r\n" + //
-                            "____________________________________________________________");
+        System.out.println("`---'         `----'    '---'                  ---....--'    `----'                            `----'    `---'       ");
+        System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
+        System.out.println("    Greetings, dear traveler! I am ZEN JESTER");
+        System.out.println("    How may I bring mirth to your day?");
+        System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
+
+        Scanner input = new Scanner(System.in);
+        String line = "";
+        do {
+            line = input.nextLine();
+            if (line.equals("bye")) {
+                System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
+                System.out.println("    Farewell, my friend! Until our laughter intertwines again");
+                System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
+                break;
+            }
+            System.out.println(System.lineSeparator() +"    -------------------------------------------------------------");
+            System.out.println("    " + line);
+            System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
+        } while (!line.equals("bye"));
     }
 }
