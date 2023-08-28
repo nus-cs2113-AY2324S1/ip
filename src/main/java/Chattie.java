9b28a256e7d0b7fbdaa42437b2c706bb0e08e865
ShaniceTang <e0959739@u.nus.edu>
2023-08-28 09:09:11 +0800

    Update Chattie.java

--- src/main/java/Chattie.java
+++ src/main/java/Chattie.java
@@ -2,7 +2,7 @@ public class Chattie {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Chattie!");
-        System.out.println("What can I do for you?\n");
+        System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
