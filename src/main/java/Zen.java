7bbcc366bbff4de6d61afacbad537bd9319d024f
Xuan127 <shanyuxuan2001@gmail.com>
2023-08-29 03:07:10 +0800

    Implement mark and unmark

--- src/main/java/Zen.java
+++ src/main/java/Zen.java
@@ -24,6 +24,7 @@ public class Zen {
         Scanner input = new Scanner(System.in);
         String line = "";
         String[] tasks = new String[100];
+        int[] taskStatus = new int[100];
         int taskCount = 0;
         do {
             line = input.nextLine();
@@ -32,17 +33,42 @@ public class Zen {
                 System.out.println("    Farewell, my friend! Until our laughter intertwines again");
                 System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
                 break;
-            } else if (line.equals("list")) { // list tasks
+            }
+            else if (line.equals("list")) { // list tasks
                 System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
                 System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < tasks.length; i++) {
                     if (tasks[i] != null) {
-                        System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                        if (taskStatus[i] == 1) {
+                            System.out.println("    " + (i + 1) + ". [X] " + tasks[i]);
+                        }
+                        else {
+                            System.out.println("    " + (i + 1) + ". [ ] " + tasks[i]);
+                        }
                     }
                 }
                 System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
                 continue;
-            } else { // add task
+            }
+            else if (line.contains("unmark")) { // mark task as done
+                int taskNumber = Integer.parseInt(line.substring(7));
+                taskStatus[taskNumber - 1] = 0;
+                System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
+                System.out.println("    Nice! I've marked this task as undone:");
+                System.out.println("      [ ] " + tasks[taskNumber - 1]);
+                System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
+                continue;
+            }
+            else if (line.contains("mark")) { // mark task as done
+                int taskNumber = Integer.parseInt(line.substring(5));
+                taskStatus[taskNumber - 1] = 1;
+                System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("      [X] " + tasks[taskNumber - 1]);
+                System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
+                continue;
+            }
+            else { // add task
                 System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
                 System.out.println("    added: " + line);
                 System.out.println(System.lineSeparator() + "   -------------------------------------------------------------");
