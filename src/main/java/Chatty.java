cfeef9e584e3521f6524793e5604cdb49ab4f7de
bnjm2000 <bnjm2000@gmail.com>
2023-08-28 21:28:27 +0800

    Add Task.java class Add ability to mark items done

--- src/main/java/Chatty.java
+++ src/main/java/Chatty.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Chatty {
     /*public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,26 +9,46 @@ public class Chatty {
                 + "|____/ \\__,_|_|\\_\\___|\n";
     }
      */
+
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Chatty!\nWhat can I do for you?");
         System.out.println("____________________________________________________________\n");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         while (!input.equalsIgnoreCase("bye")){
             if (input.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + ". [" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
+                System.out.println("____________________________________________________________\n");
+                input = scanner.nextLine();
+                continue;
+            } else if (input.startsWith("mark")) {
+                int idx=Integer.parseInt(input.substring(5)) - 1;
+                tasks[idx].mark();
                 System.out.println("____________________________________________________________");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + tasks[idx].getDescription());
+                System.out.println("____________________________________________________________\n");
+                input = scanner.nextLine();
+                continue;
+            } else if (input.startsWith("unmark")) {
+                int idx=Integer.parseInt(input.substring(7)) - 1;
+                tasks[idx].unmark();
+                System.out.println("____________________________________________________________");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[ ] " + tasks[idx].getDescription());
+                System.out.println("____________________________________________________________\n");
                 input = scanner.nextLine();
                 continue;
             }
             System.out.println("____________________________________________________________");
-            tasks[taskCount]=input;
+            tasks[taskCount] = new Task(input);
             taskCount++;
             System.out.println("added: "+input);
             System.out.println("____________________________________________________________\n");
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark(){
+        this.isDone=true;
+    }
+
+    public void unmark(){
+        this.isDone=false;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+}
