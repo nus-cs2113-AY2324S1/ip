47f94ae0458e6073a651567353f5b0f1a08176cf
ryan1604 <ryanchua1604@gmail.com>
2023-08-28 22:59:55 +0800

    Add ability to store text and list them back

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,30 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static void printList(String[] list, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println("\t" + (i + 1) + ": " + list[i]);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("\tHello! I'm RC\n\tWhat can I do for you?\n");
         Scanner in = new Scanner(System.in);
         String line = "";
+        String[] list = new String[100];
+        int count = 0;
+
         while (true) {
             line = in.nextLine();
             if (line.equals("bye")) {
                 break;
+            } else if (line.equals("list")) {
+                printList(list, count);
+            } else {
+                System.out.println("\tadded: " + line);
+                list[count] = line;
+                count++;
             }
-            System.out.println("\t" + line);
         }
         System.out.println("\tBye. Hope to see you again soon!\n");
     }
