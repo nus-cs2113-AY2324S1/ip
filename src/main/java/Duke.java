2424a97121d2d51f27c5ad32f0a803a8d5592f2a
Stanley Wijaya <wijayastanley00@gmail.com>
2023-08-28 16:07:35 +0800

    Add new line to the greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,5 +2,6 @@ public class Duke {
     public static void main(String[] args) {
         final String logo = "MudMud";
         System.out.println("Oh hello! I'm " + logo + ".");
+        System.out.println("How can I help you?");
     }
 }
