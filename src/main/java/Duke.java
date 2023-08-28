26358ee9f01309c8d6a50dfaaec8d373bb57d6a8
Stanley Wijaya <wijayastanley00@gmail.com>
2023-08-28 16:22:19 +0800

    Add input function and input echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         final String logo = "MudMud";
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Oh hello! I'm " + logo + ".");
-        System.out.println("How can I help you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Goodbye! I am going to sleep now.");
-        System.out.println("____________________________________________________________");
+        Scanner in = new Scanner(System.in);
+        String input = "";
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tOh hello! I'm " + logo + ".");
+        System.out.println("\tHow can I help you?");
+        System.out.println("\t____________________________________________________________");
+
+        while (!input.equals("bye")) {
+            input = in.nextLine();
+
+            System.out.println("\t____________________________________________________________");
+            if (input.equals("bye")) {
+                System.out.println("\tGoodbye! I am going to sleep now.");
+            } else {
+                System.out.println("\t" + input);
+            }
+            System.out.println("\t____________________________________________________________");
+
+        }
     }
 }
