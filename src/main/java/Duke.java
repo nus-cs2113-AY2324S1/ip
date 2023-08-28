9bf24f878ae5819da186f3e5035bf74ed81000fb
Ethan <ethanhuang03@gmail.com>
2023-08-28 13:47:28 +0800

    Added ASCII art when greeting user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,10 +27,19 @@ public class Duke {
         	command = input.nextLine();
         }
         sayGoodbye();
+        input.close();
     }
     
     public static void greetUser() {
     	 printLines();
+    	 System.out.println("\n"
+    	 		+ "    __________                      \n"
+    	 		+ "    \\______   \\ ____   _____ ___.__.\n"
+    	 		+ "     |       _// __ \\ /     <   |  |\n"
+    	 		+ "     |    |   \\  ___/|  Y Y  \\___  |\n"
+    	 		+ "     |____|_  /\\___  >__|_|  / ____|\n"
+    	 		+ "            \\/     \\/      \\/\\/     \n"
+    	 		+ "");
          System.out.println("    Hello! I'm Remy");
          System.out.println("    What can I do for you?");
          printLines();
