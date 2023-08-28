021cc70e693cf8eac7fb985620cdcc199ba52d2f
ryan1604 <ryanchua1604@gmail.com>
2023-08-28 22:41:09 +0800

    Modify chatbot to echo commands given by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm RC\nWhat can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("\tHello! I'm RC\n\tWhat can I do for you?\n");
+        Scanner in = new Scanner(System.in);
+        String line = "";
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            System.out.println("\t" + line);
+        }
+        System.out.println("\tBye. Hope to see you again soon!\n");
     }
 }
