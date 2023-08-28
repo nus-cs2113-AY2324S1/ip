4c318c6bd42b3411ceb8494c133845a1d063f1a8
bnjm2000 <bnjm2000@gmail.com>
2023-08-28 16:31:08 +0800

    Renamed Duke to Chatty Add intro to Chatty

---
+++ src/main/java/Chatty.java
@@ -0,0 +1,17 @@
+public class Chatty {
+    /*public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+    }
+     */
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Chatty!\n+What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
