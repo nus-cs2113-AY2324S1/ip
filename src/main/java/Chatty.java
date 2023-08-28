9330cef2de1a0e347f3dd856f34f4e0fea3264dd
bnjm2000 <bnjm2000@gmail.com>
2023-08-28 16:41:46 +0800

    Add echo

--- src/main/java/Chatty.java
+++ src/main/java/Chatty.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Chatty {
     /*public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,7 +10,16 @@ public class Chatty {
      */
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Chatty!\n+What can I do for you?");
+        System.out.println("Hello! I'm Chatty!\nWhat can I do for you?");
+        System.out.println("____________________________________________________________\n");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equalsIgnoreCase("bye")){
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________\n");
+            input = scanner.nextLine();
+        }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
