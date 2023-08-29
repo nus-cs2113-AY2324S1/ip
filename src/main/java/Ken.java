0e4acd62699968994fc640326cdc52bbf4dcc128
Benedict Chan <benedictchan247@gmail.com>
2023-08-29 19:42:22 +0800

    Add task class and ability to mark tasks

--- src/main/java/Ken.java
+++ src/main/java/Ken.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ken {
@@ -17,17 +19,30 @@ public class Ken {
                 + line);
 
         Scanner scan = new Scanner(System.in);
-        String[] storage = new String[100];
+        ArrayList<Task> tasks = new ArrayList<>();
         int count = 0;
         while (true) {
-            String input = scan.nextLine();
-            switch(input) {
+            String[] input = scan.nextLine().split(" ");
+
+            switch(input[0]) {
             case "list":
-                System.out.println(line);
-                for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + storage[i]);
+                System.out.println(line + "Here are the tasks in your list:\n");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) +"." + tasks.get(i).toString());
                 }
                 System.out.println(line);
+                break;
+            case "mark":
+                int markNumber = Integer.parseInt(input[1]) - 1;
+                tasks.get(markNumber).markAsDone();
+                System.out.println(line + "Nice! I've marked this task as done:\n"
+                        + tasks.get(markNumber).toString() + "\n" + line);
+                break;
+            case "unmark":
+                int unmarkNumber = Integer.parseInt(input[1]) - 1;
+                tasks.get(unmarkNumber).unmarkAsDone();
+                System.out.println(line + "Ok, I've marked this task as not done yet:\n"
+                        + tasks.get(unmarkNumber).toString() + "\n" + line);
                 break;
             case "bye":
                 System.out.println(line
@@ -35,9 +50,10 @@ public class Ken {
                         + line);
                 return;
             default:
-                storage[count] = input;
-                System.out.println(line + "added: " + input + "\n" + line);
-                count++;
+                String taskName = String.join(" ", input);
+                Task task = new Task(taskName);
+                tasks.add(task);
+                System.out.println(line + "added: " + taskName + "\n" + line);
                 break;
             }
 
