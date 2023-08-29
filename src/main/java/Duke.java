ce59fc1789a4e090663021d78a1bac6802381f28
AlWo223 <alexander.wolters@tum.de>
2023-08-29 12:35:08 +0800

    Implement echo responses

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,16 +9,27 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         
         String name = "Lexi";
-        final String horizontal_lines = "--------------------------------------------";
+        final String horizontal_line = "--------------------------------------------";
 
         System.out.println(logo);
 
-        System.out.println(horizontal_lines);
+        System.out.println(horizontal_line);
         System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println(horizontal_lines);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontal_lines);
+        System.out.println("How can I help you buddy?");
+        System.out.println(horizontal_line);
+        String input;
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+        do{
+            System.out.println(horizontal_line);
+            System.out.println(input);
+            System.out.println(horizontal_line);
+            input = in.nextLine();
+    } while(!input.toLowerCase().equals("bye"));
+
+        System.out.println(horizontal_line);
+        System.out.println("Have a wonderful day! Hope to see you again soon!");
+        System.out.println(horizontal_line);
 
     }
 }
