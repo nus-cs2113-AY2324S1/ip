e2d5150d8273e0edcfb875e854645c75c77ef198
yicheng-toh <tohyicheng1989@gmail.com>
2023-08-29 11:30:49 +0800

    Add Echo Response

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String generateResponse(String input){
+        String endMessage = "Bye. Hope to see you again soon!";
+
+        String trimmedInput = input.trim();
+        if(trimmedInput.equals("bye")){
+            return endMessage;
+        }
+        return input;
+    }
+
+    public static void printMessage(String message){
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t" + message);
+        System.out.println("\t____________________________________________________________");
+
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,12 +28,21 @@ public class Duke {
                  "Hello! I'm [YOUR CHATBOT NAME]\n"+
                  "What can I do for you?\n"+
                 "____________________________________________________________";
-        String endMessage = "Bye. Hope to see you again soon!\n"+
-                "____________________________________________________________";
+
 
         System.out.println("Hello from\n" + logo);
         System.out.println(startMessage);
-        System.out.println(endMessage);
+        Scanner in = new Scanner(System.in);
+        String line;
+        String response;
+        do{
+            line = in.nextLine();
+            response = generateResponse(line);
+            printMessage(response);
+
+        }while(line.equals(response));
+
+
 
 
     }
