82224dd3bab19a73cee32c763c3a8f35d7f531dd
Bryan Lee <e0725090@u.nus.edu>
2023-08-30 01:29:51 +0800

    Completed Level 2 increment and updated code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -10,17 +11,40 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Dom!");
         System.out.println("What can I do for you?");
+
         Scanner in = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
         while (true) {
             String command = in.nextLine();
             System.out.println("____________________________________________________________");
-            System.out.println(" " + command);
-            System.out.println("____________________________________________________________");
-            if (command.equals("bye")) {
-                System.out.println(("Bye. Hope to see you again soon"));
+            if(command.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (command.equalsIgnoreCase("list")) {
+                System.out.println(" " + getTaskList(list));
+            } else {
+                list.add(command);
+                System.out.println("added: " + command);
+            }
+            //System.out.println(" " + command);
+            System.out.println("____________________________________________________________");
+            //if (command.equals("bye")) {
+                //System.out.println(("Bye. Hope to see you again soon"));
+                //break;
             }
         }
+        private static String getTaskList(ArrayList<String>  list) {
+            StringBuilder task = new StringBuilder();
+            if(list.isEmpty()) {
+                task.append("There is no task in your list");
+            } else {
+                for(int i = 0; i < list.size(); i++) {
+                    task.append(i + 1).append(".").append(list.get(i)).append("\n");
+                }
+            }
+            return task.toString();
+        }
     }
-}
+
 
