fe6803cd05c0b3163648e6aad1c427d283983340
yeo-menghan <yeomenghan1989@gmail.com>
2023-08-29 22:46:15 +0800

    Add Echo to echo user input and bye to exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,47 @@
-public class Duke {
-    public static void main(String[] args) {
-      String logo = "\n" +
-      "     ____.  _____ ______________   ____.___  _________\n" +
-      "    |    | /  _  \\\\______   \\   \\ /   /|   |/   _____/\n" +
-      "    |    |/  /_\\  \\|       _/\\   Y   / |   |\\_____  \\ \n" +
-      "/\\__|    /    |    \\    |   \\ \\     /  |   |/        \\\n" +
-      "\\________\\____|__  /____|_  /  \\___/   |___/_______  /\n" +
-      "                 \\/       \\/                       \\/ \n";
-      System.out.println("Hello from\n" + logo);
+import java.util.Scanner;
 
-      System.out.println("____________________________________________________________\n" +
+public class Duke {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String getUserInput() {
+        return scanner.nextLine();
+    }
+
+    public static void printUserInput(String output) {
+        if(output.toLowerCase().equals("bye")){
+            output = " Bye. Hope to see you again soon! ";
+        }
+        System.out.println("____________________________________________________________\n" +
+                " " + output + "\n" +
+                "____________________________________________________________\n");
+    }
+
+    public static void closeScanner() {
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+
+        String logo = "\n" +
+        "     ____.  _____ ______________   ____.___  _________\n" +
+        "    |    | /  _  \\\\______   \\   \\ /   /|   |/   _____/\n" +
+        "    |    |/  /_\\  \\|       _/\\   Y   / |   |\\_____  \\ \n" +
+        "/\\__|    /    |    \\    |   \\ \\     /  |   |/        \\\n" +
+        "\\________\\____|__  /____|_  /  \\___/   |___/_______  /\n" +
+        "                 \\/       \\/                       \\/ \n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________\n" +
                 " Hello! I'm JARVIS \n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+                "____________________________________________________________\n" );
+
+        String userInput;
+        do{
+            userInput = getUserInput();
+            printUserInput(userInput);
+        }while(!userInput.equals("bye"));
+
+        closeScanner();
     }
 }
---
+++ src/main/java/Utility.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Utility {
+//    private Scanner scanner = new Scanner(System.in);
+//
+//    public String getUserInput() {
+//        String input = scanner.nextLine();
+//        if (input.toLowerCase().equals("bye")) {
+//            return " Bye. Hope to see you again soon!\n";
+//        } else {
+//            return input;
+//        }
+//    }
+//
+//    public void printUserInput(String output) {
+//        System.out.println("____________________________________________________________\n" +
+//                " " + output + " " +
+//                "____________________________________________________________\n");
+//    }
+//
+//    public void closeScanner() {
+//        scanner.close();
+//    }
+}
