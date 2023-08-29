0ffbc199692ee5391d5d13dd37efff2cc582ad74
NeoMinWei <>
2023-08-29 19:55:18 +0800

    Add task class, add ability to mark and umark tasks

--- src/main/java/Neo.java
+++ src/main/java/Neo.java
@@ -6,18 +6,38 @@ public class Neo {
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
-        String[] list = new String[100];
-        int listCount = 0;
+        Task[] list = new Task[100];
+        for (int i = 0; i < 100; i++) {
+            list[i] = new Task("");
+        }
         while (!line.equals("bye")) {
             if (line.equals("list")) {
-                for (int i = 0; i < listCount; i++) {
-                    System.out.println((i + 1) + ". " + list[i]);
+                for (int i = 0; i < Task.getTotalTasks(); i++) {
+                    System.out.print((i + 1) + ". ");
+                    System.out.print("[" + list[i].getStatusIcon() + "] ");
+                    System.out.println(list[i].description);
                 }
                 line = in.nextLine();
             }
+            else if (line.startsWith("mark")) {
+                String[] words = line.split(" ");
+                int listIndex = Integer.parseInt(words[1]) - 1;
+                list[listIndex].setDone(true);
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("    [X] " + list[listIndex].description);
+                line = in.nextLine();
+            }
+            else if (line.startsWith("unmark")) {
+                String[] words = line.split(" ");
+                int listIndex = Integer.parseInt(words[1]) - 1;
+                list[listIndex].setDone(false);
+                System.out.println("OK, I've marked this task as not done yet: ");
+                System.out.println("    [ ] " + list[listIndex].description);
+                line = in.nextLine();
+            }
             else {
-                list[listCount] = line;
-                listCount++;
+                list[Task.getTotalTasks()].description = line;
+                Task.incrementTotalTasks();
                 System.out.println("added: " + line);
                 line = in.nextLine();
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    private static int totalTasks = 0;
+
+
+    public static int getTotalTasks() {
+        return totalTasks;
+    }
+
+    public static void incrementTotalTasks() {
+        Task.totalTasks++;
+    }
+
+    public void setDone(boolean done){
+        isDone = done;
+    }
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+}
