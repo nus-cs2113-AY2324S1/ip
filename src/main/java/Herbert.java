93a262a948376c897594ebe0a0a61e6fb7e31dbc
antrikshdhand <>
2023-08-30 00:26:24 +0800

    Add task completion feature

--- src/main/java/Herbert.java
+++ src/main/java/Herbert.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 
 public class Herbert {
 
-    private ArrayList<String> history;
+    private ArrayList<Task> tasks;
 
     public Herbert() {
-        this.history = new ArrayList<>();
+        this.tasks = new ArrayList<>();
         this.sayHello();
     }
 
@@ -35,29 +36,72 @@ public class Herbert {
     }
 
     public int processLine(String line) {
-        if (line.equalsIgnoreCase("bye")) {
+        line = line.strip();
+        line = line.toLowerCase();
+
+        if (line.equals("bye")) {
             sayGoodbye();
             return 1;
-        } else if (line.equalsIgnoreCase("list")) {
+        } else if (line.equals("list")) {
             list();
+        } else if (line.startsWith("mark")) {
+            markTask(line, true);
+        } else if (line.startsWith("unmark")) {
+            markTask(line, false);
         } else {
-            this.history.add(line);
-            System.out.println("___________________________________________________________________________");
-            System.out.println("\tAdded: " + line);
-            System.out.println("___________________________________________________________________________");
+            addTask(line);
         }
 
         return 0;
     }
 
-    public void list() {
-        System.out.println("___________________________________________________________________________");
-        for (int i = 0; i < history.size(); i++) {
-            System.out.println("\t" + (i + 1) + ". " + history.get(i));
+    public void markTask(String line, boolean completed) {
+        if (line.split(" ").length != 2) {
+            if (completed) {
+                System.out.println("Invalid command. Usage: mark <task number>");
+            } else {
+                System.out.println("Invalid command. Usage: unmark <task number>");
+            }
         }
+
+        int taskToMarkIndex = Integer.parseInt(line.split(" ")[1]) - 1;
+        Task taskToMark = tasks.get(taskToMarkIndex);
+
+        taskToMark.setCompleted(completed);
+
+        System.out.println("___________________________________________________________________________");
+        if (completed) {
+            System.out.println("\tLovely! I've marked this task as done:");
+        } else {
+            System.out.println("\tOkay, I've unmarked this task for you:");
+        }
+        System.out.println("\t   [" + taskToMark.getStatusIcon() + "] " + taskToMark.getDescription());
         System.out.println("___________________________________________________________________________");
     }
 
+    public void addTask(String line) {
+        Task t = new Task(line);
+
+        this.tasks.add(t);
+        System.out.println("___________________________________________________________________________");
+        System.out.println("\tAdded: " + t.getDescription());
+        System.out.println("___________________________________________________________________________");
+    }
+
+    public void list() {
+        System.out.println("___________________________________________________________________________");
+        System.out.println("\tHere are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            String s = "\t" +
+                    (i + 1) +
+                    ". [" +
+                    tasks.get(i).getStatusIcon() +
+                    "] " +
+                    tasks.get(i).getDescription();
+            System.out.println(s);
+        }
+        System.out.println("___________________________________________________________________________");
+    }
 
 }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+
+    private String description;
+    private boolean completed;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getStatusIcon() {
+        return (completed ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
