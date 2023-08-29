5d6458c7e0819fbc1dbbbe322d8e4c01b4ac5c94
ShaniceTang <e0959739@u.nus.edu>
2023-08-29 18:32:06 +0800

    Update Chattie.java

--- src/main/java/Chattie.java
+++ src/main/java/Chattie.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Chattie {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Chattie!");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Chattie!");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+
+        do {
+            line = in.nextLine();
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t" + line);
+            System.out.println("\t____________________________________________________________");
+        } while (!line.equals("bye"));
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
