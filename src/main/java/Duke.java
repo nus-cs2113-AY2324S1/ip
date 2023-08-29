537a71a6e11e2c3c4466830f88d09f5dbcee14ff
Benedict Chan <benedictchan247@gmail.com>
2023-08-29 15:03:51 +0800

    Add logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " _   __ ______  __    __\n"
+                    + "| | / /|  ____||  \\  |  |\n"
+                    + "| |/ / | |____ |   \\ |  |\n"
+                    + "|   /  |  ____||    \\|  |\n"
+                    + "| |\\ \\ | |____ |        |\n"
+                    + "|_| \\_\\|______||__|\\____|";
+        String line = "_______________________________________________\n";
+        System.out.println(logo);
+
+        System.out.println(line
+                + "Hello! I'm Ken!\n"
+                + "What can I do for you?\n"
+                + line
+                + "Bye. Hope to see you again soon!\n"
+                + line);
+
     }
 }
