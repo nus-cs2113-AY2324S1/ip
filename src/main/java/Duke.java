705552689eaa6ffb41ac72dfa77a5dcdc769374e
hooami <27563949+hooami@users.noreply.github.com>
2023-08-30 11:01:29 +0800

    Completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static final String line = "__________________________________________";
     public static void main(String[] args) {
@@ -7,20 +8,31 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println(line);
         String input;
+        ArrayList<Task> currentTask = new ArrayList<>();
         while(true){
             Scanner in = new Scanner(System.in);
             input = in.nextLine();
-            if (input.equals("bye")){
-                System.out.println(line);
+            System.out.println(line);
+            switch(input){
+            case "bye":
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
-                break;
-            }
-            else{
+                return;
+            case "list":
+                int i = 1;
+                for (Task t: currentTask){
+                    System.out.println(i + ": " + t.getTasks());
+                    i++;
+                }
                 System.out.println(line);
-                System.out.println(input);
+
+            default:
+                Task t = new Task(input);
+                currentTask.add(t);
+                System.out.println("added: " + input);
                 System.out.println(line);
             }
+
         }
 
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+    private String tasks;
+
+    public Task(){
+
+    }
+    public Task(String tasks) {
+        this.tasks = tasks;
+    }
+
+    public String getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(String tasks) {
+        this.tasks = tasks;
+    }
+}
