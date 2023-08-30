1daf72f8f1e5678d262832d800b13315f3b2429b
000verflow <ashokcoolboy5@gmail.com>
2023-08-30 12:20:19 +0800

    Update Duke.java
    Update to meet coding standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,26 +2,29 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    static String chatbotName="Andrew Tate";
-    static ArrayList<Task> tasks = new ArrayList<Task>();
+    static String CHATBOTNAME="Andrew Tate";
+    static ArrayList<Task> TASKS = new ArrayList<Task>();
 
     public static void main(String[] args){
         Scanner myScanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("Hello! I'm " + CHATBOTNAME);
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+
         label:
         while (true){
             String commandGiven = myScanner.nextLine();
             String[] splitCommand = commandGiven.split("\\s+");
-            switch (splitCommand[0]) {
+            String actionCommand = splitCommand[0];
+
+            switch (actionCommand) {
                 case "bye":
                     break label;
                 case "list":
                     System.out.println("____________________________________________________________");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        Task task = tasks.get(i);
+                    for (int i = 0; i < TASKS.size(); i++) {
+                        Task task = TASKS.get(i);
                         System.out.println(i + 1 + ".[" + task.getStatusIcon() + "] " + task.description);
                     }
                     System.out.println("____________________________________________________________");
@@ -30,7 +33,7 @@ public class Duke {
                 case "mark":
                     try{
                         int taskIndex = Integer.parseInt(splitCommand[1]);
-                        Task taskToMark = tasks.get(taskIndex-1);
+                        Task taskToMark = TASKS.get(taskIndex-1);
                         taskToMark.markAsDone();
                         System.out.println("____________________________________________________________");
                         System.out.println("Marked this task as done:");
@@ -38,14 +41,16 @@ public class Duke {
                         System.out.println("____________________________________________________________");
 
                     }catch(Exception e){
-                        System.out.println("Invalid index.");
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Invalid index, or other error!");
+                        System.out.println("____________________________________________________________");
                     }
                     break;
 
                 case "unmark":
                     try{
                         int taskIndex = Integer.parseInt(splitCommand[1]);
-                        Task taskToUnmark = tasks.get(taskIndex-1);
+                        Task taskToUnmark = TASKS.get(taskIndex-1);
                         taskToUnmark.markAsUndone();
                         System.out.println("____________________________________________________________");
                         System.out.println("Marked this task as undone:");
@@ -53,13 +58,15 @@ public class Duke {
                         System.out.println("____________________________________________________________");
 
                     }catch(Exception e){
-                        System.out.println("Invalid index.");
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Invalid index, or other error!");
+                        System.out.println("____________________________________________________________");
                     }
                     break;
 
                 default:
                     Task task = new Task(commandGiven);
-                    tasks.add(task);
+                    TASKS.add(task);
                     System.out.println("____________________________________________________________");
                     System.out.println("added: " + commandGiven);
                     System.out.println("____________________________________________________________");
