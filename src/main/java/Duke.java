0553a67ba2a97d2dbb0631690a54702f24dd0b7c
ChungZhiXuan <zhixuanchung03@gmail.com>
2023-08-30 23:51:47 +0800

    Add list function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,6 +8,10 @@ public class Duke {
             + " \\__   |  ___/  |_|  |  |   |   __|        |  |_|  |       |\n"
             + "/      | |   |   _   |   \\__|     |   __   |   _   |       |\n"
             + "|____ /|_|   |__| |__|\\_____|_____|__|  |__|__| |__|__|\\___|\n";
+
+        String[] itemList = new String[100];
+        int count = 0;
+
         System.out.println("Hello from\n" + logo);
         System.out.println("------------------------------------------------------------");
         System.out.println("Hello! I'm Spaceman");
@@ -17,19 +21,33 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
 
         String text = sc.nextLine();
-        do {
-            echo(text);
+        while (!text.equals("bye")) {
+            if (text.equals("list")) {
+                printList(itemList, count);
+            } else{
+                addToList(itemList, text, count);
+                count++;
+            }
             text = sc.nextLine();
-        } while (!text.equals("bye"));
+        }
 
         System.out.println("------------------------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("------------------------------------------------------------");
     }
 
-    public static void echo(String text){
+    public static void addToList(String[] itemList, String item, int count){
+        itemList[count] = item;
         System.out.println("------------------------------------------------------------");
-        System.out.println(text);
+        System.out.println("added: " + item);
+        System.out.println("------------------------------------------------------------");
+    }
+
+    public static void printList(String[] items, int count){
+        System.out.println("------------------------------------------------------------");
+        for (int i = 0; i < count; i++){
+            System.out.println(i + 1 + ". " + items[i]);
+        }
         System.out.println("------------------------------------------------------------");
     }
 
