c78f17c658f865531c9f649e6557e0acb175ebfe
kevinz420 <kzhang0305@gmail.com>
2023-08-30 22:45:01 +0800

    Added command echoing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm KevBot");
         System.out.println("What can I do for you?");
+
+        Scanner in = new Scanner(System.in);
+        String line;
+        while (in.hasNextLine()) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+
+            System.out.println(line);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
