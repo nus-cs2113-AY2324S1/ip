da4a93a448854f4d021a5ea3012f012056f888de
kevinz420 <kzhang0305@gmail.com>
2023-08-30 23:19:53 +0800

    Added support for marking/unmarking tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static String[] tasks = new String[100];
+    private static Task[] tasks = new Task[100];
 
     public static void main(String[] args) {
         System.out.println("Hello! I'm KevBot");
@@ -16,6 +16,15 @@ public class Duke {
                 break;
             } else if (line.equals("list")) {
                 printTasks();
+            } else if (line.contains("mark")) {
+                int divider = line.indexOf(" ");
+                int idx = Integer.parseInt(line.substring(divider + 1)) - 1;
+                if (idx < 0 || tasks[idx] == null) {
+                    System.out.println("Sorry! That's not a valid task");
+                    continue;
+                }
+
+                markTask(idx, line.startsWith("mark"));
             } else {
                 addTask(line);
             }
@@ -23,16 +32,26 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    public static void markTask(int index, boolean isDone) {
+        tasks[index].setStatus(isDone);
+        if (isDone) {
+            System.out.println("Nice! I've marked this task as done:");
+        } else {
+            System.out.println("OK, I've marked this task as not done yet:");
+        }
+        System.out.println(tasks[index].getFormattedTask());
+    }
+
     public static void printTasks() {
         for (int i = 0; i < tasks.length && tasks[i] != null; i++) {
-            System.out.println((i + 1) + ". " + tasks[i]);
+            System.out.println((i + 1) + ". " + tasks[i].getFormattedTask());
         }
     }
 
     public static void addTask(String task) {
         for (int i = 0; i < tasks.length; i ++) {
             if (tasks[i] == null) {
-                tasks[i] = task;
+                tasks[i] = new Task(task);
                 break;
             }
         }
