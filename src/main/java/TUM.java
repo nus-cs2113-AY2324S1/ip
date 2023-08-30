15894161eebf5c3c7795920551de35c9051050ae
HaoyuLi <johndoe@example.com>
2023-08-30 17:48:25 +0800

    Third Increment(Add, List)

--- src/main/java/TUM.java
+++ src/main/java/TUM.java
@@ -1,7 +1,10 @@
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TUM {
     public static void main(String[] args) {
+        List<String> textList = new LinkedList<>();
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm TUM");
         System.out.println(" What can I do for you?");
@@ -10,11 +13,20 @@ public class TUM {
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while (!line.equals("bye")) {
-            System.out.println("____________________________________________________________");
-            System.out.println(line);
-            System.out.println("____________________________________________________________");
+            if (line.equals("list")) {
+                for(int i = 0; i < textList.size(); i++) {
+                    System.out.println((i+1) + ". " + textList.get(i));
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("____________________________________________________________");
+                textList.add(line);
+                System.out.println("added: " + line);
+                System.out.println("____________________________________________________________");
+            }
             line = in.nextLine();
         }
+        System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
