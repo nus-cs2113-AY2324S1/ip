82a5318a383e440dac0850f46de8dab3cdd7c777
ShaniceTang <e0959739@u.nus.edu>
2023-08-30 14:41:09 +0800

    Update Chattie.java

--- src/main/java/Chattie.java
+++ src/main/java/Chattie.java
@@ -3,11 +3,24 @@ import java.util.Scanner;
 public class Chattie {
     public static void main(String[] args) {
 
-        String line = " ";
+        String line;
         Scanner in = new Scanner(System.in);
 
         System.out.println("\t____________________________________________________________");
-        System.out.println("\tHello! I'm Chattie!");
+        System.out.println("\tHello! I'm Chattie! How was your day?");
+        System.out.println("\t____________________________________________________________");
+
+        line = in.nextLine();
+        line = line.toLowerCase();
+
+        System.out.println("\t____________________________________________________________");
+        if(line.contains("good")) {
+            System.out.println("\tGreat to hear that! :D");
+        } else if (line.contains("bad")) {
+            System.out.println("\tI'm sorry to hear that :(");
+        } else {
+            System.out.println("\tSounds like you had quite a day");
+        }
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
 
