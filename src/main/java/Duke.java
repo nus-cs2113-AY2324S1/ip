b1687935aad60856e3a3d0c00620486af445aaad
ChungZhiXuan <zhixuanchung03@gmail.com>
2023-08-30 23:22:19 +0800

    Add echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "  ____  _____   ___    _____ _____ __    __   ___   __   __\n"
@@ -11,7 +13,24 @@ public class Duke {
         System.out.println("Hello! I'm Spaceman");
         System.out.println("What can I do for you?");
         System.out.println("------------------------------------------------------------");
+
+        Scanner sc = new Scanner(System.in);
+
+        String text = sc.nextLine();
+        do {
+            echo(text);
+            text = sc.nextLine();
+        } while (!text.equals("bye"));
+
+        System.out.println("------------------------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("------------------------------------------------------------");
     }
+
+    public static void echo(String text){
+        System.out.println("------------------------------------------------------------");
+        System.out.println(text);
+        System.out.println("------------------------------------------------------------");
+    }
+
 }
