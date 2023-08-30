a7217358363442081656fc956544fd5740ff7de2
Mark Lin <mark.lin2234@gmail.com>
2023-08-30 18:19:47 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void respond(String s) {
+        System.out.println("____________________________________________________________\n" +
+                s + "\n" +
+                "____________________________________________________________");
+    }
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -6,12 +14,13 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Mark\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
-
+        respond("Hello! I'm Mark\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String in = sc.nextLine();
+        while(!in.equals("bye")) {
+            respond(in);
+            in = sc.nextLine();
+        }
+        respond("Bye. Hope to see you again soon!");
     }
 }
