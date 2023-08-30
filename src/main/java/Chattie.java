dc2d2e39da66a591ffe5ad6bca5223b7c53e788d
ShaniceTang <e0959739@u.nus.edu>
2023-08-30 15:05:31 +0800

    Add list to Chattie.java

--- src/main/java/Chattie.java
+++ src/main/java/Chattie.java
@@ -1,16 +1,8 @@
 import java.util.Scanner;
 
 public class Chattie {
-    public static void main(String[] args) {
 
-        String line;
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tHello! I'm Chattie! How was your day?");
-        System.out.println("\t____________________________________________________________");
-
-        line = in.nextLine();
+    public static void chattieGreeting(String line) {
         line = line.toLowerCase();
 
         System.out.println("\t____________________________________________________________");
@@ -23,17 +15,46 @@ public class Chattie {
         }
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
+    }
+
+    public static void chattieList(String[] list, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println("\t" + (i+1) + ". " + list[i]);
+        }
+    }
+    public static void main(String[] args) {
+
+        int count = 0;
+        String[] list = new String[100];
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Chattie! How was your day?");
+        System.out.println("\t____________________________________________________________");
 
         line = in.nextLine();
+        chattieGreeting(line);
+
+        line = in.nextLine();
+
         while(!line.equals("bye")) {
             System.out.println("\t____________________________________________________________");
-            System.out.println("\t" + line);
+
+            if(line.equals("list")) {
+                chattieList(list, count);
+            } else {
+                list[count] = line;
+                count++;
+                System.out.println("\tadded: " + line);
+            }
+
             System.out.println("\t____________________________________________________________");
             line = in.nextLine();
         }
 
         System.out.println("\t____________________________________________________________");
-        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\tByeeeee. Hope to see you again soon! :)");
         System.out.println("\t____________________________________________________________");
     }
 }
