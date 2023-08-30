eaa7199d130edf8980eb5d7028638a8d89819129
hooami <27563949+hooami@users.noreply.github.com>
2023-08-30 10:48:30 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
 public class Duke {
-    public static final String line = "---------------------------------------------";
+    public static final String line = "__________________________________________";
     public static void main(String[] args) {
         System.out.println(line);
         System.out.println("Hello! I'm Chat0PT");
-        System.out.println("What can I do for you?\n");
+        System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println(line);
+        String input;
+        while(true){
+            Scanner in = new Scanner(System.in);
+            input = in.nextLine();
+            if (input.equals("bye")){
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            else{
+                System.out.println(line);
+                System.out.println(input);
+                System.out.println(line);
+            }
+        }
+
+
     }
+
 }
