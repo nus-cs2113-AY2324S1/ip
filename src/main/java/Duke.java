a0623dbc5ab52d57c5171b6a3df87ae3dbed02f8
kevinz420 <kzhang0305@gmail.com>
2023-08-30 23:00:03 +0800

    Added task tracking functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] tasks = new String[100];
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm KevBot");
         System.out.println("What can I do for you?");
+
 
         Scanner in = new Scanner(System.in);
         String line;
@@ -11,10 +14,28 @@ public class Duke {
             line = in.nextLine();
             if (line.equals("bye")) {
                 break;
+            } else if (line.equals("list")) {
+                printTasks();
+            } else {
+                addTask(line);
             }
-
-            System.out.println(line);
         }
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printTasks() {
+        for (int i = 0; i < tasks.length && tasks[i] != null; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+    }
+
+    public static void addTask(String task) {
+        for (int i = 0; i < tasks.length; i ++) {
+            if (tasks[i] == null) {
+                tasks[i] = task;
+                break;
+            }
+        }
+        System.out.println("added: " + task);
     }
 }
