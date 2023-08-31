f19959271e972178ab8edf16bc509f168440e871
Jeremy Y. <imaginarys96@gmail.com>
2023-08-31 21:05:18 +0800

    Implemented Level 1 - echo, bye

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Chatbot {
     public static void main(String[] args) {
-        String msg = "____________________________________________________________\n" +
+        String greetingMsg = "____________________________________________________________\n" +
                 " Hello! I'm Chatbot\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n";
+        String byeMsg = "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
-        System.out.println(msg);
+        System.out.println(greetingMsg);
+
+
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+            input = in.nextLine();
+        }
+        System.out.println(byeMsg);
+
     }
 }
