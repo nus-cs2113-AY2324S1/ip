ad5a71720da5c08d19e9dadf4dc0bc2e202e5e4a
huanghuiling <huanghuiling2022@outlook.com>
2023-08-31 21:44:42 +0800

    Add a new class Task and the ability to mark as done

--- src/main/java/Sun.java
+++ src/main/java/Sun.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Sun {
     public static void main(String[] args) {
         String logo = " ____               \n"
@@ -13,23 +12,43 @@ public class Sun {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         String command;
         do{
             command = scanner.nextLine();
+
             if (!command.equals("bye")) {
                 if (command.equals("list")) {
                     System.out.println("____________________________________________________________");
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
+                        System.out.println((i + 1) + "." + tasks[i].getStatusIcon() + " " + tasks[i].description);
                     }
                     System.out.println("____________________________________________________________");
+                } else if (command.startsWith("mark ")) {
+                    int taskIndex = Integer.parseInt(command.substring(5)) - 1;
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        tasks[taskIndex].markAsDone();
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  " + tasks[taskIndex].getStatusIcon() + " " + tasks[taskIndex].description);
+                        System.out.println("____________________________________________________________");
+                    }
+                } else if (command.startsWith("unmark ")) {
+                    int taskIndex = Integer.parseInt(command.substring(7)) - 1;
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        tasks[taskIndex].markAsNotDone();
+                        System.out.println("____________________________________________________________");
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("  " + tasks[taskIndex].getStatusIcon() + " " + tasks[taskIndex].description);
+                        System.out.println("____________________________________________________________");
+                    }
                 } else {
-                    tasks[taskCount] = command;
+                    tasks[taskCount] = new Task(command);
                     taskCount++;
                     System.out.println("____________________________________________________________");
-                    System.out.println("> added: " + command);
+                    System.out.println("added: " + command);
                     System.out.println("____________________________________________________________");
                 }
             }
