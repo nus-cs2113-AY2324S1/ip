d9d04f2dbbcae64ff71fa5433ae2575c3a6d84ec
KenCheung18 <kycheungbf@connect.ust.hk>
2023-08-31 22:55:42 +0800

    Task 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println(" Hello! I'm Bot Hilary ");
         System.out.println("  What can I do for you? ");
-        System.out.println("  Bye. Hope to see you again soon! ");
-
+        String line = in.nextLine();
+        if (line.equals("bye")) {
+            System.out.println("  Bye. Hope to see you again soon! ");
+        }
+        else{
+            System.out.println(line);
+        }
     }
 }
