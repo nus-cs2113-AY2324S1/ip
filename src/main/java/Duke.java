8d479f19e779fcdaf84b969d0065afaf502174d2
Your <icisaiahcerven@gmail.com>
2023-08-31 03:55:11 -0600

    Add introduction statements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello my name is DUKE");
+        System.out.println("What can I do for you today?");
+
+        System.out.println("Fine. If you have no ideas Imma head out");
+
     }
 }
