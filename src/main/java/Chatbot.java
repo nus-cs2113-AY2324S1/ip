74cd2693d95508c8d6f6edff3f658dda3b34477a
Jeremy Y. <imaginarys96@gmail.com>
2023-09-01 00:22:50 +0800

    Implemented Level 2 - Add, list

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Chatbot {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int numOfTasks = 0;
+
         String greetingMsg = "____________________________________________________________\n" +
                 " Hello! I'm Chatbot\n" +
                 " What can I do for you?\n" +
@@ -15,9 +18,22 @@ public class Chatbot {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         while(!input.equals("bye")) {
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
+            if( input.equals("list") ) {
+                System.out.println("____________________________________________________________");
+                for(int i=0; i<numOfTasks; i++){
+                    System.out.println( (i+1) + ". " + tasks[i] );
+                }
+                System.out.println("____________________________________________________________");
+
+            } else {
+                // if not bye, list, then the command by default is add
+                tasks[numOfTasks] = input;
+                numOfTasks++;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
+                System.out.println("____________________________________________________________");
+            }
+
             input = in.nextLine();
         }
         System.out.println(byeMsg);
