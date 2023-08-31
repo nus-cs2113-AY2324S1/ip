5e82531ead13d9baef79cadfffeca2660b0f9470
KenCheung18 <kycheungbf@connect.ust.hk>
2023-09-01 04:04:48 +0800

    task 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,13 +5,37 @@ public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println(" Hello! I'm Bot Hilary ");
-        System.out.println("  What can I do for you? ");
+        String[] list = new String[100];
+        boolean[] isDone = new boolean[100];
+        int listCount = 0;
+        System.out.println(" What can I do for you? ");
         String line = in.nextLine();
-        if (line.equals("bye")) {
-            System.out.println("  Bye. Hope to see you again soon! ");
+        while (!line.equals("bye")){
+            if (line.equals("list")) {
+                for (int i = 0; i < listCount; ++i) {
+                    System.out.print(i + 1 + ". [");
+                    System.out.println(((isDone[i]) ? "X" : " " )+ "]" + list[i]);
+                }
+            }
+            else if (line.contains("unmark")){
+                line = line.replace("unmark ", "");
+                isDone[Integer.parseInt(line) - 1] = false;
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   [ ]" + list[Integer.parseInt(line) - 1]);
+            }
+            else if (line.contains("mark")){
+                line = line.replace("mark ", "");
+                isDone[Integer.parseInt(line) - 1] = true;
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   [X]" + list[Integer.parseInt(line) - 1]);
+            }
+            else {
+                list[listCount] = line;
+                listCount++;
+                System.out.println("Added: " + line);
+            }
+            line = in.nextLine();
         }
-        else{
-            System.out.println(line);
-        }
+        System.out.println("  Bye. Hope to see you again soon! ");
     }
 }
