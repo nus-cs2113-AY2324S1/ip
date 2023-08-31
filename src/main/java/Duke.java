e4c98c90245178dfffb0576361ab6ff54ae1957a
ryanlohyr <ryanloh88@ymail.com>
2023-08-31 22:33:04 +0800

    Add name and greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logoRyan = " ____  _          \n"
+                + "|  _ \\| | ___   __\n"
+                + "| |_) | |/ _ \\ / _|\n"
+                + "|  __/| | (_) | (__\n"
+                + "|_|   |_|\\___/ \\___|\n";
+
+        System.out.println("Hello from\n" + logoRyan);
+
+        String line = "Hello! I'm Ryan Loh \nWhat can I do for you?\n";
+
+        String lineBreak = "------------------------------ \n";
+        System.out.println(line + lineBreak + "Bye. Hope to see you again soon!\n");
+
     }
 }
