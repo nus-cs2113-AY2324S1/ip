8318944849fd8bf1b686fea7ff1a060fb92eb243
junhyeong0411 <jazzdosa@kaist.ac.kr>
2023-08-31 23:47:55 +0900

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,50 @@
 import java.util.Scanner;
+
+class Task{
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description){
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon(){
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void doMark(){
+        isDone = true;
+    }
+
+    public void unMark(){
+        isDone = false;
+    }
+
+}
 public class Duke {
     static void printLine(String s){
         System.out.println("     " + s);
     }
 
+    static boolean isNumeric(String s){
+        try{
+            Integer.parseInt(s);
+            return true;
+        }
+        catch(NumberFormatException e){
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[] inputs = new String[100];
-        int inputs_size = 0;
+        Task[] tasks = new Task[100];
+        int tasks_size = 0;
 
         System.out.println("    ____________________________________________________________");
         printLine("Hello! I'm Nupjuk");
@@ -16,24 +53,71 @@ public class Duke {
 
         while(true){ // get input until bye
             String cmd = input.nextLine();
+            String[] tokens = cmd.split(" ", 2);
             System.out.println("    ____________________________________________________________");
+            //System.out.println(tokens[0]);
             if(cmd.equals("bye")){ // bye command
                 break;
             }
             else if(cmd.equals("list")){
-                if(inputs_size == 0){
+                if(tasks_size == 0){
                     printLine("Nothing in the list");
                 }
                 else{
-                    for(int i=0;i<inputs_size;i++){
-                        printLine(String.format("%d: ", i+1) + inputs[i]);
+                    for(int i=0;i<tasks_size;i++){
+                        printLine(String.format("%d.[", i+1) + tasks[i].getStatusIcon() + "] "  + tasks[i].getDescription());
                     }
                 }
                 System.out.println("    ____________________________________________________________\n");
             }
+            else if(tokens[0].equals("mark")){
+
+                // error handling
+                if(tokens.length !=2 || !isNumeric(tokens[1])){
+                    printLine("unexpected behavior: 'mark' needs one integer parameter");
+                    System.out.println("    ____________________________________________________________\n");
+                    continue;
+                }
+
+                printLine("Nice! I've marked this task as done:");
+                int mark_idx = Integer.parseInt(tokens[1])-1; // index starts from 0
+
+                // if idx not in proper range
+                if(mark_idx <0 || mark_idx >= tasks_size){
+                    printLine("unexpected behavior: index out of range");
+                    System.out.println("    ____________________________________________________________\n");
+                    continue;
+                }
+                // do mark
+                tasks[mark_idx].doMark();
+                printLine(" [" + tasks[mark_idx].getStatusIcon() + "] " + tasks[mark_idx].getDescription());
+                System.out.println("    ____________________________________________________________\n");
+            }
+            else if(tokens[0].equals("unmark")){
+                if(tokens.length !=2 || !isNumeric(tokens[1])){
+                    printLine("unexpected behavior: 'mark' needs one integer parameter");
+                    System.out.println("    ____________________________________________________________\n");
+                    continue;
+                }
+
+                printLine("OK, I've marked this task as not done yet:");
+                int mark_idx = Integer.parseInt(tokens[1])-1; // index starts from 0
+
+                // if idx not in proper range
+                if(mark_idx <0 || mark_idx >= tasks_size){
+                    printLine("unexpected behavior: index out of range");
+                    System.out.println("    ____________________________________________________________\n");
+                    continue;
+                }
+
+                // unmark
+                tasks[mark_idx].unMark();
+                printLine(" [" + tasks[mark_idx].getStatusIcon() + "] " + tasks[mark_idx].getDescription());
+                System.out.println("    ____________________________________________________________\n");
+            }
             else{
                 printLine("added: " + cmd); // add list
-                inputs[inputs_size++] = cmd;
+                tasks[tasks_size++] = new Task(cmd);
                 System.out.println("    ____________________________________________________________\n");
             }
         }
