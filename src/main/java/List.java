79574e1466e3efd5a1e647f53e8b31d3d7052c2b
ziyi105 <ahziyi@gmail.com>
2023-08-31 18:53:20 +0800

    List.java: update arraylist of strings to arraylist of tasks

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,21 +1,21 @@
 import java.util.ArrayList;
 
 public class List {
-    ArrayList<String> items;
+    ArrayList<Task> tasks;
 
     public List() {
-        this.items = new ArrayList<String>(100);
+        this.tasks = new ArrayList<Task>(100);
     }
 
-    public void add(String item) {
-        this.items.add(item);
+    public void add(String task) {
+        this.tasks.add(Task.createTask(task));
     }
 
     @Override
     public String toString() {
-        String output = "";
-        for (int i = 1; i <= items.size(); i++) {
-            output = output + i + ". " + this.items.get(i-1) + "\n";
+        String output = "Here are the tasks in your list: \n";
+        for (int i = 1; i <= tasks.size(); i++) {
+            output = output + i + "." + this.tasks.get(i-1) + "\n";
         }
         return output;
     }
