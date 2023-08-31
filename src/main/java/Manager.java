a77a8ea6db119575c183498a47879ec6e7e1361f
ziyi105 <ahziyi@gmail.com>
2023-08-31 18:35:57 +0800

    Manager.java: add list attribute

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -1,11 +1,18 @@
 class Manager {
+    List list;
+
+    public Manager() {
+        this.list = new List();
+    }
     void process(String input) {
         String convertedInput = input.toLowerCase();
         if (convertedInput.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
             System.exit(0);
+        } else if (convertedInput.equals("list")){
+            System.out.println(this.list);
         } else {
-            System.out.println(input);
+            this.list.add(input);
         }
     }
 }
