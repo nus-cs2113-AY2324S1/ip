acc84ea79aed53ce874103715d215e08cf77f94c
junhyeong0411 <jazzdosa@kaist.ac.kr>
2023-08-31 22:58:53 +0900

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Nupjuk");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Nupjuk");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________\n");
+
+        while(true){ // get input until bye
+            String cmd = input.nextLine();
+            System.out.println("     ____________________________________________________________");
+            if(cmd.equals("bye")){ // bye command
+                break;
+            }
+            else{
+                System.out.println("      " + cmd); // echo
+                System.out.println("     ____________________________________________________________\n");
+            }
+        }
+
+        // exit the program
+        System.out.println("      Bye. Hope to see you again soon!");
+        System.out.println("     ____________________________________________________________\n");
     }
 }
