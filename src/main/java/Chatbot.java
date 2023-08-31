70db08d0b7fb011414f0e945f730e2ca32d80e81
Jeremy Y. <imaginarys96@gmail.com>
2023-08-31 13:01:19 +0800

    Implemented skeletal chatbot functionality

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -1,10 +1,11 @@
 public class Chatbot {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String msg = "____________________________________________________________\n" +
+                " Hello! I'm Chatbot\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(msg);
     }
 }
