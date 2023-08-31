a30a0be2b6923f26cf4034e512b5e5b5458896b2
Cazh2 <cazh1or2@gmail.com>
2023-08-31 15:40:16 +0800

    Add Echo function

--- src/main/java/Nuke.java
+++ src/main/java/Nuke.java
@@ -1,22 +1,39 @@
+import java.util.Scanner;
 public class Nuke {
 
     public static void greet() {
+        line();
         System.out.println("Hello! I'm Nuke\n" + "What can I do for you?");
+        line();
     }
 
     public static void exit() {
+        line();
         System.out.println("Bye. Hope to see you again soon!");
+        line();
     }
 
     public static void line() {
         System.out.println("____________________________________________________________");
     }
 
+    public static void echo() {
+
+        Scanner input = new Scanner(System.in);
+        String item = input.nextLine();
+        if (item.equals("bye")) {
+            exit();
+            return;
+        } else {
+            line();
+            System.out.println(item);
+            line();
+            echo();
+        }
+    }
+
     public static void main(String[] args) {
-        line();
         greet();
-        line();
-        exit();
-        line();
+        echo();
     }
 }
