2fa2fd0946d98f7bcf403c2dbd1ba9e6a49699e9
NaychiMin <naychimin02@gmail.com>
2023-09-01 12:17:45 +0800

    Add new function 'addItems'
    This function accepts user's input and adds it to a To Do list.

--- src/main/java/Zran.java
+++ src/main/java/Zran.java
@@ -1,24 +1,49 @@
 import java.util.Scanner;
 
 public class Zran {
+    static String outputFormat = "    ____________________________________________________________\n" +
+            "    %s\n    ____________________________________________________________";
+
     public static void main(String[] args) {
-        System.out.println("    ____________________________________________________________");
-        System.out.println("    Hello! I'm Zran!");
-        System.out.println("    What can I do for you?");
-        System.out.println("    ____________________________________________________________");
-        echo();
+        System.out.printf((outputFormat) + "%n",
+                "hello! I'm Zran, your personal assistant:)! \n" +
+                "    Type in your to dos for the day and press enter! \n " +
+                "   Type 'list' to view your to do list. \n " +
+                "   Type 'bye' to end my service.");
+        addItems();
     }
 
-    public static void echo() {
-        String input = "";
+    //The function 'echo' is used to print any output to the user
+    public static void echo(String input, String[] items) {
+        if(!input.equals("list")) {
+            System.out.printf((outputFormat) + "%n", "New task added: " + input);
+        } else {
+            System.out.println("    ____________________________________________________________");
+            int index=0;
+            for(String item : items){
+                if((item != null)) {
+                    System.out.println("    "+ ++index + ". " + item);
+                }
+            }
+            System.out.println("    ____________________________________________________________");
+        }
+    }
+
+    //The function 'addItems' is used to accept user's input and add it to their To Do list
+    public static void addItems() {
         Scanner in = new Scanner(System.in);
-        String outputFormat = "    ____________________________________________________________\n" +
-                "    %s\n    ____________________________________________________________";
+        String input = "";
+
+        String[] items = new String[100];
+        int index = 0;
 
         while(!(input = in.nextLine()).equals("bye")){
-            System.out.printf((outputFormat) + "%n", input);
+            if(!input.equals("list")){
+                items[index++] = input;
+            }
+            echo(input, items);
         }
 
-        System.out.printf((outputFormat) + "%n", "Bye. Hope to see you again soon!");
+        System.out.printf((outputFormat) + "%n", "have a great day ahead!");
     }
 }
