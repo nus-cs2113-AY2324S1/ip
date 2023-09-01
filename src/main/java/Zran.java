b7031b081fd8178e0cb9a0771467ba8dcfb3533e
NaychiMin <naychimin02@gmail.com>
2023-09-01 11:18:22 +0800

    Add a new function 'echo'
    The function echos inputs entered by the user and exits when the user inputs 'bye'.

--- src/main/java/Zran.java
+++ src/main/java/Zran.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Zran {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Zran!");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Zran!");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        echo();
+    }
+
+    public static void echo() {
+        String input = "";
+        Scanner in = new Scanner(System.in);
+        String outputFormat = "    ____________________________________________________________\n" +
+                "    %s\n    ____________________________________________________________";
+
+        while(!(input = in.nextLine()).equals("bye")){
+            System.out.printf((outputFormat) + "%n", input);
+        }
+
+        System.out.printf((outputFormat) + "%n", "Bye. Hope to see you again soon!");
     }
 }
