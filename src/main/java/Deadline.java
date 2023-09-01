8d21e54183f15e6ead233cf8e8f66042ab12fccd
Ethan <ethanhuang03@gmail.com>
2023-09-02 03:45:06 +0800

    Add functionality for different types of Tasks

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,16 @@
+public class Deadline extends Task {
+	protected String due;
+	
+	public Deadline(String description, String due) {
+		super(description);
+		this.due = due;
+	}
+	
+	public String getDue() {
+		return due;
+	}
+	
+	public String toString() {
+		return "[D]" + super.toString() + "(by: " + due + ")";
+	}
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -12,18 +13,43 @@ public class Duke {
         while (!command.equals("bye")) {
         	if (command.equals("list")) {
         		printToDoList();
-        	} else if (command.contains("mark")) {
+        	} else {
         		String[] mark = command.split(" ");
-        		int index = Integer.parseInt(mark[1]) - 1;
+        		if (mark[0].contains("mark")) {
+            		int index = Integer.parseInt(mark[1]) - 1;
+        			if (mark[0].equals("mark")) {
+            			toDoList[index].setDone(true);
+            		} else {
+            			toDoList[index].setDone(false);
+            		}
+        		} else if (mark[0].equals("todo")) {
+        			String description = String.join(" ", Arrays.copyOfRange(mark, 1, mark.length));
+        			Task task = new ToDo(description);
+        			addTask(task);
+        		} else if (mark[0].equals("deadline")) {
+        			String[] descAndDue = getDeadlineDescription(mark);
+        			Task task = new Deadline(descAndDue[0], descAndDue[1]);
+        			addTask(task);
+        		} else if (mark[0].equals("event")) {
+        			String[] info = getEventDescription(mark);
+        			Task task = new Event(info[0], info[1], info[2]);
+        			addTask(task);
+        		} else {
+        			System.out.println("This was your command: " + command);
+        		}
+        	}
+        		/**if (command.contains("mark")) {
+        		String[] mark = command.split(" ");
         		if (mark[0].equals("mark")) {
         			toDoList[index].setDone(true);
         		} else {
         			toDoList[index].setDone(false);
         		}
-        	} else {
-        		toDoList[size++] = new Task(command);
-        		respond(" added: " + command);
-        	}
+        	} else if (command.contains("todo")) {
+        		String[] mark = command.split(" ");
+        		//Task task = new Task(command);
+        		//toDoList[size++] = task;
+        		//addTask(task);**/
         	command = input.nextLine();
         }
         sayGoodbye();
@@ -49,9 +75,10 @@ public class Duke {
     	System.out.println("    ____________________________________________________________");
     }
     
-    public static void respond(String response) {
+    public static void addTask(Task task) {
+    	toDoList[size++] = task;
     	printLines();
-    	System.out.println("    " + response);
+    	System.out.println("     Got it. I've added this task:\n       " + task.toString() + "\n     Now you have " + size + " tasks in the list.");
     	printLines();
     	System.out.println();
     }
@@ -63,12 +90,44 @@ public class Duke {
     }
     
     public static void printToDoList() {
-    	
     	printLines();
     	System.out.println("     " + "Here's your tasks:");
     	for (int i = 0; i < size; i++) {
     		System.out.println("     " + Integer.toString(i + 1) + "." + toDoList[i]);
     	}
     	printLines();
+    }
+    
+    public static String[] getDeadlineDescription(String[] description) {
+    	String due = "";
+    	String desc = "";
+    	for (int i = 1; i < description.length; i++) {
+    		if (description[i].equals("/by")) {
+    			due += String.join(" ", Arrays.copyOfRange(description, i + 1, description.length));
+    			break;
+    		} else {
+    			desc += description[i] + " ";
+    		}
+    	}
+    	String[] descAndDue = {desc, due};
+    	return descAndDue;
+    }
+    
+    public static String[] getEventDescription(String[] description) {
+    	int fromIndex = 0;
+    	int toIndex = 0;
+    	for (int i = 0; i < description.length; i++) {
+    		if (description[i].equals("/from")) {
+    			fromIndex = i;
+    		} else if (description[i].equals("/to")) {
+    			toIndex = i;
+    			break;
+    		}
+    	}
+    	String desc = String.join(" ", Arrays.copyOfRange(description, 1, fromIndex));
+    	String from = String.join(" ", Arrays.copyOfRange(description, fromIndex + 1, toIndex));
+    	String to = String.join(" ", Arrays.copyOfRange(description, toIndex + 1, description.length));
+    	String[] info = {desc, from, to};
+    	return info;
     }
 }
