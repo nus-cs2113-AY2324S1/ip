2df45b6be0d8415646f1d7953378b2bc614194e9
Faris Sirraj <farish.sirraj@gmail.com>
2023-09-01 12:19:34 +0800

    Add indentations to output messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 public class Duke {
 
     public static void exit(){
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 
     public static void echo(String input){
-        System.out.println("____________________________________________________________");
-        System.out.println(input);
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    "+input);
+        System.out.println("    ____________________________________________________________");
     }
     public static void main(String[] args) {
         String logo = "______       _     _\n"
@@ -21,11 +21,11 @@ public class Duke {
                 + "\\____/ \\___/|_.__/|_.__/ \\__, |\n"
             + "                          __/ |\n"
             + "                         |___/";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Bobby");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("    Hello from\n" + logo);
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Bobby");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
         while (true){
