8dff5e96b8d7fb02a34a90d36863858ab55c5f42
danielpappa <daniel.pappalardo@icloud.com>
2023-09-01 13:20:54 +0800

    Edit task class to obtain correct description and implement overview of number of total tasks

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,11 @@
 public class Task {
     private String description;
     private boolean isDone;
-
+    private static int numberOfTotalTasks = 0;
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        numberOfTotalTasks++;
     }
 
     public char getStatusIcon() {
@@ -16,13 +17,24 @@ public class Task {
     public void markTaskAsNotDone() {
         isDone = false;
     }
-
     public String getDescription() {
-        return description;
+        boolean containsTime = description.contains("/");
+        String commandlessDescription = description.substring(description.indexOf(" "));
+        if (containsTime == true) {
+            return commandlessDescription.substring(0, commandlessDescription.indexOf('/'));
+        } else {
+            return commandlessDescription;
+        }
     }
-
     public boolean isDone() {
         return isDone;
+    }
+    public static int getNumberOfTotalTasks() {
+        return numberOfTotalTasks;
+    }
+    public String toString() {
+        String summary = String.format("[%c] %s", getStatusIcon(), getDescription());
+        return summary;
     }
 }
 
