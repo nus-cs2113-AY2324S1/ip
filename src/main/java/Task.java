0809aa2d7809f90a0e6b2b532e34318e1b76f927
Faris Sirraj <farish.sirraj@gmail.com>
2023-09-01 13:13:39 +0800

    Create new Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,15 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description){
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getStatusIcon(){
+        if (this.isDone){
+            return "[X]";
+        }
+        return "[ ]";
+    }
+}
