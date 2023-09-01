7cad1967ca79fb9c39a10e3e1225ecaa79446116
NeoMinWei <>
2023-09-02 02:43:56 +0800

    Add todo functionality, refactor the printing of Task

--- src/main/java/Neo.java
+++ src/main/java/Neo.java
@@ -7,15 +7,12 @@ public class Neo {
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         Task[] list = new Task[100];
-        for (int i = 0; i < 100; i++) {
-            list[i] = new Task("");
-        }
         while (!line.equals("bye")) {
             if (line.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < Task.getTotalTasks(); i++) {
                     System.out.print((i + 1) + ". ");
-                    System.out.print("[" + list[i].getStatusIcon() + "] ");
-                    System.out.println(list[i].description);
+                    System.out.println(list[i]);
                 }
                 line = in.nextLine();
             } else if (line.startsWith("mark")) {
@@ -23,19 +20,23 @@ public class Neo {
                 int listIndex = Integer.parseInt(words[1]) - 1;
                 list[listIndex].setDone(true);
                 System.out.println("Nice! I've marked this task as done: ");
-                System.out.println("    [X] " + list[listIndex].description);
+                System.out.println("    " + list[listIndex]);
                 line = in.nextLine();
             } else if (line.startsWith("unmark")) {
                 String[] words = line.split(" ");
                 int listIndex = Integer.parseInt(words[1]) - 1;
                 list[listIndex].setDone(false);
                 System.out.println("OK, I've marked this task as not done yet: ");
-                System.out.println("    [ ] " + list[listIndex].description);
+                System.out.println("    " + list[listIndex]);
                 line = in.nextLine();
             } else {
-                list[Task.getTotalTasks()].description = line;
-                Task.incrementTotalTasks();
-                System.out.println("added: " + line);
+                String description = line;
+                if (line.startsWith("todo")) {
+                    description = line.substring(5);
+                }
+                list[Task.getTotalTasks()] = new Todo(description);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("    " + list[Task.getTotalTasks() - 1]);
                 line = in.nextLine();
             }
         }
