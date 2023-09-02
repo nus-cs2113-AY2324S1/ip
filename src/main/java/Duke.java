78de5a19f2a2325e19fc983faad7c09433e9e2ea
SebasFok <sebasfok2001@gmail.com>
2023-09-02 17:44:32 +0800

    Add echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
         String logo = "Simon";
-        System.out.println(line);
-        System.out.println("Hello I'm " + logo);
-        System.out.println("What can I do for you?\n" + line);
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
+        System.out.println("\t" + line);
+        System.out.println("\t" + "Hello I'm " + logo);
+        System.out.println("\t" + "What can I do for you?");
+        System.out.println("\t" + line);
+        String userInput;
+        Scanner in = new Scanner(System.in);
+
+        userInput = in.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println("\t" + line);
+            System.out.println("\t" + userInput);
+            System.out.println("\t" + line);
+            userInput = in.nextLine();
+        }
+        System.out.println("\t" + line);
+        System.out.println("\t" + "Bye. Hope to see you again soon!");
+        System.out.println("\t" + line);
     }
 }
