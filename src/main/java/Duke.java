b7de3451b28c4858abdd63fb3b88687648796b07
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-09-03 00:03:07 +0800

    Implement Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;  // Import the Scanner class
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,7 +10,21 @@ public class Duke {
 
         //Create a greeting message for the user
         System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?"); 
+        System.out.println("What can I do for you?");
+
+        //Get user input
+        Scanner userScan = new Scanner(System.in);  // Create scanner object
+        String userInput = userScan.nextLine();  // Get user input
+
+        //Echo the arguments provided unless it is "bye" which quits the program
+        while(!"bye".equalsIgnoreCase(userInput)){
+            userInput = userScan.nextLine();  // Get user input again
+            System.out.println(userInput); // Print user input
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        
+        //close the scanner
+        userScan.close();
 
     }
 }
