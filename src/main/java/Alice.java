548327f40ed726b1b733931e27e3cd3b3792a8b3
charkty <charkty@gmail.com>
2023-09-02 21:18:58 +0800

    Store text entered by user and display them when requested

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -4,13 +4,25 @@ public class Alice {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
         String helloMessage = " Hi there, I'm Alice, a chatbot!\n What can I do for you?\n";
+        String[] allUserInput = new String[100];
+        int inputCount = 0;
         Scanner in = new Scanner(System.in);
         System.out.println(helloMessage + line);
         String userInput = in.nextLine();
         while (!(userInput.equals("bye"))){
-            System.out.println(" " + userInput + "\n" + line);
+            if (userInput.equals("list")){
+                for (int i=0; i<inputCount; i++){
+                    System.out.printf("%d. %s\n", i+1, allUserInput[i]);
+                }
+                System.out.println(line);
+            } else{
+                allUserInput[inputCount] = userInput;
+                inputCount++;
+                System.out.println(" added: " + userInput + "\n" + line);
+            }
             userInput = in.nextLine();
         }
+
         String byeMessage = " Bye. Hope to see you again soon!\n";
         System.out.println(byeMessage + line);
     }
