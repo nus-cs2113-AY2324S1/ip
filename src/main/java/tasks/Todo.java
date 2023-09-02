6d859dbd5143c55ffe9b94401bab15dbab227c27
TongZhengHong <zhtong315@gmail.com>
2023-09-03 01:56:12 +0800

    Fix Todo toString printing error

--- src/main/java/tasks/Todo.java
+++ src/main/java/tasks/Todo.java
@@ -2,13 +2,13 @@ package tasks;
 
 public class Todo extends Task {
 
-	public Todo(String description) {
-		super(description);
-	}
+    public Todo(String description) {
+        super(description);
+    }
 
     @Override
     public String toString() {
-        return String.format("[T]%s (by: %s)", super.toString());
+        return String.format("[T]%s", super.toString());
     }
-    
+
 }
