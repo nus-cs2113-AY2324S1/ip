6efa73ee388d8edb29ee7901613166071298a517
kai jie <leowkaijie@gmail.com>
2023-09-02 21:30:38 +0800

    Made Torchie a copy-cat chatbot

--- src/main/java/Torchie.java
+++ src/main/java/Torchie.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 public class Torchie {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Torchie!");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Let's play copytorchie today! You say something and I ll copy!");
+
+        String input;
+
+        do {
+            input = scanner.nextLine();
+            if (!input.equals("bye")){
+                System.out.println(input);
+            }
+        } while (!input.equals("bye"));
+
+        System.out.println("Awww bye :( Let's play again soon!");
     }
 }
