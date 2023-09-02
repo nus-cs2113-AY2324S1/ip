60bf8d6a72f66bc2277616b618e6970136cbebdc
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-09-03 00:01:22 +0800

    Implement Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        //Create a greeting message for the user
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?"); 
+
     }
 }
