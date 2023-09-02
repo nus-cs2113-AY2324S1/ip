2cf44fb75b40bba16500f056170ecf035765b9f4
aaronxujiachen <1943999635x@gmail.com>
2023-09-02 18:09:56 +0800

    Tweak the code to comply with coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Duke {
 
+    // Method to draw horizontal lines
     public static void drawLine() {
         for (int i = 0; i < 30; i++) {
             System.out.print("_");
@@ -10,13 +11,16 @@ public class Duke {
         System.out.println();
     }
 
+    // Method to print the logo and introductory message
     public static void hiDude() {
+        // Logo string
         String logo = "###            #        \n"
                 + "#  #           #        \n"
                 + "#  #  #  #   ###   ##   \n"
                 + "#  #  #  #  #  #  # ##  \n"
                 + "#  #  #  #  #  #  ##    \n"
                 + "###    ###   ###   ##   \n";
+
         System.out.println("Hello from\n" + logo);
         drawLine();
         System.out.println("Hello! I'm your best Dude :)");
@@ -24,12 +28,15 @@ public class Duke {
         drawLine();
     }
 
+    // Method to handle the storage of tasks
     public static void storeDude() {
+        // Initialize Scanner and ArrayList for tasks
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
         ArrayList<Task> tasks = new ArrayList<>();
-        int curPos = 0;
+        int curPos = 0; // Variable to keep track of the current task position
 
+        // Main loop to process commands
         while (!(input.isEmpty())) {
             drawLine();
             if (input.equals("bye")) {
@@ -41,22 +48,21 @@ public class Duke {
                     System.out.println((i + 1) + ". " + tasks.get(i).getStatusIcon() + " " + tasks.get(i).description);
                 }
             } else if (input.startsWith("mark") || input.startsWith("unmark")) {
+                // Split the input to separate command and task index
                 String[] arrOfInput = input.split(" ", 2);
                 if (arrOfInput.length < 2) {
                     System.out.println("Please specify the task index.");
                 } else {
                     try {
+                        // Convert user input index to zero-based index for ArrayList
                         int index = Integer.parseInt(arrOfInput[1]) - 1;
                         if (index < 0 || index >= curPos) {
                             System.out.println("Task index out of range.");
                         } else {
-                            if (input.startsWith("mark")) {
-                                tasks.get(index).isDone = true;
-                                System.out.println("Nice! I've marked this task as done:");
-                            } else {
-                                tasks.get(index).isDone = false;
-                                System.out.println("OK, I've marked this task as not done yet:");
-                            }
+                            // Mark or unmark the task
+                            tasks.get(index).isDone = input.startsWith("mark");
+                            String message = input.startsWith("mark") ? "Nice! I've marked this task as done:" : "OK, I've marked this task as not done yet:";
+                            System.out.println(message);
                             System.out.println("   " + tasks.get(index).getStatusIcon() + " " + tasks.get(index).description);
                         }
                     } catch (NumberFormatException e) {
@@ -64,6 +70,7 @@ public class Duke {
                     }
                 }
             } else {
+                // Add new task
                 System.out.println("added: " + input);
                 tasks.add(new Task(input));
                 curPos++;
@@ -71,14 +78,17 @@ public class Duke {
             drawLine();
             input = scan.nextLine();
         }
+        // Close the Scanner to prevent resource leak
         scan.close();
     }
 
+    // Method to print the goodbye message
     public static void byeDude() {
         System.out.println("Bye. Hope to see you again soon!");
         drawLine();
     }
 
+    // Main method
     public static void main(String[] args) {
         hiDude();
         storeDude();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,17 @@
 public class Task {
+    // Instance variables to store task description and status
     protected String description;
-    protected boolean isDone;  // protected => public
+    protected boolean isDone;
 
+    // Constructor to initialize a task with a description
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    // Method to get the status icon based on whether the task is done or not
     public String getStatusIcon() {
-        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+        return (isDone ? "[X]" : "[ ]");
     }
 }
+
