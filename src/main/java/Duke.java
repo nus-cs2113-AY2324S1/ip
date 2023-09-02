26ede5337b908500aa5a17f45611b1373151f1c1
SebasFok <sebasfok2001@gmail.com>
2023-09-02 23:15:38 +0800

    Add 'add' and 'list' behaviour

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,14 +9,29 @@ public class Duke {
         System.out.println("\t" + "What can I do for you?");
         System.out.println("\t" + line);
         String userInput;
-        Scanner in = new Scanner(System.in);
+        String[] userList = new String[100];
+        int listCounter = 0;
 
+        Scanner in = new Scanner(System.in);
         userInput = in.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println("\t" + line);
-            System.out.println("\t" + userInput);
-            System.out.println("\t" + line);
-            userInput = in.nextLine();
+            switch (userInput) {
+            case "list":
+                System.out.println("\t" + line);
+                for (int i = 0; i < listCounter; i++) {
+                    System.out.println("\t" + (i + 1) + ". " + userList[i]);
+                }
+                System.out.println("\t" + line);
+                userInput = in.nextLine();
+                break;
+            default:
+                userList[listCounter] = userInput;
+                listCounter++;
+                System.out.println("\t" + line);
+                System.out.println("\t" + "added: " + userInput);
+                System.out.println("\t" + line);
+                userInput = in.nextLine();
+            }
         }
         System.out.println("\t" + line);
         System.out.println("\t" + "Bye. Hope to see you again soon!");
