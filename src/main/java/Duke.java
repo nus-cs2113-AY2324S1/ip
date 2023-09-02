646e2773b92b82c133a2f061c35dd8cba03d65ac
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-09-03 00:04:30 +0800

    Implement Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,26 @@
 import java.util.Scanner;  // Import the Scanner class
 public class Duke {
+
+    //Create a task class
+    public static class Task {
+        private String taskName;
+        private boolean isDone;
+        public Task(String taskName){
+            this.taskName = taskName;
+            this.isDone = false;
+        }
+        public void markAsDone(){
+            this.isDone = true;
+        }
+        public String toString(){
+            return this.taskName;
+        }
+    }
+
+    //create a list of tasks
+    public static Task[] tasks = new Task[100];
+    public static int taskCount = 0;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -18,8 +39,19 @@ public class Duke {
 
         //Echo the arguments provided unless it is "bye" which quits the program
         while(!"bye".equalsIgnoreCase(userInput)){
+            if("list".equalsIgnoreCase(userInput)){
+                //Print out the list of tasks
+                for(int i=0;i<taskCount;i++){
+                    System.out.println((i+1) + ". " + tasks[i]);
+                }
+                userInput = userScan.nextLine();  // Get user input again
+                continue;
+            }
+            System.out.println("added: " + userInput); // Print user input
+            //add to task list
+            tasks[taskCount]=new Task(userInput);
+            taskCount++;
             userInput = userScan.nextLine();  // Get user input again
-            System.out.println(userInput); // Print user input
         }
         System.out.println("Bye. Hope to see you again soon!");
         
