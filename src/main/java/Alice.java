4e8e5b140d1427fc27dcbbb163b849cb4ac44902
charkty <charkty@gmail.com>
2023-09-02 20:41:11 +0800

    Echo messages from user input

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Alice {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
-        String helloMessage = " Hello! I'm Alice\n What can I do for you?\n";
+        String helloMessage = " Hi there, I'm Alice, a chatbot!\n What can I do for you?\n";
+        Scanner in = new Scanner(System.in);
+        System.out.println(helloMessage + line);
+        String userInput = in.nextLine();
+        while (!(userInput.equals("bye"))){
+            System.out.println(" " + userInput + "\n" + line);
+            userInput = in.nextLine();
+        }
         String byeMessage = " Bye. Hope to see you again soon!\n";
-        System.out.println(line + helloMessage + line + byeMessage + line);
+        System.out.println(byeMessage + line);
     }
 }
