5b4bcb6ceaec7e32fa3ba06508b105e1989982ed
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-09-03 00:05:28 +0800

    Implement Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,12 +9,40 @@ public class Duke {
             this.taskName = taskName;
             this.isDone = false;
         }
+        //mark as done
         public void markAsDone(){
             this.isDone = true;
         }
-        public String toString(){
-            return this.taskName;
+        //mark as not done
+        public void markAsNotDone(){
+            this.isDone = false;
         }
+        //get the status of the task and the task name
+        public String toString(){
+            if(this.isDone){
+                return "[X] " + this.taskName;
+            }else{
+                return "[ ] " + this.taskName;
+            }
+        }
+    }
+    //check if an input has mark as the first word
+    public static boolean isMark(String input){
+        if(input.length()>=4){
+            if("mark".equalsIgnoreCase(input.substring(0,4))){
+                return true;
+            }
+        }
+        return false;
+    }
+    //check if an input has unmark as the first word
+    public static boolean isUnmark(String input){
+        if(input.length()>=6){
+            if("unmark".equalsIgnoreCase(input.substring(0,6))){
+                return true;
+            }
+        }
+        return false;
     }
 
     //create a list of tasks
@@ -41,17 +69,39 @@ public class Duke {
         while(!"bye".equalsIgnoreCase(userInput)){
             if("list".equalsIgnoreCase(userInput)){
                 //Print out the list of tasks
+                System.out.println("Here are the tasks in your list:");
                 for(int i=0;i<taskCount;i++){
                     System.out.println((i+1) + ". " + tasks[i]);
                 }
                 userInput = userScan.nextLine();  // Get user input again
-                continue;
             }
-            System.out.println("added: " + userInput); // Print user input
-            //add to task list
-            tasks[taskCount]=new Task(userInput);
-            taskCount++;
-            userInput = userScan.nextLine();  // Get user input again
+            else if(isUnmark(userInput)){
+                //get the task number
+                int taskNumber = Integer.parseInt(userInput.substring(7));
+                //mark the task as done
+                tasks[taskNumber-1].markAsNotDone();
+                //print out the task that was marked as done
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[taskNumber-1]);
+                userInput = userScan.nextLine();  // Get user input again              
+            }
+            else if(isMark(userInput)){
+                //get the task number
+                int taskNumber = Integer.parseInt(userInput.substring(5));
+                //mark the task as done
+                tasks[taskNumber-1].markAsDone();
+                //print out the task that was marked as done
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[taskNumber-1]);
+                userInput = userScan.nextLine();  // Get user input again              
+            }
+            else{
+                System.out.println("added: " + userInput); // Print user input
+                //add to task list
+                tasks[taskCount]=new Task(userInput);
+                taskCount++;
+                userInput = userScan.nextLine();  // Get user input again
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
         
