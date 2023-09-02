c69b24afe97ec95c03a0ffb33b286db95cb817a6
SebasFok <sebasfok2001@gmail.com>
2023-09-03 04:07:55 +0800

    Add 'Mark as Done' behaviour

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,24 +9,41 @@ public class Duke {
         System.out.println("\t" + "What can I do for you?");
         System.out.println("\t" + line);
         String userInput;
-        String[] userList = new String[100];
-        int listCounter = 0;
+        Task[] tasks = new Task[100];
 
         Scanner in = new Scanner(System.in);
         userInput = in.nextLine();
         while (!userInput.equals("bye")) {
-            switch (userInput) {
-            case "list":
+            if (userInput.equals("list")) {
                 System.out.println("\t" + line);
-                for (int i = 0; i < listCounter; i++) {
-                    System.out.println("\t" + (i + 1) + ". " + userList[i]);
+                System.out.println("\tHere are the tasks in your list:");
+                for (int i = 0; i < Task.getNumberOfTask(); i++) {
+                    System.out.print("\t" + (i + 1) + ".[" + tasks[i].getStatusIcon() + "]");
+                    System.out.println(" " + tasks[i].getDescription());
                 }
                 System.out.println("\t" + line);
                 userInput = in.nextLine();
-                break;
-            default:
-                userList[listCounter] = userInput;
-                listCounter++;
+            }
+            else if (userInput.startsWith("mark ")) {
+                int target = Integer.parseInt(userInput.substring(5)) - 1;
+                tasks[target].markAsDone();
+                System.out.println("\t" + line);
+                System.out.println("\tNice! I've marked this task as done:");
+                System.out.println("\t  [X] " + tasks[target].getDescription());
+                System.out.println("\t" + line);
+                userInput = in.nextLine();
+            }
+            else if (userInput.startsWith("unmark ")) {
+                int target = Integer.parseInt(userInput.substring(7)) - 1;
+                tasks[target].unmarkAsDone();
+                System.out.println("\t" + line);
+                System.out.println("\tOkay, I've marked this task as not done yet:");
+                System.out.println("\t  [] " + tasks[target].getDescription());
+                System.out.println("\t" + line);
+                userInput = in.nextLine();
+            }
+            else {
+                tasks[Task.getNumberOfTask()] = new Task(userInput);
                 System.out.println("\t" + line);
                 System.out.println("\t" + "added: " + userInput);
                 System.out.println("\t" + line);
