7e30d2db82f23ca255409910e662f8c7738c894d
wwweert123 <puachengxuan.fp@gmail.com>
2023-09-03 00:43:15 +0800

    Add Deadline and Event class
    Deadlines are tasks that need to be done before a specific date/time while Events are tasks that start at a specific date/time and ends at a specific date/time

---
+++ src/main/java/Event.java
@@ -0,0 +1,30 @@
+public class Event extends Task {
+    protected String start;
+    protected String end;
+    public Event(String description, String start, String end) {
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + this.start + " to: "+ this.end + ")";
+    }
+}
