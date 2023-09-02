9e2f501117e4f59a9dff33bea02f46aa7ee66f62
SebasFok <sebasfok2001@gmail.com>
2023-09-03 04:06:53 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,35 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    private int id;
+    private static int numberOfTask = 0;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        this.id = numberOfTask + 1;
+        numberOfTask++;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getId() {
+        return id;
+    }
+    public boolean getStatus() {
+        return isDone;
+    }
+    public void markAsDone() {
+        isDone = true;
+    }
+    public void unmarkAsDone() {
+        isDone = false;
+    }
+    public static int getNumberOfTask() {
+        return numberOfTask;
+    }
+}
