73ee5636cd96894e60aa1007322935d9ff0c3e01
TongZhengHong <zhtong315@gmail.com>
2023-09-03 02:01:28 +0800

    Add printing when adding tasks

--- src/main/java/dawson/TaskList.java
+++ src/main/java/dawson/TaskList.java
@@ -17,6 +17,13 @@ public class TaskList {
     public void add(Task task) {
         taskList[size] = task;
         size++;
+
+        String[] addText = {
+                "Got it. I've added this task:",
+                "  " + task.toString(),
+                String.format("Now you have %d tasks in the list.", size)
+        };
+        Dawson.printText(addText);
     }
 
     public boolean isIndexValid(int index) {
@@ -24,25 +31,29 @@ public class TaskList {
     }
 
     public String markAsDoneIndex(int index) {
-        if (!isIndexValid(index)) return "";
+        if (!isIndexValid(index)) {
+            return "";
+        }
         taskList[index].markAsDone();
         return taskList[index].toString();
     }
 
     public String unmarkIndex(int index) {
-        if (!isIndexValid(index)) return "";
+        if (!isIndexValid(index)) {
+            return "";
+        }
         taskList[index].unmark();
         return taskList[index].toString();
     }
 
     public String[] getTaskStrings() {
         if (size == 0) {
-            return new String[]{"Empty list!"};
+            return new String[] { "Empty list!" };
         }
 
         List<String> result = new ArrayList<String>();
         result.add("Here are the tasks in your list: ");
-        
+
         for (int i = 0; i < size; i++) {
             String line = String.format("%d. %s", i + 1, taskList[i]);
             result.add(line);
