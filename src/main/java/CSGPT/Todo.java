ad9f51431f28e996ae0330ca848a0a1e8a852689
mcmc101001 <tohmingchun101001@gmail.com>
2023-09-02 17:16:48 +0800

    Add Todo class to git

---
+++ src/main/java/CSGPT/Todo.java
@@ -0,0 +1,12 @@
+package CSGPT;
+
+public class Todo extends Task {
+    public Todo (String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
