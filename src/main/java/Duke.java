5a5459c743e9897e3e3f1d83dea052ae2383dc5d
KenCheung18 <kycheungbf@connect.ust.hk>
2023-09-03 21:38:11 +0800

    Change from array to class Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,34 +5,32 @@ public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println(" Hello! I'm Bot Hilary ");
-        String[] list = new String[100];
-        boolean[] isDone = new boolean[100];
-        int listCount = 0;
+        Task[] list = new Task[100];
         System.out.println(" What can I do for you? ");
         String line = in.nextLine();
         while (!line.equals("bye")){
             if (line.equals("list")) {
-                for (int i = 0; i < listCount; ++i) {
+                for (int i = 0; i < Task.listCount; ++i) {
                     System.out.print(i + 1 + ". [");
-                    System.out.println(((isDone[i]) ? "X" : " " )+ "]" + list[i]);
+                    System.out.println(list[i].getStatusIcon() + "]" + list[i].getDescription());
                 }
             }
             else if (line.contains("unmark")){
                 line = line.replace("unmark ", "");
-                isDone[Integer.parseInt(line) - 1] = false;
+                list[Integer.parseInt(line) - 1].unmark();
                 System.out.println(" OK, I've marked this task as not done yet:");
-                System.out.println("   [ ]" + list[Integer.parseInt(line) - 1]);
+                System.out.println("   [ ]" + list[Integer.parseInt(line) - 1].getDescription());
             }
             else if (line.contains("mark")){
                 line = line.replace("mark ", "");
-                isDone[Integer.parseInt(line) - 1] = true;
+                list[Integer.parseInt(line) - 1].markAsDone();
                 System.out.println(" Nice! I've marked this task as done:");
-                System.out.println("   [X]" + list[Integer.parseInt(line) - 1]);
+                System.out.println("   [X]" + list[Integer.parseInt(line) - 1].getDescription());
             }
-            else {
-                list[listCount] = line;
-                listCount++;
-                System.out.println("Added: " + line);
+            else{
+                list[Task.listCount] = new Task(line);
+                Task.listCount++;
+                System.out.println("added: " + line);
             }
             line = in.nextLine();
         }
