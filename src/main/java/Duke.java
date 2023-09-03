5503b66cec2aea4b24574505f5bd2f099779cf2a
tangzhenen <tangzhenen2000@gmail.com>
2023-09-03 17:18:44 +0800

    Gave chatbot a new name Implement an initial skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printLines(30,'-');
+        System.out.println("Hello! I'm En\nWhat can i do for you?");
+        printLines(30,'-');
+        System.out.println("Bye! Have a nice day");
+        printLines(30,'-');
+   }
+    public static void printLines(int l, char c){
+        for (int i = 0; i < l; i++){
+            System.out.print(c);
+        }
+        System.out.println();
     }
 }
