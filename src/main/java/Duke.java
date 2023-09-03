969ba649918c94c1f851d3dd85951df86f3a1d8d
tangzhenen <tangzhenen2000@gmail.com>
2023-09-03 18:03:09 +0800

    Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
@@ -8,6 +10,8 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         String userInput;
+        List<String> userInputs = new ArrayList<>();
+        int inputCount = 0;
 
         while (true) {
             System.out.print("You: ");
@@ -18,11 +22,20 @@ public class Duke {
                 System.out.println("Bye! Have a nice day.");
                 printLines(30,'-');
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                printLines(30, '-');
+                for (int i = 0; i < inputCount; i++) {
+                    System.out.println((i + 1) + "." + userInputs.get(i));
+                }
+                printLines(30,'-');
             } else {
-                System.out.println("En: " + userInput);
+                inputCount ++;
+                userInputs.add(userInput);
+                System.out.println("Added:" + userInput);
                 printLines(30,'-');
             }
         }
+        scanner.close();
    }
     public static void printLines(int l, char c){
         for (int i = 0; i < l; i++){
