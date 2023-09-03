7a7066d49ab828c0ea359b7ac3c0ca81c40762cd
Dexter Hoon <e0727166@u.nus.edu>
2023-09-04 00:06:58 +0800

    Update Alan.java to follow coding standard

--- src/main/java/Alan.java
+++ src/main/java/Alan.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Alan {
 
-    public static void printGreet() {
+    public static void printGreetingMessage() {
         printHorizontalLine();
         String man = " @/\n" +
                      "/| \n" +
@@ -14,7 +14,7 @@ public class Alan {
         printHorizontalLine();
     }
 
-    public static void printExit() {
+    public static void printExitMessage() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
@@ -23,10 +23,10 @@ public class Alan {
     }
 
     public static void main(String[] args) {
-        Task[] taskList = new Task[101];
-        int currentTaskListIndex = 1;
+        Task[] tasks = new Task[101];
+        int currentTasksIndex = 1;
 
-        printGreet();
+        printGreetingMessage();
 
         String userInput = " ";
         Scanner in = new Scanner(System.in);
@@ -37,35 +37,40 @@ public class Alan {
             userInput = in.nextLine();
 
             printHorizontalLine();
+
             if (userInput.equals("bye")) {
-                printExit();
-            } else if (userInput.equals("list")) {     //print the tasks in the lists
+                printExitMessage();
+            } else if (userInput.equals("list")) {
+                //print the tasks in the lists
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 1; i < currentTaskListIndex; i++) {
-                    System.out.println((i) + ". [" + taskList[i].getStatusIcon() + "] " + taskList[i].getDescription());
+                for (int i = 1; i < currentTasksIndex; i++) {
+                    System.out.println((i) + ". [" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
-            } else if (userInput.startsWith("mark")) {  //mark tasks as done
+            } else if (userInput.startsWith("mark")) {
+                //mark tasks as done
                 String[] words = userInput.split(" ");
 
-                taskList[Integer.parseInt(words[1])].setDone(true);
+                tasks[Integer.parseInt(words[1])].setDone(true);
 
                 System.out.println("Alright! I've marked this task as done:");
-                System.out.println("\t[" + taskList[Integer.parseInt(words[1])].getStatusIcon() + "] " +
-                        taskList[Integer.parseInt(words[1])].getDescription());
+                System.out.println("\t[" + tasks[Integer.parseInt(words[1])].getStatusIcon() + "] " +
+                        tasks[Integer.parseInt(words[1])].getDescription());
 
-            } else if (userInput.startsWith("unmark")) { //unmark tasks as undone
+            } else if (userInput.startsWith("unmark")) {
+                //unmark tasks as undone
                 String[] words = userInput.split(" ");
 
-                taskList[Integer.parseInt(words[1])].setDone(false);
+                tasks[Integer.parseInt(words[1])].setDone(false);
 
                 System.out.println("Ok, I've marked this task as not done yet:");
-                System.out.println("\t[" + taskList[Integer.parseInt(words[1])].getStatusIcon() + "] " +
-                        taskList[Integer.parseInt(words[1])].getDescription());
+                System.out.println("\t[" + tasks[Integer.parseInt(words[1])].getStatusIcon() + "] " +
+                        tasks[Integer.parseInt(words[1])].getDescription());
 
-            } else {    //add a new task to the list
+            } else {
+                //add a new task to the list
                 Task task = new Task(userInput);
-                taskList[currentTaskListIndex] = task;
-                currentTaskListIndex++;
+                tasks[currentTasksIndex] = task;
+                currentTasksIndex++;
                 System.out.println("added: " + userInput);
             }
 
