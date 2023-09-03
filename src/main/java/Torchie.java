63f50a8a8d934b71a0d52107939e54f557da4829
kai jie <leowkaijie@gmail.com>
2023-09-03 23:56:35 +0800

    Add Task class to allow tasks to be marked as done and undone

--- src/main/java/Torchie.java
+++ src/main/java/Torchie.java
@@ -2,45 +2,82 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Torchie {
 
-    private static String[] textStore = new String[100];
-    private static int textStoreSpace = 0; // keep track of next null space in array
+    private static Task[] taskStore = new Task[100];
+    private static int taskStoreSpace = 0; // keep track of next null space in array
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello! I'm Torchie!");
         System.out.println("What can I do for you?");
-//        System.out.println("Let's play copytorchie today! You say something and I ll copy!");
         System.out.println("Let's play storetorchie today! You say something and I ll store it!");
 
         String input;
-
         do {
             input = scanner.nextLine();
-            if (input.equals("list")){
-                String[] list = Arrays.copyOf(getTextStore(),textStoreSpace);
-                showList(list);
-            }else if (!input.equals("bye")){
-                setTextStore(input);
+            String firstWord = input.split(" ")[0];
+
+            switch(firstWord) {
+            case "list":
+                showList(taskStore);
+                break;
+            case "mark":
+                taskStore = markItem(taskStore, Integer.parseInt(input.split(" ")[1]));
+                break;
+            case "unmark":
+                taskStore = unmarkItem(taskStore, Integer.parseInt(input.split(" ")[1]));
+                break;
+            case "bye":
+                System.out.println("Awww bye :( Let's play again soon!");
+                break;
+            default:
+                Task t = new Task(input);
+                setTaskStore(t);
                 System.out.println("added: " + input);
             }
+
         } while (!input.equals("bye"));
 
-        System.out.println("Awww bye :( Let's play again soon!");
     }
 
-    public static String[] getTextStore() {
-        return textStore;
+    public static String[] getTaskStore() {
+        String[] taskListString  = new String[taskStore.length];
+        for (int i=0; i< taskStore.length; i++){
+            taskListString[i] = taskStore[i].toString();
+        }
+        return taskListString;
     }
 
-    public static void setTextStore(String text) {
-        textStore[textStoreSpace] = text;
-        textStoreSpace += 1;
+    public static void setTaskStore(Task t) {
+        taskStore[taskStoreSpace] = t;
+        taskStoreSpace += 1;
     }
 
-    public static void showList(String[] list){
-        // list is an array containing no null items
+    public static void showList(Task[] list){
+        System.out.println("Here are the tasks in your list: ");
         for (int i=0; i<list.length; i++){
-            System.out.println(( (i+1) + ". " + list[i]));
+            if (list[i] == null){
+                // only print valid tasks
+                break;
+            }
+            System.out.println(( (i+1) + ".[" + list[i].getStatusIcon() + "] " + list[i].description));
         }
     }
+
+    public static Task[] markItem(Task[] list, int itemNum){
+        int index = itemNum - 1; // obtain index of item to mark
+        list[index].isDone = true;
+        System.out.println("Nice! I've marked this task as done: ");
+        list[index].printTask();
+        return list;
+    }
+
+    public static Task[] unmarkItem(Task[] list, int itemNum){
+        int index = itemNum - 1; // obtain index of item to unmark
+        list[index].isDone = false;
+        System.out.println("Ok, I've marked this task as not done yet: ");
+        list[index].printTask();
+        return list;
+    }
+
+
 }
