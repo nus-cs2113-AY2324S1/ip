cf312bea9d8bca091065edef4c767c25aefb8816
NgLixuanNixon <e0726667@u.nus.edu>
2023-09-04 21:21:14 +0800

    Level 0 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String prompt = "___________________________________\n"
+                +  "Hello! I'm NOXIN \n"
+                + "What can I do for you?\n"
+                + "___________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "___________________________________\n";
+
+        System.out.println(prompt);
     }
 }
