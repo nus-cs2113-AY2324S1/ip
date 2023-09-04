332b3f4c8a83495047bf2eb6784a3c4e1c10e292
nihalzp <81457724+nihalzp@users.noreply.github.com>
2023-09-04 20:51:19 +0800

    Add Python ASCII art

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,14 @@ public class Duke {
     final private static String BOT_NAME = "Python";
     final private static int HORIZONTAL_LINE_LENGTH = 80;
 
+    final private static String pythonASCIIArt =
+            "\t ____        _   _                 \n" +
+            "\t|  _ \\ _   _| |_| |__   ___  _ __  \n" +
+            "\t| |_) | | | | __|  _ \\ / _ \\|  _ \\ \n" +
+            "\t|  __/| |_| | |_| | | | (_) | | | |\n" +
+            "\t|_|    \\__, |\\__|_| |_|\\___/|_| |_|\n" +
+            "\t       |___/                        ";
+
     final private static Scanner in = new Scanner(System.in);
 
     private static void printHorizontalLine() {
@@ -14,6 +22,7 @@ public class Duke {
     }
     
     public static void main(String[] args) {
+        System.out.println(pythonASCIIArt);
         printHorizontalLine();
         System.out.printf("\tHello! I am a short Java Bot %s!\n", BOT_NAME);
         System.out.println("\tWhat can I do for you?");
