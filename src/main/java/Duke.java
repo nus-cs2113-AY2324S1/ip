54224dbc11e1fe45bf9010544f90bccfbcbb961b
Cheezeblokz <jingxi2001@gmail.com>
2023-09-05 00:21:40 +0800

    - Added ASCII art for robot name - Added and personalised robot prompt statements - Added functionality of echoing user input - Added exit program when given command 'bye'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String name = "Cheezeblokz";
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm "+ name + ".\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        partition();
+        String name = " _____ _                       _     _       _         \n" +
+                "/  __ \\ |                     | |   | |     | |        \n" +
+                "| /  \\/ |__   ___  ___ _______| |__ | | ___ | | __ ____\n" +
+                "| |   | '_ \\ / _ \\/ _ \\_  / _ \\ '_ \\| |/ _ \\| |/ /|_  /\n" +
+                "| \\__/\\ | | |  __/  __// /  __/ |_) | | (_) |   <  / / \n" +
+                " \\____/_| |_|\\___|\\___/___\\___|_.__/|_|\\___/|_|\\_\\/___|";
+
+        System.out.print("Hello! My name is:\n" + name + "\n\n" +
+                "What can I do for you today? :)\n" + ">>");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        while(!userInput.equalsIgnoreCase("bye"))
+        {
+            System.out.println(userInput);
+            partition();
+
+            System.out.print("What do you want to do next? :o\n" + ">>");
+            userInput = scanner.nextLine();
+        }
+        System.out.println(" Bye. Hope to see you again soon! :D");
+        partition();
+    }
+
+    private static void partition(){
+        System.out.println("____________________________________________________________");
     }
 }
