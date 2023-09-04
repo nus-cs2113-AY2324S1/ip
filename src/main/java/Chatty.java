082a01e74bc8319c9302f35ad4d1fb1524c5a36e
bnjm2000 <bnjm2000@gmail.com>
2023-09-04 19:10:49 +0800

    Add ToDos, Events, Deadlines Change if to switch statements

--- src/main/java/Chatty.java
+++ src/main/java/Chatty.java
@@ -1,15 +1,6 @@
 import java.util.Scanner;
 
 public class Chatty {
-    /*public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-    }
-     */
-
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Chatty!\nWhat can I do for you?");
@@ -18,43 +9,74 @@ public class Chatty {
         String input = scanner.nextLine();
         Task[] tasks = new Task[100];
         int taskCount = 0;
-        while (!input.equalsIgnoreCase("bye")){
-            if (input.equalsIgnoreCase("list")) {
+        int index;
+        loop:
+        while (!input.equalsIgnoreCase("bye")) {
+            String[] words = input.split(" ");
+            String command = words[0].toLowerCase();
+            switch (command) {
+            case "list":
                 System.out.println("____________________________________________________________");
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". [" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
+                    System.out.println((i + 1) + ". " + tasks[i].getDescription());
                 }
                 System.out.println("____________________________________________________________\n");
                 input = scanner.nextLine();
-                continue;
-            } else if (input.startsWith("mark")) {
-                int idx=Integer.parseInt(input.substring(5)) - 1;
-                tasks[idx].mark();
+                continue loop;
+            case "mark":
+                index = Integer.parseInt(input.substring(5)) - 1;
+                tasks[index].mark();
                 System.out.println("____________________________________________________________");
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[X] " + tasks[idx].getDescription());
-                System.out.println("____________________________________________________________\n");
-                input = scanner.nextLine();
-                continue;
-            } else if (input.startsWith("unmark")) {
-                int idx=Integer.parseInt(input.substring(7)) - 1;
-                tasks[idx].unmark();
+                System.out.println(tasks[index].getDescription());
+                break;
+            case "unmark":
+                index = Integer.parseInt(input.substring(7)) - 1;
+                tasks[index].unmark();
                 System.out.println("____________________________________________________________");
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("[ ] " + tasks[idx].getDescription());
-                System.out.println("____________________________________________________________\n");
-                input = scanner.nextLine();
-                continue;
-            } else {
+                System.out.println("[ ] " + tasks[index].getDescription());
+                break;
+            case "todo":
+                String todoDescription = input.substring(5);
+                tasks[taskCount] = new Todo(todoDescription);
+                taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println("[T][ ] " + todoDescription);
+                break;
+            case "deadline":
+                int byIndex = input.indexOf("/by");
+                String deadlineDescription = input.substring(9, byIndex).trim();
+                String by = input.substring(byIndex + 3).trim();
+                tasks[taskCount] = new Deadline(deadlineDescription, by);
+                taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println("[D][ ] " + deadlineDescription + " (by: " + by + ")");
+                break;
+            case "event":
+                int fromIndex = input.indexOf("/from");
+                int toIndex = input.indexOf("/to");
+                String eventDescription = input.substring(6, fromIndex).trim();
+                String from = input.substring(fromIndex + 5, toIndex).trim();
+                String to = input.substring(toIndex + 3).trim();
+                tasks[taskCount] = new Event(eventDescription, from, to);
+                taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println("[E][ ] " + eventDescription + " (from: " + from + " to: " + to + ")");
+                break;
+            default:
                 System.out.println("____________________________________________________________");
                 tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
-                System.out.println("____________________________________________________________\n");
-                input = scanner.nextLine();
+                break;
             }
+            System.out.println("Now you have " + taskCount + " tasks in the list.");
+            System.out.println("____________________________________________________________\n");
+            input = scanner.nextLine();
         }
+
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -21,3 +21,57 @@ public class Task {
         return (isDone ? "X" : " ");
     }
 }
+
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String getDescription() {
+        return "[T][" + getStatusIcon() + "] " + super.getDescription();
+    }
+}
+
+class Deadline extends Task {
+    private String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    public String getBy() {
+        return by;
+    }
+
+    @Override
+    public String getDescription() {
+        return "[D][" + getStatusIcon() + "] " + super.getDescription() + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+    private String from;
+    private String to;
+
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    @Override
+    public String getDescription() {
+        return "[E][" + getStatusIcon() + "] " + super.getDescription() + " (from: " + from + " to: " + to + ")";
+    }
+}
