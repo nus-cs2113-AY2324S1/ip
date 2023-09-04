75be7cef568c14b7d116010c030c1e2bf469e7ee
nihalzp <81457724+nihalzp@users.noreply.github.com>
2023-09-04 20:32:00 +0800

    Echo the user given command, exit when "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Duke {
     final private static String BOT_NAME = "Python";
     final private static int HORIZONTAL_LINE_LENGTH = 80;
+
+    final private static Scanner in = new Scanner(System.in);
 
     private static void printHorizontalLine() {
         char[] horizontalLine = new char[HORIZONTAL_LINE_LENGTH];
@@ -15,7 +18,17 @@ public class Duke {
         System.out.printf("Hello! I am a short Java Bot %s!\n", BOT_NAME);
         System.out.println("What can I do for you?");
         printHorizontalLine();
-        System.out.println("Bye. See you again when you run the program again!");
-        printHorizontalLine();
+
+        String inputCommand;
+        do {
+            inputCommand = in.nextLine();
+            printHorizontalLine();
+            if (inputCommand.equals("bye")) {
+                System.out.println("Bye. See you again when you run the program again!");
+            } else {
+                System.out.println(inputCommand);
+            }
+            printHorizontalLine();
+        } while(!inputCommand.equals("bye"));
     }
 }
