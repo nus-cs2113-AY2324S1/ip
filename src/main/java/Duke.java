0cb2e1a2be8e8bd901d96d6c38f57d7ecb284f33
ryanlohyr <ryanloh88@ymail.com>
2023-09-04 16:56:49 +0800

    Add echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
+
         String logoRyan = " ____  _          \n"
                 + "|  _ \\| | ___   __\n"
                 + "| |_) | |/ _ \\ / _|\n"
@@ -13,10 +11,23 @@ public class Duke {
 
         System.out.println("Hello from\n" + logoRyan);
 
+        // String[] storageArray() = new String[100];
+
         String line = "Hello! I'm Ryan Loh \nWhat can I do for you?\n";
 
+        Scanner scanner = new Scanner(System.in);
+
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("Bye")) {
+            System.out.println("      " + userInput);
+            userInput = scanner.nextLine(); 
+        }
         String lineBreak = "------------------------------ \n";
+
         System.out.println(line + lineBreak + "Bye. Hope to see you again soon!\n");
+
+        // Close the scanner when you're done with it
+        scanner.close();
 
     }
 }
