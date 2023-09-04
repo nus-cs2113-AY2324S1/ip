c1290705fe56f96f5111b108f702d36e7cdd120b
barbaracwx <barbara9668@gmail.com>
2023-09-05 03:33:22 +0800

    commit echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                "Hello! I'm Calebra\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Calebra\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+
+
+        Scanner input = new Scanner(System.in);
+        String line = input.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println("____________________________________________________________\n");
+            System.out.println(line);
+            System.out.println("____________________________________________________________\n");
+            line = input.nextLine();
+        }
+
+        // When the user types "bye," exit the loop and display the goodbye message
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye! Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
     }
 }
