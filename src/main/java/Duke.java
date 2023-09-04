a4564e4f57e952f108d309d08b125152f9243679
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-09-04 18:57:08 +0800

    rename "Duke" to "Luke", and added greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /* String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n"; */
+        String logo = " _           _        \n"
+                + "| |    _   _| | _____ \n"
+                + "| |   | | | | |/ / _ \\\n"
+                + "| |___| |_| |   <  __/\n"
+                + "|_____|\\__,_|_|\\_\\___|\n";
+        System.out.println("Hello! I'm\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
