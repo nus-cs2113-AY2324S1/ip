860d8788728b2d3dabc1f8e7636e765eaccda6c3
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-09-04 21:58:48 +0800

    add echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        /* String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n"; */
         String logo = " _           _        \n"
                 + "| |    _   _| | _____ \n"
                 + "| |   | | | | |/ / _ \\\n"
@@ -12,6 +9,17 @@ public class Duke {
                 + "|_____|\\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm\n" + logo);
         System.out.println("What can I do for you?");
+
+        Scanner in = new Scanner(System.in);
+
+        String echo = in.nextLine();
+        while (!echo.equals("bye")) {
+            System.out.println(echo);
+            echo = in.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+        
+        in.close();
     }
 }
