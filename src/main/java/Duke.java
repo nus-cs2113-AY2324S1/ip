3015c0c66abaa07acf1c910baf8cc70628f3bd08
Lim Yuh Ching <e0726103@u.nus.edu>
2023-09-05 20:47:19 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String intro = "Hello! I'm Wenny!\n"
                         + "How may I help you?\n";
         System.out.println(intro);
-
-        bye();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            System.out.println(userInput);
+            if (userInput.equals("bye")) {
+                bye();
+            }
+        }
     }
     public static void bye() {
         System.out.println("Bye. Hope to see you again soon!");
+        System.exit(0);
     }
 }
