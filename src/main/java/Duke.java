32416b31f8f39b082563e81edd237c199822d90d
choonkit <choonkitong@u.nus.edu>
2023-09-05 16:59:54 +0800

    Edit bot to Add + List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -12,15 +13,24 @@ public class Duke {
         System.out.println(logo);
         System.out.println("Hello! I'm Jake");
         System.out.println("What can I do for you?");
-        // Bot Echo
+        // Bot Read + Add + List
         Scanner readIn = new Scanner(System.in);
         String userInput = "";
+        ArrayList<String> botMemory = new ArrayList<String>();
         while (true) {
+            System.out.print("User: ");
             userInput = readIn.nextLine();
             if (userInput.equalsIgnoreCase("bye")) {
                 break;
             }
-            System.out.println(userInput);
+            if (userInput.equalsIgnoreCase("list")) {
+                for (int i = 0; i < botMemory.size(); i++) {
+                    System.out.println(i + 1 + ". " + botMemory.get(i));
+                  }
+                continue;
+            }
+            botMemory.add(userInput);
+            System.out.println("added: " + userInput);
         }
         // Bot Exit 
         System.out.println("Bye. Hope to see you again soon!");
