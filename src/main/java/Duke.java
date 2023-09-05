8e0282308b1369693157aed36676b840eaf6547f
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-09-05 23:32:47 +0800

    add feature (mark as done)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,18 +10,32 @@ public class Duke {
         System.out.println("Hello! I'm\n" + logo);
         System.out.println("What can I do for you?");
 
-        String[] theList = new String[100];
+        Task[] theList = new Task[100];
         Scanner in = new Scanner(System.in);
         int i = 0;
 
         String echo = in.nextLine();
+
         while (!echo.equals("bye")) {
+            String[] words = echo.split(" "); //to identify usage of features "mark" & "unmark"
             if (echo.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int j = 0; j < i; j += 1) {
-                    System.out.println((j + 1) + ". " + theList[j]);
+                    System.out.print((j + 1) + ".");
+                    theList[j].printTask();
                 }
+            } else if (words[0].equals("mark")) {
+                int taskNumber = Integer.parseInt(words[1]) - 1;
+                System.out.println("Woohoo! You have accomplished:");
+                theList[taskNumber].markAsDone();
+                theList[taskNumber].printTask();
+            } else if (words[0].equals("unmark")) {
+                int taskNumber = Integer.parseInt(words[1]) - 1;
+                System.out.println("HA! You still have to complete:");
+                theList[taskNumber].markAsIncomplete();
+                theList[taskNumber].printTask();
             } else {
-                theList[i] = echo;
+                theList[i] = new Task(echo);
                 System.out.println("added: " + echo);
                 i += 1;
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected String taskName;
+    protected boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public void printTask() {
+        System.out.println("[" + getStatusIcon() + "] " + taskName);
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsIncomplete() {
+        isDone = false;
+    }
+
+    //...
+}
