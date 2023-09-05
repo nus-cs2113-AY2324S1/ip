98adbaefc17fb24d6e49f5076973fcb5b3d7fa25
choonkit <choonkitong@u.nus.edu>
2023-09-05 16:33:31 +0800

    Echo bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "       _       _        \n"
@@ -6,9 +8,22 @@ public class Duke {
                 + "  _   | |/ _` | |/ / _ \\\n"
                 + " | |__| | (_| |   <  __/\n"
                 + "  \\____/ \\__,_|_|\\_\\___|\n";
+        // Bot Greet
         System.out.println(logo);
         System.out.println("Hello! I'm Jake");
         System.out.println("What can I do for you?");
+        // Bot Echo
+        Scanner readIn = new Scanner(System.in);
+        String userInput = "";
+        while (true) {
+            userInput = readIn.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println(userInput);
+        }
+        // Bot Exit 
         System.out.println("Bye. Hope to see you again soon!");
+        readIn.close();
     }
 }
