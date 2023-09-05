f21bf134b9d9e6ddc462559bab3df1c91b589e92
Lim Yuh Ching <e0726103@u.nus.edu>
2023-09-05 21:25:28 +0800

    Renamed Duke. Added greetings.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        // Greetings
         String intro = "Hello! I'm Wenny!\n"
                         + "How may I help you?\n";
         System.out.println(intro);
+        List list = new List();
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String userInput = scanner.nextLine();
-            System.out.println(userInput);
             if (userInput.equals("bye")) {
                 bye();
+            } else if (userInput.equals("list")) {
+                list.print_list();
+            } else {
+                list.add_to_list(userInput);
             }
         }
     }
@@ -17,4 +22,5 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
         System.exit(0);
     }
+
 }
