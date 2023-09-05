326786bde9b2148fb8b03edfab2bc5782e976519
Azfarul Matin <azfarulmatin@gmail.com>
2023-09-05 11:55:59 +0800

    Add chatbot message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,6 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "__________________________________________\n";
+        System.out.println(logo + "Hello I'm MatinBot\n" + "What can I do for you?\n" +logo + "Bye. Hope to see you again soon!\n" + logo);
     }
 }
