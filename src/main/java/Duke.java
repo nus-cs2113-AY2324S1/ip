942c0c4b5e07c189403c56d996795483d429f59f
Azfarul Matin <azfarulmatin@gmail.com>
2023-09-06 00:46:44 +0800

    Add level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "__________________________________________\n";
-        System.out.println(logo + "Hello I'm MatinBot\n" + "What can I do for you?\n" +logo + "Bye. Hope to see you again soon!\n" + logo);
+        System.out.println(logo + "Hello I'm MatinBot\n" + "What can I do for you?\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        do {
+            userInput = scanner.nextLine();
+            System.out.println(logo + userInput + "\n" + logo);
+        } while (!userInput.equals("bye"));
+
+        System.out.println("Bye. Hope to see you again soon!\n" + logo);
+        scanner.close();
     }
 }
