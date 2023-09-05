40122269d408bfa83cb483638a35c9923d1806d8
Hong Zhi Ee <hongzhiee@gmail.com>
2023-09-05 14:55:56 +0800

    update Duke.java week3 level1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void printLine(){
         System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        printLine();
         System.out.println("Hello! I'm TheChattyFatty");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        printLine();
+
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String response = scanner.nextLine();
+
+            if(response.equals("bye")){
+                break;
+            }
+
+            printLine();
+            System.out.println(response);
+            printLine();
+        }
+
+
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        printLine();
     }
 }
