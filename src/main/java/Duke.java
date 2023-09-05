1eef87b5e1aac0247434d34d02e5f86c6675a37a
Azfarul Matin <azfarulmatin@gmail.com>
2023-09-06 01:55:14 +0800

    Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,32 +1,76 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsUndone() {
+        isDone = false;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    @Override
+    public String toString() {
+        if (isDone) {
+            return "[X] " + description;
+        } else {
+            return "[ ] " + description;
+        }
+    }
+}
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "__________________________________________\n";
         System.out.println(logo + "Hello I'm MatinBot\n" + "What can I do for you?\n" + logo);
 
         Scanner scanner = new Scanner(System.in);
-        String[] toAdd = new String[100];
+        Task[] tasks = new Task[100];
         int count = 0;
-        String userInput;
+        String userInput = "hi";
 
-        do {
+        while (true) {
             userInput = scanner.nextLine();
             System.out.println(logo);
 
-            if (userInput.equals("list")) {
-                String[] taskList = Arrays.copyOf(toAdd, count);
-                for (int i = 0; i < taskList.length; i++) {
-                    System.out.println((i + 1) + ". " + taskList[i]);
+            if (userInput.equals("bye")) {
+                break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i].toString());
                 }
+            } else if (userInput.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
+                tasks[taskIndex].markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + tasks[taskIndex]);
+            } else if (userInput.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
+                tasks[taskIndex].markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet:\n" + tasks[taskIndex]);
             } else {
-                toAdd[count] = userInput;
+                tasks[count] = new Task(userInput);
                 System.out.println("added: " + userInput);
                 count++;
             }
             System.out.println(logo);
-        } while (!userInput.equals("bye"));
+        }
 
         System.out.println("Bye. Hope to see you again soon!\n" + logo);
         scanner.close();
