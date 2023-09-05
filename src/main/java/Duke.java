c4e3f6e45718f54ee08f70c91e8a04ef9cae4fc3
lckjosh <lckjosh@gmail.com>
2023-09-05 14:38:46 +0800

    add echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -5,8 +7,24 @@ public class Duke {
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("____________________________________________________________");
         System.out.println("Hello from BotBuddy!");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        String command;
+
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            command = in.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(command);
+            System.out.println("____________________________________________________________");
+
+        }
+
         System.out.println("____________________________________________________________");
         System.out.println("Goodbye, hope to see you again soon!");
         System.out.println("____________________________________________________________");
