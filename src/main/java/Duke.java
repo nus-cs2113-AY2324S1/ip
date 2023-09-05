35b72fcb51bd055676b7de8755d68a1fd6f86246
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-09-05 22:15:53 +0800

    add features (add, list but reverse)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,12 +10,30 @@ public class Duke {
         System.out.println("Hello! I'm\n" + logo);
         System.out.println("What can I do for you?");
 
+        String[] theList = new String[100];
         Scanner in = new Scanner(System.in);
+        int i = 0;
 
         String echo = in.nextLine();
         while (!echo.equals("bye")) {
-            System.out.println(echo);
+            if (echo.equals("list")) {
+                //theList[i] is the oldest String, start printing from theList[i - 1]
+                int k = i - 1;
+                for (int j = 1; theList[k] != null && j <= 100; j += 1) {
+                    System.out.println(j + ". " + theList[k]);
+                    k -= 1;
+                    if (k < 0) {
+                        k += 100;
+                    }
+                }
+            }
+            theList[i] = echo;
+            System.out.println("added: " + echo);
             echo = in.nextLine();
+            i += 1;
+            if (i >= 100) {
+                i = 0;
+            }
         }
 
         System.out.println("Bye. Hope to see you again soon!");
