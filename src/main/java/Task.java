2d8c6827dcfbf66a9d6fb47d8396206f10a6c8eb
lckjosh <lckjosh@gmail.com>
2023-09-05 15:25:58 +0800

    create Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        if (isDone) {
+            return "X";
+        } else {
+            return " ";
+        }
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsUndone() {
+        isDone = false;
+    }
+}
