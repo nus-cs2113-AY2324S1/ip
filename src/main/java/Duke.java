5fc8ba6f5f76778df0cb83d3efeb1684a31d027d
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-09-05 22:31:39 +0800

    amend features (add, list)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,23 +17,15 @@ public class Duke {
         String echo = in.nextLine();
         while (!echo.equals("bye")) {
             if (echo.equals("list")) {
-                //theList[i] is the oldest String, start printing from theList[i - 1]
-                int k = i - 1;
-                for (int j = 1; theList[k] != null && j <= 100; j += 1) {
-                    System.out.println(j + ". " + theList[k]);
-                    k -= 1;
-                    if (k < 0) {
-                        k += 100;
-                    }
+                for (int j = 0; j < i; j += 1) {
+                    System.out.println((j + 1) + ". " + theList[j]);
                 }
+            } else {
+                theList[i] = echo;
+                System.out.println("added: " + echo);
+                i += 1;
             }
-            theList[i] = echo;
-            System.out.println("added: " + echo);
             echo = in.nextLine();
-            i += 1;
-            if (i >= 100) {
-                i = 0;
-            }
         }
 
         System.out.println("Bye. Hope to see you again soon!");
