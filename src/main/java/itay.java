6a9b286bbc03d41b17edd950103ecad225a91bd8
itayrefaely <refaelyitay@gmail.com>
2023-09-06 01:30:00 +0800

    Echo

--- src/main/java/itay.java
+++ src/main/java/itay.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
 public class itay {
+
+    public static String parseString(String line){
+        return line.trim();
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,6 +16,17 @@ public class itay {
         // greet
         System.out.println("Hello! I'm Itay");
         System.out.println("What can i do for you?");
+
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        line = parseString(line);
+
+        while(! line.equals("bye")) {
+            System.out.println(line);
+            line = in.nextLine();
+            line = parseString(line);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
