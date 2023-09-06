794caaafa1211dd9eb945c39c17ab13dc5280f29
AlWo223 <alexander.wolters@tum.de>
2023-09-06 20:58:21 +0800

    Refactor the code to improve code quality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,86 +4,52 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static final String horizontalLine = "--------------------------------------------";
+    private static final String HORIZONTAL_LINE = "--------------------------------------------";
+    private static List<Task> tasks = new ArrayList<>();
     public static void main(String[] args) {
         introduceBot();
+        handleCommands();
+        farewellBot();
 
-        String input;
+    }
+
+    public static void handleCommands(){
         Scanner in = new Scanner(System.in);
-        input = in.nextLine();
+        String input;
 
-        List<Task> tasks = new ArrayList<Task>();
         do {
-            String command = input.split(" ", 2)[0];
-            System.out.println(horizontalLine);
-            switch (command.toLowerCase()) {
+            System.out.println(HORIZONTAL_LINE);
+            input = in.nextLine().trim();
+            String[] parts = input.split(" ", 2);
+            String command = parts[0].toLowerCase();
+            String argument = null;
+            if(parts.length>1){
+                argument = parts[1];
+            }
+            switch (command) {
             case "list":
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i+1) + "." + tasks.get(i).toString());
-                }
+                printList();
                 break;
             case "mark":
-                String argument = input.split(" ", 2)[1];
-                int index = Integer.parseInt(argument);
-                if (index < 1 || index > tasks.size()) {
-                    System.out.println("I am sorry, but this task does not exist");
-                } else {
-                    System.out.println("Great! I have marked this task as done:");
-                    tasks.get(index-1).setDone(true);
-                    System.out.println(tasks.get(index-1).toString());
-                }
+                editTask(argument, true);
                 break;
             case "unmark":
-                argument = input.split(" ", 2)[1];
-                index = Integer.parseInt(argument);
-                if (index < 1 || index > tasks.size()) {
-                    System.out.println("I am sorry, but this task does not exist");
-                } else {
-                    System.out.println("Alright, I have marked this task as not done:");
-                    tasks.get(index - 1).setDone(false);
-                    System.out.println(tasks.get(index - 1).toString());
-                }
+                editTask(argument, false);
                 break;
             case "todo":
-                argument = input.split(" ", 2)[1];
-                System.out.println("Ok, I have added the following task:");
-                Task todo = new Todo(argument);
-                tasks.add(todo);
-                System.out.println("   " + todo.toString());
-                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                addToDo(argument);
                 break;
             case "deadline":
-                argument = input.split(" ", 2)[1];
-                System.out.println("Ok, I have added the following task:");
-                String dueDate = argument.split(" /by ")[1];
-                String description = argument.split(" /by ")[0];
-                Task deadline = new Deadline(description, dueDate);
-                tasks.add(deadline);
-                System.out.println("   " + deadline.toString());
-                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                addDeadline(argument);
                 break;
             case "event":
-                argument = input.split(" ", 2)[1];
-                System.out.println("Ok, I have added the following task:");
-                description = argument.split(" /from ")[0];
-                String startDate = argument.split(" /from ")[1].split(" /to ")[0];
-                String endDate = argument.split(" /from ")[1].split(" /to ")[1];
-                Task event = new Event(description, startDate, endDate);
-                tasks.add(event);
-                System.out.println("   " + event.toString());
-                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                addEvent(argument);
                 break;
             default:
+                break;
             }
-            System.out.println(horizontalLine);
-            input = in.nextLine();
-    } while (!input.equalsIgnoreCase("bye"));
-
-        System.out.println(horizontalLine);
-        System.out.println("Have a wonderful day! Hope to see you again soon!");
-        System.out.println(horizontalLine);
-
+            System.out.println(HORIZONTAL_LINE);
+        } while (!input.equalsIgnoreCase("bye"));
     }
 
     public static void introduceBot(){
@@ -92,14 +58,70 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-
         String name = "Lexi";
 
         System.out.println(logo);
-
-        System.out.println(horizontalLine);
+        System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm " + name);
         System.out.println("How can I help you buddy?");
-        System.out.println(horizontalLine);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void farewellBot(){
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Have a wonderful day! Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void printList(){
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i+1) + "." + tasks.get(i));
+        }
+    }
+
+    public static void editTask(String argument, boolean done){
+        int index = Integer.parseInt(argument);
+
+        if (index < 1 || index > tasks.size()){
+            System.out.println("I am sorry, but this task does not exist");
+            return;
+        }
+
+        if (done){
+            System.out.println("Great! I have marked this task as done:");
+        } else{
+            System.out.println("Alright, I have marked this task as not done:");
+        }
+        tasks.get(index-1).setDone(done);
+        System.out.println(tasks.get(index-1));
+    }
+
+    public static void printTaskAddedMessage(Task task){
+        System.out.println("Ok, I have added the following task:");
+        System.out.println("   " + task);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+    }
+    public static void addToDo(String argument){
+        Task todo = new Todo(argument);
+        tasks.add(todo);
+        printTaskAddedMessage(todo);
+    }
+
+    public static void addDeadline(String argument){
+        String dueDate = argument.split(" /by ")[1];
+        String description = argument.split(" /by ")[0];
+        Task deadline = new Deadline(description, dueDate);
+        tasks.add(deadline);
+        printTaskAddedMessage(deadline);
+    }
+
+    public static void addEvent(String argument){
+        String description = argument.split(" /from ")[0];
+        String startDate = argument.split(" /from ")[1].split(" /to ")[0];
+        String endDate = argument.split(" /from ")[1].split(" /to ")[1];
+        Task event = new Event(description, startDate, endDate);
+        tasks.add(event);
+        printTaskAddedMessage(event);
     }
 }
