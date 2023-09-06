ad137e5dab12c4537622edcb0254aa330cdbbd8d
Azfarul Matin <azfarulmatin@gmail.com>
2023-09-06 21:08:27 +0800

    Tweak the code to comply with a coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -38,8 +38,8 @@ class Task {
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = "__________________________________________\n";
-        System.out.println(logo + "Hello I'm MatinBot\n" + "What can I do for you?\n" + logo);
+        String LINE = "__________________________________________\n";
+        System.out.println(LINE + "Hello I'm MatinBot\n" + "What can I do for you?\n" + LINE);
 
         Scanner scanner = new Scanner(System.in);
         Task[] tasks = new Task[100];
@@ -47,32 +47,39 @@ public class Duke {
         String userInput = "hi";
 
         while (true) {
+            // Get the user input first
             userInput = scanner.nextLine();
-            System.out.println(logo);
+            System.out.println(LINE);
 
+            // Checks if input is bye, if not proceed
             if (userInput.equals("bye")) {
                 break;
             } else if (userInput.equals("list")) {
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i].toString());
+                    System.out.println((i + 1)
+                                        + ". "
+                                        + tasks[i].toString());
                 }
             } else if (userInput.startsWith("mark ")) {
                 int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
                 tasks[taskIndex].markAsDone();
-                System.out.println("Nice! I've marked this task as done:\n" + tasks[taskIndex]);
+                System.out.println("Nice! I've marked this task as done:\n"
+                                   + tasks[taskIndex]);
             } else if (userInput.startsWith("unmark ")) {
                 int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
                 tasks[taskIndex].markAsUndone();
-                System.out.println("OK, I've marked this task as not done yet:\n" + tasks[taskIndex]);
+                System.out.println("OK, I've marked this task as not done yet:\n"
+                                   + tasks[taskIndex]);
             } else {
                 tasks[count] = new Task(userInput);
-                System.out.println("added: " + userInput);
+                System.out.println("added: "
+                                   + userInput);
                 count++;
             }
-            System.out.println(logo);
+            System.out.println(LINE);
         }
 
-        System.out.println("Bye. Hope to see you again soon!\n" + logo);
+        System.out.println("Bye. Hope to see you again soon!\n" + LINE);
         scanner.close();
     }
 }
