c0f2659121f4ea04ccd4e00f4ccf19de1fa358e6
James-Hong-Jey <jameshong8387@gmail.com>
2023-09-06 09:47:09 +0800

    Change Todo to Task, Add Deadline and Event

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,14 @@
+public class Deadline extends Task {
+
+    protected String dueDate;
+
+    public Deadline(String description, String dueDate) {
+        super(description);
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + dueDate + ")";
+    }
+}
