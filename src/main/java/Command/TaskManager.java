5a54626f8d6531a7a51380ce0b53d09864694c6c
yeo-menghan <yeomenghan1989@gmail.com>
2023-09-07 01:15:05 +0800

    Add Command files - MessageDisplayer, TaskManager, UserInputHandler and  Refactor Jarvis.java

---
+++ src/main/java/Command/TaskManager.java
@@ -0,0 +1,111 @@
+package Command;
+
+import Tasks.Task;
+import Tasks.Todo;
+import Tasks.Deadline;
+import Tasks.Event;
+
+import java.util.ArrayList;
+
+public class TaskManager {
+
+    private final ArrayList<Task> taskList = new ArrayList<>();
+
+    public void listTasks() {
+        System.out.println("Here's your tasks!");
+        for (int i = 0; i < taskList.size(); i++) {
+            int indexNum = i + 1;
+            System.out.println(indexNum + "." + taskList.get(i));
+        }
+    }
+
+    public void markTaskAsDone(int index) {
+        if (isValidIndex(index)) {
+            System.out.println("Nice! I've marked this task as done:");
+            taskList.get(index).markAsDone();
+            System.out.println("    " + taskList.get(index));
+        } else {
+            printInvalidTaskMessage(index);
+        }
+    }
+
+    public void unmarkTaskAsDone(int index) {
+        if (isValidIndex(index)) {
+            System.out.println("Nice! I've unmarked:");
+            taskList.get(index).markAsUndone();
+            System.out.println("    " + taskList.get(index));
+        } else {
+            printInvalidTaskMessage(index);
+        }
+    }
+
+    public void addTodo(String description) {
+        if (description.isEmpty()) {
+            displayInvalidFormatMessage("todo");
+            return;
+        }
+
+        Todo todo = new Todo(description);
+        taskList.add(todo);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("    " + todo);
+        displayTaskCount();
+    }
+
+    public void addDeadline(String description, String time) {
+        if (description.isEmpty() || time.isEmpty()) {
+            displayInvalidFormatMessage("deadline");
+            return;
+        }
+
+        Deadline deadline = new Deadline(description, time);
+        taskList.add(deadline);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("    " + deadline);
+        displayTaskCount();
+    }
+
+    public void addEvent(String description, String startTime, String endTime) {
+        if (description.isEmpty() || startTime.isEmpty() || endTime.isEmpty()) {
+            displayInvalidFormatMessage("event");
+            return;
+        }
+
+        String timeRange = startTime + " to " + endTime;
+        Event event = new Event(description, timeRange);
+        taskList.add(event);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("    " + event);
+        displayTaskCount();
+    }
+
+    void displayInvalidFormatMessage(String taskType) {
+        switch(taskType) {
+        case "todo":
+            System.out.println("Invalid format. Description cannot be empty for todo.");
+            break;
+        case "deadline":
+            System.out.println("Invalid format. Use: deadline <description> /by <time>");
+            break;
+        case "event":
+            System.out.println("Invalid format. Use: event <description> /from <start_time> /to <end_time>");
+            break;
+        default:
+            System.out.println("Invalid task format.");
+            break;
+        }
+    }
+
+    private void displayTaskCount() {
+        System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+    }
+
+    private boolean isValidIndex(int index) {
+        return index >= 0 && index < taskList.size();
+    }
+
+    private void printInvalidTaskMessage(int index) {
+        System.out.println("Invalid task number " + (index + 1) + ". Try Again!");
+    }
+}
+
---
+++ src/main/java/Command/UserInputHandler.java
@@ -0,0 +1,95 @@
+package Command;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class UserInputHandler {
+
+    @FunctionalInterface
+    interface Command {
+        void execute(String userInput);
+    }
+
+    private static final String LINE_BREAK = "____________________________________________________________";
+    private static final Scanner sc = new Scanner(System.in);
+    private static final TaskManager taskManager = new TaskManager();
+
+    private static final Map<String, Command> commands = new HashMap<>();
+    static {
+        commands.put("list", userInput -> taskManager.listTasks());
+
+        commands.put("mark", userInput -> {
+            int index = extractIndex(userInput, 5);
+            if (index != -1) {
+                taskManager.markTaskAsDone(index);
+            }
+        });
+
+        commands.put("unmark", userInput -> {
+            int index = extractIndex(userInput, 7);
+            if (index != -1) {
+                taskManager.unmarkTaskAsDone(index);
+            }
+        });
+
+        commands.put("todo", userInput -> taskManager.addTodo(userInput.substring(5).trim()));
+
+        commands.put("deadline", userInput -> {
+            int lastIndex = userInput.lastIndexOf("/by");
+            if (lastIndex != -1) {
+                String description = userInput.substring(9, lastIndex).trim();
+                String time = userInput.substring(lastIndex + 4).trim();
+                taskManager.addDeadline(description, time);
+            } else {
+                taskManager.displayInvalidFormatMessage("deadline");
+            }
+        });
+
+        commands.put("event", userInput -> {
+            String[] parts = userInput.substring(6).trim().split("/from|/to", 3);
+            if (parts.length >= 3) {
+                String description = parts[0].trim();
+                String startTime = parts[1].trim();
+                String endTime = parts[2].trim();
+                taskManager.addEvent(description, startTime, endTime);
+            } else {
+                taskManager.displayInvalidFormatMessage("event");
+            }
+        });
+    }
+
+    public static void processUserCommands() {
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(LINE_BREAK);
+
+            Command command = commands.getOrDefault(getCommandWord(userInput), null);
+            if (command != null) {
+                command.execute(userInput);
+            } else {
+                System.out.println("Unknown command. Please try again.");
+            }
+
+            System.out.println(LINE_BREAK);
+            userInput = sc.nextLine();
+        }
+    }
+
+    private static String getCommandWord(String userInput) {
+        return userInput.split(" ")[0];
+    }
+
+    private static int extractIndex(String userInput, int startIndex) {
+        try {
+            if (startIndex < userInput.length()) {
+                return Integer.parseInt(userInput.substring(startIndex).trim()) - 1; // Convert to 0-indexed
+            } else {
+                throw new NumberFormatException();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid task number. Please try again.");
+            return -1; // Invalid number
+        }
+    }
+}
