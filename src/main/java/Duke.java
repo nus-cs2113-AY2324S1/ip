1121f605655a97dcc74bc1ba0d0d322960c2689a
Hong Zhi Ee <hongzhiee@gmail.com>
2023-09-06 19:32:52 +0800

    add mark/unmark functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,12 +14,57 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         while(true) {
+            // Get user input
             String response = scanner.nextLine();
+
+            // Handle basic response cases
             if (response.equals("bye")) {
                 break;
-            } else if (response.equals("list")) {
+            }
+            else if (response.equals("list")) {
                 taskList.show();
-            } else {
+                continue;
+            }
+
+            // Split user input into array of words
+            String[] words = response.split(" ");
+
+            // Handling mark case
+            if(words[0].equals("mark")) {
+                // Handle first exception case: number of elements not equal to 2
+                if (words.length != 2) {
+                    taskList.add(response);
+                    continue;
+                }
+
+                // Handle second exception case: second element is not an int
+                try {
+                    int markIndex = Integer.parseInt(words[1]);
+                    taskList.mark(markIndex);
+                }
+                catch (NumberFormatException e) {
+                    System.out.println("Please enter am integer value for the index");
+                }
+            }
+            else if(words[0].equals("unmark")){
+                // Handle first exception case: number of elements not equal to 2
+                if (words.length != 2){
+                    taskList.add(response);
+                    continue;
+                }
+
+                // Handle second exception case: second element is not an int
+                try {
+                    int unmarkIndex = Integer.parseInt(words[1]);
+                    taskList.unmark(unmarkIndex);
+                }
+                catch (NumberFormatException e){
+                    System.out.println("Please enter an integer value for the index");
+                }
+            }
+
+            // Default case, add to list
+            else{
                 taskList.add(response);
             }
         }
--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,10 +1,16 @@
 public class List{
     private String[] tasks;
+    private boolean[] isDone;
     private int numItems;
 
     public List(){
         tasks = new String[100];
+        isDone = new boolean[100];
         numItems = 0;
+
+        for(int i = 0; i < 100; i++){
+            isDone[i] = false;
+        }
     }
 
     public void add(String arg){
@@ -13,10 +19,46 @@ public class List{
         numItems++;
     }
 
+    public void mark(int i){
+        //Check if integer value is in range
+        if(i < 1 || i > numItems){
+            System.out.println("Please enter an integer index within range");
+            return;
+        }
+
+        isDone[i - 1] = true;
+        System.out.println("Marked as done:");
+        System.out.println(tasks[i - 1]);
+        show();
+    }
+
+    public void unmark(int i){
+        //Check if integer value is in range
+        if(i < 1 || i > numItems){
+            System.out.println("Please enter an integer index within range");
+            return;
+        }
+
+        isDone[i - 1] = false;
+        System.out.println("Marked as not done yet:");
+        System.out.println(tasks[i - 1]);
+        show();
+    }
+
     public void show(){
         for(int i = 0; i < numItems; i++){
             System.out.print(i + 1);
-            System.out.println(". " + tasks[i]);
+            System.out.print(".[");
+
+            if(isDone[i]){
+                System.out.print("X");
+            }
+            else{
+                System.out.print(" ");
+            }
+
+            System.out.print("] ");
+            System.out.println(tasks[i]);
         }
     }
 }
