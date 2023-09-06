ca361256d880e71ba7db5818a26205947c66d647
NeoMinWei <>
2023-09-06 23:38:35 +0800

    Remove mystery numbers, restructure code

--- src/main/java/Neo.java
+++ src/main/java/Neo.java
@@ -1,54 +1,76 @@
 import java.util.Scanner;
 public class Neo {
     public static void printList(Task[] list) {
+        int listIndex = 1;
+
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < Task.getTotalTasks(); i++) {
-            System.out.print((i + 1) + ". ");
+            System.out.print((listIndex) + ". ");
             System.out.println(list[i]);
+            listIndex++;
         }
     }
 
     public static void markTask(String line, Task[] list) {
         String[] words = line.split(" ");
-        int listIndex = Integer.parseInt(words[1]) - 1;
-        list[listIndex].setDone(true);
+        int listIndex = Integer.parseInt(words[1]);
+        int listArrayIndex = listIndex - 1;
+
+        list[listArrayIndex].setDone(true);
         System.out.println("Nice! I've marked this task as done: ");
-        System.out.println("    " + list[listIndex]);
+        System.out.println("    " + list[listArrayIndex]);
     }
 
     public static void unmarkTask(String line, Task[] list) {
         String[] words = line.split(" ");
-        int listIndex = Integer.parseInt(words[1]) - 1;
-        list[listIndex].setDone(false);
+        int listIndex = Integer.parseInt(words[1]);
+        int listArrayIndex = listIndex - 1;
+
+        list[listArrayIndex].setDone(false);
         System.out.println("OK, I've marked this task as not done yet: ");
-        System.out.println("    " + list[listIndex]);
+        System.out.println("    " + list[listArrayIndex]);
     }
 
     public static void addEvent(String line, Task[] list) {
         int fromIndex = line.indexOf("/from");
+        int fromStringLength = 5;
         int toIndex = line.indexOf("/to");
-        String description = line.substring(6, fromIndex - 1);
-        String from = line.substring(fromIndex + 6, toIndex - 1);
-        String to = line.substring(toIndex + 4);
-        list[Task.getTotalTasks()] = new Event(description, from, to);
-        list[Task.getTotalTasks() - 1].printAddedTask();
+        int toStringLength = 3;
+        int eventStringLength= 5;
+
+        String description = line.substring(eventStringLength + 1, fromIndex - 1);
+        String from = line.substring(fromIndex + fromStringLength + 1, toIndex - 1);
+        String to = line.substring(toIndex + toStringLength + 1);
+
+        int taskArrayIndex = Task.getTotalTasks();
+
+        list[taskArrayIndex] = new Event(description, from, to);
+        list[taskArrayIndex].printAddedTask();
     }
 
     public static void addDeadline(String line, Task[] list) {
         int byIndex = line.indexOf("/by");
-        String description = line.substring(9, byIndex - 1);
-        String by = line.substring(byIndex + 4);
-        list[Task.getTotalTasks()] = new Deadline(description, by);
-        list[Task.getTotalTasks() - 1].printAddedTask();
+        int byStringLength = 3;
+        int deadlineStringLength = 8;
+
+        String description = line.substring(deadlineStringLength + 1, byIndex - 1);
+        String by = line.substring(byIndex + byStringLength + 1);
+
+        int taskArrayIndex = Task.getTotalTasks();
+
+        list[taskArrayIndex] = new Deadline(description, by);
+        list[taskArrayIndex].printAddedTask();
     }
 
     public static void addTodo(String line, Task[] list) {
-        String description = line;
-        if (line.startsWith("todo")) {
-            description = line.substring(5);
-        }
-        list[Task.getTotalTasks()] = new Todo(description);
-        list[Task.getTotalTasks() - 1].printAddedTask();
+        int todoStringLength = 4;
+
+        String description = line.substring(todoStringLength + 1);
+
+        int taskArrayIndex = Task.getTotalTasks();
+
+        list[taskArrayIndex] = new Todo(description);
+        list[taskArrayIndex].printAddedTask();
     }
     public static void main(String[] args) {
         System.out.println("Hello! I'm Neo.");
