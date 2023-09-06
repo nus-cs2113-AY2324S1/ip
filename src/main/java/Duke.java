4895c529675f64e3b6432311828da350e29dbb02
Bayasgalan Kherlen <e1248887@u.nus.edu>
2023-09-06 19:48:40 +0800

    Add add_List() and conditionals for Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,23 @@
 import java.util.*;
 public class Duke {
     public static String BOT_NAME = "Amy"; // a constant bot name
-    public static void echo() { // Level-1 Echo
+    public static String[] taskList = new String[100]; // contains tasks
+    public static int numberOfTasks = 0; // number of tasks
+    public static void add_List() { // Level-2 add,list
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String userInput = scanner.nextLine(); // Read user input
             System.out.println("____________________________________________________________");
-            System.out.println(userInput); // Echo the user's input
             if (userInput.equals("bye")) {
                 break; // Exit the loop if the user enters "bye"
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < numberOfTasks; i++) {
+                    System.out.println((i + 1) + ". " + taskList[i]); // list if the user enters "list'
+                }
+            } else {
+                System.out.println("added: " + userInput); // print add
+                taskList[numberOfTasks] = userInput;
+                numberOfTasks++;
             }
             System.out.println("____________________________________________________________");
         }
@@ -25,9 +34,8 @@ public class Duke {
         System.out.println("Hello! I'm " + BOT_NAME);
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        echo();
+        add_List();
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
-
     }
 }
