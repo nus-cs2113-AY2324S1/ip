9922052a33127810207f5310d410fea1e3bc6f9c
YFshadaow <renzd2001@gmail.com>
2023-09-06 20:23:14 +0800

    Feature:   - Add tasks   - List tasks

--- src/main/java/cn/yfshadaow/cs2113/ip/CommandHandler.java
+++ src/main/java/cn/yfshadaow/cs2113/ip/CommandHandler.java
@@ -2,6 +2,8 @@ package cn.yfshadaow.cs2113.ip;
 
 import cn.yfshadaow.cs2113.ip.XiaoAiBot;
 
+import java.util.List;
+
 public class CommandHandler {
 
     private final XiaoAiBot bot;
@@ -11,12 +13,12 @@ public class CommandHandler {
 
     public void handleCommand(String command){
         if (command == null) {
-            bot.sendMessages("Command is null!");
+            bot.sendMessage("Command is null!");
             return;
         }
         String[] splitCommand = command.split(" ");
         if (splitCommand.length < 1) {
-            bot.sendMessages("No command detected!");
+            bot.sendMessage("No command detected!");
             return;
         }
         String[] args = new String[splitCommand.length - 1];
@@ -25,8 +27,16 @@ public class CommandHandler {
             case "bye":
                 bot.setShouldQuit(true);
                 break;
+            case "list":
+                List<String> tasks = bot.getTasks();
+                for (int i = 0; i < tasks.size(); i += 1) {
+                    bot.sendMessageWithoutSplit(i + ". " + tasks.get(i));
+                }
+                bot.sendSplit();
+                break;
             default:
-                bot.sendMessages(command);
+                bot.getTasks().add(command);
+                bot.sendMessage("added " + command);
         }
     }
 }
