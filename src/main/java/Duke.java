3b49f2fab984cf7a1cf2f83ac6732ebce41b5b1f
lctxct <caitinglien@hotmail.com>
2023-09-06 10:38:06 +0800

    Refactor code to improve modularity

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,21 +2,23 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static final String LINE = "____________________________________________________________";
+    private static final String LINE = "____________________________________________________________";
+    private static Task[] listItems = new Task[100];
+    private static int listIdx = 0;
 
     public static void println(String line) {
         System.out.println("\t" + line);
     }
     public static void printWelcome() {
 
-        String bob = "\t   ___    ___  ___ \n"
+        String BOB = "\t   ___    ___  ___ \n"
                 + "\t  / __\\  /___\\/ __\\\n"
                 + "\t /__\\// //  //__\\/\\\n"
                 + "\t/ \\/  \\/ \\_// \\/  \\\n"
                 + "\t\\_____/\\___/\\_____/\n";
 
         println(LINE);
-        println("Hello! I'm\n" + bob);
+        println("Hello! I'm\n" + BOB);
         println("What can I do for you?");
         println(LINE);
     }
@@ -26,99 +28,104 @@ public class Duke {
         println(LINE);
     }
 
-    public static String getCommand(String input) {
-        if (input.contains(" ")) {
-            return input.substring(0, input.indexOf(" "));
+    public static void printList() {
+        for (int i = 0; i < listIdx; i++) {
+            println(String.format("%d. %s", i+1, listItems[i].getTask()));
         }
+    }
 
-        return input;
+    public static int getMarkIdx(String line) {
+        return Integer.parseInt(line.split(" ")[1]) - 1;
+    }
+
+    public static void markItem(String line) {
+        int markIdx = getMarkIdx(line);
+        listItems[markIdx].setIsDone(true);
+
+        println("Nice! I've marked this task as done: ");
+        println(listItems[markIdx].getTask());
+    }
+
+    public static void unmarkItem(String line) {
+        int markIdx = getMarkIdx(line);
+        listItems[markIdx].setIsDone(false);
+
+        println("Nice! I've marked this task as undone: ");
+        println(listItems[markIdx].getTask());
+    }
+
+    public static void handleCreateTask(String line) {
+        listItems[listIdx] = new Task(line);
+        listIdx++;
+
+        println(listItems[listIdx-1].getTaskAdded(listIdx));
+    }
+
+    public static void handleCreateTodo(String line) {
+        String todo = line.substring(line.indexOf(" ")+1);
+        listItems[listIdx] = new Todo(todo);
+        listIdx++;
+
+        println(listItems[listIdx-1].getTaskAdded(listIdx));
+    }
+
+    public static void handleCreateDeadline(String line) {
+        int byIdx = line.indexOf("/by");
+
+        // Extract task description and deadline from user input
+        String description = line.substring(line.indexOf(" ")+1, byIdx-1);
+        String deadline = line.substring(byIdx+ "/by ".length());
+
+        listItems[listIdx] = new Deadline(description, deadline);
+        listIdx++;
+
+        println(listItems[listIdx-1].getTaskAdded(listIdx));
+    }
+
+    public static void handleCreateEvent(String line) {
+        int fromIdx = line.indexOf("/from");
+        int toIdx = line.indexOf("/to");
+
+        // Extract task description, start time and end time from user input
+        String description = line.substring(line.indexOf(" ")+1, fromIdx-1);
+        String start = line.substring(fromIdx+ "/from ".length(), toIdx-1);
+        String end = line.substring(toIdx+ "/to ".length());
+
+        listItems[listIdx] = new Event(description, start, end);
+        listIdx++;
+
+        println(listItems[listIdx-1].getTaskAdded(listIdx));
     }
 
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
 
-        String command;
-
-        int listIdx = 0;
-        Task[] listItems = new Task[100];
-        int markIdx;
-
         printWelcome();
 
-        line = in.nextLine().trim();
+        line = in.nextLine();
         while (line.equals("bye") == false) {
             println(LINE);
 
-            command = getCommand(line);
-
-            if (command.equals("list")) {
-
-                for (int i = 0; i < listIdx; i++) {
-                    println(String.format("%d. %s", i+1, listItems[i].getTask()));
-                }
-
-            } else if (command.equals("mark")) {
-
-                markIdx = Integer.parseInt(line.split(" ")[1]) - 1;
-                listItems[markIdx].setIsDone(true);
-
-                println("Nice! I've marked this task as done: ");
-                println(listItems[markIdx].getTask());
-
-            } else if (command.equals("unmark")) {
-
-                markIdx = Integer.parseInt(line.split(" ")[1]) - 1;
-                listItems[markIdx].setIsDone(false);
-
-                println("Nice! I've marked this task as undone: ");
-                println(listItems[markIdx].getTask());
-
-            } else if (command.equals("todo")) {
-
-                String todo = line.substring(line.indexOf(" ")+1);
-                listItems[listIdx] = new Todo(todo);
-                listIdx++;
-
-                println(listItems[listIdx-1].getTaskAdded(listIdx));
-
-            } else if (command.equals("deadline")) {
-
-                int byIdx = line.indexOf("/by");
-                String description = line.substring(line.indexOf(" ")+1, byIdx-1);
-                String deadline = line.substring(byIdx+ "/by ".length());
-
-                listItems[listIdx] = new Deadline(description, deadline);
-                listIdx++;
-
-                println(listItems[listIdx-1].getTaskAdded(listIdx));
-
-            } else if (command.equals("event")) {
-
-                int fromIdx = line.indexOf("/from");
-                int toIdx = line.indexOf("/to");
-
-                String description = line.substring(line.indexOf(" ")+1, fromIdx-1);
-                String start = line.substring(fromIdx+ "/from ".length(), toIdx-1);
-                String end = line.substring(toIdx+ "/to ".length());
-
-                listItems[listIdx] = new Event(description, start, end);
-                listIdx++;
-
-                println(listItems[listIdx-1].getTaskAdded(listIdx));
-
+            if (line.startsWith("list")) {
+                printList();
+            } else if (line.startsWith("mark")) {
+                markItem(line);
+            } else if (line.startsWith("unmark")) {
+                unmarkItem(line);
+            } else if (line.startsWith("todo")) {
+                handleCreateTodo(line);
+            } else if (line.startsWith("deadline")) {
+                handleCreateDeadline(line);
+            } else if (line.startsWith("event")) {
+                handleCreateEvent(line);
             } else {
-
-                listItems[listIdx] = new Task(line);
-                listIdx++;
-
-                println(listItems[listIdx-1].getTaskAdded(listIdx));
-
+                handleCreateTask(line);
             }
 
             println(LINE);
 
-            line = in.nextLine().trim();
+            line = in.nextLine();
         }
 
         printFarewell();
