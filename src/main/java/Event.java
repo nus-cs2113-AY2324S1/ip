d6f453712c6fe3c215f238cc151bee087677f262
MSI\jyben <jybentian@gmail.com>
2023-09-07 01:50:54 +0800

    add new file "Event.java" as subclass of Task

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Event {
+public class Event extends Task {
+    protected String start, end;
+
+    public Event(String description, String start, String end) {
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+
+
+    @Override
+    public String toString() {
+        //print example: [E][ ] project meeting (from: Aug 6th 2pm to: 4pm)
+        return "[E]" + super.toString() + " (from: " + this.start +  " to: " + this.end + ")";
+    }
+
 }
