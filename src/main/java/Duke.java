98eeed0e23401bf536d991dc0306a8bc08ae07ab
Bayasgalan Kherlen <e1248887@u.nus.edu>
2023-09-06 15:32:07 +0800

    Add echo() method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,19 @@
+import java.util.*;
 public class Duke {
-    public static String BOT_NAME = "Amy";
+    public static String BOT_NAME = "Amy"; // a constant bot name
+    public static void echo() { // Level-1 Echo
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine(); // Read user input
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput); // Echo the user's input
+            if (userInput.equals("bye")) {
+                break; // Exit the loop if the user enters "bye"
+            }
+            System.out.println("____________________________________________________________");
+        }
+        scanner.close();
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,7 +25,9 @@ public class Duke {
         System.out.println("Hello! I'm " + BOT_NAME);
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+        echo();
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+
     }
 }
