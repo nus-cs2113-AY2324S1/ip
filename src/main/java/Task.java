9e32f62974b84f76afa0fec475ae1a3413fd036a
Faris Sirraj <farish.sirraj@gmail.com>
2023-09-06 22:48:46 +0800

    Update attributes and getter function to handle Events

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,15 +1,32 @@
 public class Task {
     protected String description;
     protected boolean isDone;
-
-    public Task(String description){
+    protected String taskType;
+    protected String deadline;
+    protected String from;
+    protected String to;
+    public Task(String description, String taskType){
         this.description = description;
         this.isDone = false;
+        this.taskType = taskType;
     }
     public String getStatusIcon(){
         if (this.isDone){
             return "[X]";
         }
         return "[ ]";
+    }
+
+    public String getTaskTypeIcon(){
+        switch (this.taskType){
+        case "todo":
+            return "[T]";
+        case "deadline":
+            return "[D]";
+        case "event":
+            return "[E]";
+        default:
+            return "[ ]";
+        }
     }
 }
