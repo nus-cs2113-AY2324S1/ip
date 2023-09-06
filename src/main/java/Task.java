22f7501b1f3f14836a6f491900502aa43ffa5b96
MSI\jyben <jybentian@gmail.com>
2023-09-07 01:51:44 +0800

    add new file "Task.java" as parent class of task objects

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void Done(){
+        this.isDone = true;
+    }
+
+    public void unDone(){
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+
 }
