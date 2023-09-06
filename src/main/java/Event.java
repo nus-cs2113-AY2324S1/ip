8e049b782f6c1fd5223002c1c7c04cf777479a94
ChungZhiXuan <zhixuanchung03@gmail.com>
2023-09-06 21:50:11 +0800

    Add Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,15 @@
+public class Event extends Task {
+
+    protected String start;
+    protected String end;
+    public Event (String description, String start, String end){
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String getDescription() {
+        return "[E]" + super.getDescription() + " (from: " + start + "to: " + end + ")";
+    }
+}
