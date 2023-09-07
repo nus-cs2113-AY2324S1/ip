cd10722c7a782ba6ac51f54432d4197fa5bbbe34
ZH <cazh1or2@gmail.com>
2023-09-07 17:11:09 +0800

    Tweak the code to comply with coding standard

--- src/main/java/Nuke.java
+++ src/main/java/Nuke.java
@@ -3,19 +3,19 @@ import java.util.ArrayList;
 
 public class Nuke {
 
-    public static void greet() {
-        line();
+    public static void printGreetingMessage() {
+        printLine();
         System.out.println("     Hello! I'm Nuke\n" + "     What can I do for you?");
-        line();
+        printLine();
     }
 
-    public static void exit() {
-        line();
+    public static void printExitMessage() {
+        printLine();
         System.out.println("     Bye. Hope to see you again soon!");
-        line();
+        printLine();
     }
 
-    public static void line() {
+    public static void printLine() {
         System.out.println("    ____________________________________________________________");
     }
 
@@ -23,12 +23,12 @@ public class Nuke {
         Scanner input = new Scanner(System.in);
         String item = input.nextLine();
         if (item.equals("bye")) {
-            exit();
+            printExitMessage();
             return;
         } else {
-            line();
+            printLine();
             System.out.println(item);
-            line();
+            printLine();
             echo();
         }
     }
@@ -37,18 +37,18 @@ public class Nuke {
         Scanner input = new Scanner(System.in);
         String item = input.nextLine();
          if (item.equals("bye")) {
-            exit();
+            printExitMessage();
             return;
         } else if (item.equals("list")) {
             list(itemList, markList);
         } else if (item.length() > 4 && item.substring(0, 4).equals("mark")) {
             String[] splitItem = item.split(" ");
-            int markNum = Integer.parseInt(splitItem[1]);
-            mark(itemList, markList, markNum);
+            int listIndex = Integer.parseInt(splitItem[1]);
+            mark(itemList, markList, listIndex);
         } else if (item.length() > 6 && item.substring(0, 6).equals("unmark")) {
             String[] splitItem = item.split(" ");
-            int markNum = Integer.parseInt(splitItem[1]);
-            unmark(itemList, markList, markNum);
+            int listIndex = Integer.parseInt(splitItem[1]);
+            unmark(itemList, markList, listIndex);
         } else {
             add(itemList, markList, item);
         }
@@ -56,40 +56,40 @@ public class Nuke {
     }
 
     public static void add(ArrayList<String> itemList, ArrayList<String> markList, String item) {
-            line();
+            printLine();
             itemList.add(item);
             markList.add("[ ]");
             System.out.printf("     added: %s\n", item);
-            line();
+            printLine();
     }
 
     public static void list(ArrayList<String> itemList, ArrayList<String> markList) {
-        line();
+        printLine();
         System.out.println("     Here are the tasks in your list:");
         for (int i = 0; i < itemList.size(); i++) {
             System.out.printf("     %d.%s %s\n", i+1, markList.get(i), itemList.get(i));
         }
-        line();
+        printLine();
     }
 
-    public static void mark(ArrayList<String> itemList, ArrayList<String> markList, int markNum) {
-        line();
+    public static void mark(ArrayList<String> itemList, ArrayList<String> markList, int listIndex) {
+        printLine();
         System.out.println("     Nice! I've marked this task as done:");
-        markList.set(markNum - 1, "[X]");
-        System.out.printf("       %s %s\n", markList.get(markNum - 1), itemList.get(markNum - 1));
-        line();
+        markList.set(listIndex - 1, "[X]");
+        System.out.printf("       %s %s\n", markList.get(listIndex - 1), itemList.get(listIndex - 1));
+        printLine();
     }
 
-    public static void unmark(ArrayList<String> itemList, ArrayList<String> markList, int markNum) {
-        line();
+    public static void unmark(ArrayList<String> itemList, ArrayList<String> markList, int listIndex) {
+        printLine();
         System.out.println("     OK, I've marked this task as not done yet:");
-        markList.set(markNum - 1, "[ ]");
-        System.out.printf("       %s %s\n", markList.get(markNum - 1), itemList.get(markNum - 1));
-        line();
+        markList.set(listIndex - 1, "[ ]");
+        System.out.printf("       %s %s\n", markList.get(listIndex - 1), itemList.get(listIndex - 1));
+        printLine();
     }
 
     public static void main(String[] args) {
-        greet();
+        printGreetingMessage();
         ArrayList<String> itemList = new ArrayList<String>();
         ArrayList<String> markList = new ArrayList<String>();
         dialogue(itemList, markList);
