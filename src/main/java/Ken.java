4a96d471913bc4d5fe28f37698edfc4080a3eb56
karishma <karishma13th@gmail.com>
2023-09-07 17:23:14 +0800

    Add level-3

--- src/main/java/Ken.java
+++ src/main/java/Ken.java
@@ -7,6 +7,7 @@ public class Ken {
 
         //Initialise array to store tasks
         String[] tasks = new String[100];
+        boolean[] taskStatus = new boolean[100]; // true for done, false for not done
         int taskCount = 0;
 
         // Print the initial message
@@ -32,17 +33,42 @@ public class Ken {
             } else if (userInput.equalsIgnoreCase("list")) {
                 System.out.println(" Tasks ");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + "." + tasks[i]);
+                    char statusSymbol = taskStatus[i] ? 'X' : ' ';
+                    System.out.println(" " + (i + 1) + ".[" + statusSymbol + "] " + tasks[i]);                }
+            } else if (userInput.startsWith("mark " )) {
+                try {
+                    int taskIndex = Integer.parseInt(userInput.substring(5).trim()) - 1;
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        taskStatus[taskIndex] = true;
+                        System.out.println(" Nice! I've marked this task as done:");
+                        System.out.println("   [X] " + tasks[taskIndex]);
+                    } else {
+                        System.out.println(" Task not found. Please provide a valid task number.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(" Invalid input. Please provide a valid task number.");
                 }
-            } else {
+            } else if (userInput.startsWith("unmark ")) {
+                try {
+                    int taskIndex = Integer.parseInt(userInput.substring(7).trim()) - 1;
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        taskStatus[taskIndex] = false;
+                        System.out.println(" OK, I've marked this task as not done yet:");
+                        System.out.println("   [ ] " + tasks[taskIndex]);
+                    } else {
+                        System.out.println(" Task not found. Please provide a valid task number.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(" Invalid input. Please provide a valid task number.");
+                }
+            }
+            else {
                 //Add task to the array
                 tasks[taskCount] = userInput;
                 taskCount++;
                 System.out.println(" added: " + userInput);
             }
-
         }
-
         // Close the scanner when done
         scanner.close();
     }
