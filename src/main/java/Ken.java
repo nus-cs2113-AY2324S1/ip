d97f8cf622c09519bacbbe0fe29618ccd9af488e
karishma <karishma13th@gmail.com>
2023-09-07 17:09:06 +0800

    Add level-2

--- src/main/java/Ken.java
+++ src/main/java/Ken.java
@@ -5,6 +5,10 @@ public class Ken {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //Initialise array to store tasks
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         // Print the initial message
         System.out.println(" ____________________________________________________________");
         System.out.println(" Hello! I'm Ken");
@@ -17,7 +21,7 @@ public class Ken {
 
             // Echo the user's command
             System.out.println(" ____________________________________________________________");
-            System.out.println(" " + userInput);
+
 
             // Check if the user wants to exit
             if (userInput.equalsIgnoreCase("bye")) {
@@ -25,7 +29,18 @@ public class Ken {
                 System.out.println(" Bye then!");
                 System.out.println("____________________________________________________________");
                 break; // Exit the loop
+            } else if (userInput.equalsIgnoreCase("list")) {
+                System.out.println(" Tasks ");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
+                }
+            } else {
+                //Add task to the array
+                tasks[taskCount] = userInput;
+                taskCount++;
+                System.out.println(" added: " + userInput);
             }
+
         }
 
         // Close the scanner when done
