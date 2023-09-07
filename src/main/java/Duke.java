56b56598cfc13ad37024367a6aec80204b96e538
TeoHaoZhi <e0957449@u.nus.edu>
2023-09-07 22:19:36 +0800

    Delete Echo class, shift Echo class contents to Duke class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,55 @@
+import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         //Greetings & renaming
         System.out.println("Hello! I'm JARVIS");
         System.out.println("What can I do for you?");
+        ToDoList newList  = new ToDoList();
 
-        Echo respond = new Echo();
-        respond.echoBack();
+        Scanner in = new Scanner(System.in);
+        String text = in.next();
+        if (text.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
+        while(text.equals("bye") == false) {
 
 
+            if (text.equals(newList.checker)) {
+                newList.listTask();
+                text = in.next();
 
+            } else if (text.startsWith(newList.marker)) {
 
+                String part = in.nextLine();
+                newList.mark(text + part);
+                text = in.next();
 
+            } else if (text.startsWith(newList.unmarked)) {
+                String part = in.nextLine();
+                newList.unmark(text + part);
+                text = in.next();
+
+            } else if (text.equals("todo")) {
+                String task = in.nextLine();
+                newList.addToDo(task);
+                text = in.next();
+
+            } else if (text.equals("deadline")) {
+                String[] task = in.nextLine().split("/by");
+                newList.addDeadline(task[0], task[1]);
+                text = in.next();
+
+            } else if (text.equals("event")) {
+                String[] task = in.nextLine().split("/from | /to");
+                newList.addEvent(task[0], task[1], task[2]);
+                text = in.next();
+            } else {
+                text = in.next();
+
+            }
+        }
     }
+
 }
+
