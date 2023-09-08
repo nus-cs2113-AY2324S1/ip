7b661902a860fb109123c17e09870014e69d1d3d
Bayasgalan Kherlen <e1248887@u.nus.edu>
2023-09-08 15:31:45 +0800

    Add Deadline, Todo, and Event classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,10 +3,12 @@ public class Duke {
     public static String BOT_NAME = "Amy"; // a constant bot name
     public static Task[] taskList = new Task[100]; // contains tasks
     public static int numberOfTasks = 0; // number of tasks
-    public static void addToList(String task) {
-        taskList[numberOfTasks] = new Task(task);
+    public static void addToList(Task task) {
+        taskList[numberOfTasks] = task;
         numberOfTasks++;
-        System.out.println("added: " + task);
+        System.out.println("Got it. I've added this task: ");
+        System.out.println("  " + task);
+        System.out.println("Now you have " + numberOfTasks + " tasks in the list");
     }
     public static void listTasks() {
         System.out.println("Here are the tasks in your list:");
@@ -63,8 +65,25 @@ public class Duke {
                 // Extract the task index from the user input
                 int taskIndex = Integer.parseInt(userInput.substring(7).trim());
                 unmarkTask(taskIndex);
-            } else {
-                addToList(userInput);
+            } else if (userInput.startsWith("todo")) {
+                String description = userInput.substring(5).trim();
+                Todo todo = new Todo(description);
+                addToList(todo);
+            }else if (userInput.startsWith("deadline")) {
+                int inputIndex = userInput.indexOf(" /by ");
+                String description = userInput.substring("deadline ".length(), inputIndex);
+                String by = userInput.substring(inputIndex + " /by ".length());
+                addToList(new Deadline(description, by));
+            } else if (userInput.startsWith("event")) {
+                String[] parts = userInput.substring(5).split("/from");
+                String description = parts[0].trim();
+                String[] dateTimeParts = parts[1].split("/to");
+                String from = dateTimeParts[0].trim();
+                String to = dateTimeParts[1].trim();
+                Event event = new Event(description, from, to);
+                addToList(event);
+            }  else {
+                System.out.println("Invalid command. Please try again.");
             }
             System.out.println("____________________________________________________________");
         }
