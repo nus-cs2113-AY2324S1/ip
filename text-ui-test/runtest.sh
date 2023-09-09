e0f20188df9851c23f8874fcdf69652e391afd68
Martin Schneider <martin.schneider@ikmail.com>
2023-09-09 19:17:16 +0800

    Update tests

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# create bin directory if it doesn't exist
-if [ ! -d "../bin" ]
-then
-    mkdir ../bin
-fi
+# # create bin directory if it doesn't exist
+# if [ ! -d "../bin" ]
+# then
+#     mkdir ../bin
+# fi
 
 # delete output from previous run
 if [ -e "./ACTUAL.TXT" ]
@@ -13,18 +13,18 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../out/production/ip ../src/main/java/jerry/*.java ../src/main/java/jerry/task/*.java ../src/main/java/jerry/userInterface/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../out/production/ip jerry.Jerry < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+# dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
