20a9fa423ce88832e5cd3fa96f6dfadbbcf33f11
Faris Sirraj <faris@curium.sg>
2023-09-09 17:46:37 +0800

    Create a separate package file for Event

---
+++ src/main/java/duke/Event.java
@@ -0,0 +1,10 @@
+package duke;
+public class Event extends Task{
+
+    public Event(String description, String from, String to) {
+        super(description);
+        super.taskType = "event";
+        super.from = from;
+        super.to = to;
+    }
+}
