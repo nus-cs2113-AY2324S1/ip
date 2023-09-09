f2eafbe7249572d44ca777fd4e6ea0eff3b69927
Faris Sirraj <farish.sirraj@gmail.com>
2023-09-09 17:41:48 +0800

    Remove classes files from the java directory

--- src/main/java/Deadline.java
+++
@@ -1,8 +0,0 @@
-public class Deadline extends Task {
-
-    public Deadline(String description, String by) {
-        super(description);
-        super.taskType = "deadline";
-        super.deadline = by;
-    }
-}
