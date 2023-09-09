4b85126dccfef5b0b099cb59d485b72d8493d402
Faris Sirraj <faris@curium.sg>
2023-09-09 17:41:48 +0800

    Remove classes files from the java directory

--- src/main/java/Event.java
+++
@@ -1,9 +0,0 @@
-public class Event extends Task{
-
-    public Event(String description, String from, String to) {
-        super(description);
-        super.taskType = "event";
-        super.from = from;
-        super.to = to;
-    }
-}
--- src/main/java/ToDo.java
+++
@@ -1,7 +0,0 @@
-public class ToDo extends Task{
-
-    public ToDo(String description) {
-        super(description);
-        super.taskType = "todo";
-    }
-}
