ea2fd876d5d5a29282b882da93970e340e4513fd
woodenclock <luckyisland3710@gmail.com>
2023-09-10 17:50:59 +0800

    Update Elvis.java

--- src/elvis/Elvis.java
+++ src/elvis/Elvis.java
@@ -3,7 +3,6 @@ package elvis;
 public class Elvis {
 
     public static void main(String[] args) {
-
         //Functionalities of ChatBot
         //Echo.shoutEcho(); (WEEK 1)
         //List.makeList();  (WEEK 2)
