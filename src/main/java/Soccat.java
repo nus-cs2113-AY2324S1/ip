b5132e28c4414793a0dda64cf1e87cca2ec31924
lisizhuang-0121 <sizhuangli6@gmail.com>
2023-09-12 14:21:45 +0800

    Add error handling features for user input
    Catch errors entered by users and provide better feedback for user actions.

--- src/main/java/Soccat.java
+++ src/main/java/Soccat.java
@@ -3,23 +3,40 @@ import java.util.Scanner;
 public class Soccat {
 
     static Task[] tasks = new Task[100];
+    static final String[] COMMANDS = {"bye","list","todo","deadline","event","mark","unmark"};
 
     public static void addTask(String command, String type) {
         String task = command.replace(type, "").strip();
         int taskIndex = Task.getTaskCount();
         switch(type) {
         case "todo":
+            if (task.isEmpty()) {
+                System.out.println("Oops, the task for todo cannot be blank!");
+                return;
+            }
             tasks[taskIndex] = new Todo(task);
             break;
         case "deadline":
-            String[] deadlineTokens = task.split(" /by ");
-            tasks[taskIndex] = new Deadline(deadlineTokens[0], deadlineTokens[1]);
-            break;
+            try {
+                String[] deadlineTokens = task.split(" /by ");
+                tasks[taskIndex] = new Deadline(deadlineTokens[0], deadlineTokens[1]);
+                break;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Oops, you have missing fields for creating a deadline!");
+                System.out.println("Please try deadline <task> /by <deadline>");
+                return;
+            }
         case "event":
-            String[] eventTokens = task.split(" /");
-            String from = eventTokens[1].replace("from ", "");
-            String by = eventTokens[2].replace("to ", "");
-            tasks[taskIndex] = new Event(eventTokens[0], from, by);
+            try {
+                String[] eventTokens = task.split(" /");
+                String from = eventTokens[1].replace("from ", "");
+                String by = eventTokens[2].replace("to ", "");
+                tasks[taskIndex] = new Event(eventTokens[0], from, by);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Oops, you have missing fields for creating an event!");
+                System.out.println("Please try event <task> /from <start> /to <end>");
+                return;
+            }
             break;
         }
         System.out.println("Got it. I've added this task: ");
@@ -43,10 +60,15 @@ public class Soccat {
     }
 
     public static void toggleDone(String taskNumber, boolean isDone) {
+        int taskCount = Task.getTaskCount();
+        if (taskCount == 0) {
+            System.out.println("No tasks to mark for now, you may take a break!");
+            return;
+        }
         try {
             int taskId = Integer.parseInt(taskNumber);
-            if (taskId > Task.getTaskCount()) {
-                System.out.println("Task number out of range!");
+            if (taskId > taskCount) {
+                System.out.println("Oops, there are only " + taskCount + " tasks now!");
                 return;
             }
             // Subtract 1 for array index
@@ -56,37 +78,51 @@ public class Soccat {
         } catch (NumberFormatException e) {
             System.out.println("Invalid task number!");
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Task number out of range!");
+            System.out.println("Soccat.Task number out of range!");
         }
+    }
+
+    public static void testCommandInput(String commandInput) throws SoccatException {
+        for (String command : COMMANDS) {
+            if (commandInput.equals(command)) {
+                return;
+            }
+        }
+        throw new SoccatException();
     }
 
     public static void processCommand(String line) {
         Scanner textIn = new Scanner(System.in);
-        String command = null;
-        while (true) {
-            command = textIn.nextLine();
-            if (command.equals("bye")) {
-                System.out.println(line);
-                break;
-            }
-            String[] tokens = command.split(" ");
+        String commandInput = "";
+        while (!commandInput.equals("bye")) {
+            commandInput = textIn.nextLine();
+            String[] tokens = commandInput.split(" ");
             System.out.println(line);
+            // Test command for compatability
+            try {
+                testCommandInput(tokens[0]);
+            } catch (SoccatException e) {
+                System.out.println("Oops, your command is not recognized, please try again!");
+                System.out.println(line);
+                continue;
+            }
             switch (tokens[0]) {
-            case "list": listTasks();
+            case "bye":
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            case "list":
+                listTasks();
                 break;
             case "todo":
             case "deadline":
             case "event":
-                addTask(command, tokens[0]);
+                addTask(commandInput, tokens[0]);
                 break;
             case "mark":
                 toggleDone(tokens[1], true);
                 break;
             case "unmark":
                 toggleDone(tokens[1], false);
-                break;
-            default:
-                System.out.println("Sorry, I could not recognize your command.");
                 break;
             }
             System.out.println(line);
@@ -97,6 +133,5 @@ public class Soccat {
         String line = "____________________________________________________________";
         System.out.println(line + "\nHello! I'm soccat!\nWhat can I do for you?\n" + line);
         processCommand(line);
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
     }
 }
