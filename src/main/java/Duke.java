49cb2e566ed04d0d85345dd8ae603040a6ed6d05
ryanlohyr <ryanloh88@ymail.com>
2023-09-13 22:51:08 +0800

    Refactor if else statement into switch statement

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -46,71 +46,73 @@ public class Duke {
 
             String initialWord = words[0];
 
-            if (initialWord.equals("list")) {
+            switch (initialWord) {
+                case "list" -> printList(storageArray, numOfItems);
+                case "mark" -> {
+                    System.out.println("mark " + words[1]);
+                    int index = Integer.parseInt(words[1]);
+                    // we need to check if its out of bounds first
+                    if (index > numOfItems || index <= 0) {
+                        System.out.println("index out of bounds, try again later.");
+                    } else {
+                        Task currTask = storageArray[index - 1];
+                        currTask.markStatus();
+                        System.out.println("Nice! I've marked this task as done: ");
+                        String message = String.format("[%s] %s", currTask.getStatusIcon(), currTask.getDescription());
+                        System.out.println(message);
+                    }
 
-                printList(storageArray, numOfItems);
-
-            } else if (initialWord.equals("mark")) {
-
-                System.out.println("mark " + words[1]);
-                int index = Integer.parseInt(words[1]);
-                // we need to check if its out of bounds first
-                if (index > numOfItems || index <= 0) {
-                    System.out.println("index out of bounds, try again later.");
-                } else {
-                    Task currTask = storageArray[index - 1];
-                    currTask.markStatus();
-                    System.out.println("Nice! I've marked this task as done: ");
-                    String message = String.format("[%s] %s", currTask.getStatusIcon(), currTask.getDescription());
-                    System.out.println(message);
                 }
+                case "unmark" -> {
+                    // unmark an item
+                    System.out.println("unmark " + words[1]);
+                    int index = Integer.parseInt(words[1]);
+                    // we need to check if its out of bounds first
+                    if (index > numOfItems || index <= 0) {
+                        System.out.println("index out of bounds, try again later.");
+                    } else {
+                        Task currTask = storageArray[index - 1];
+                        currTask.unmarkStatus();
+                        System.out.println("Nice! I've marked this task as done: ");
+                        String message = String.format("[%s] %s", currTask.getStatusIcon(), currTask.getDescription());
+                        System.out.println(message);
+                    }
 
-            } else if (initialWord.equals("unmark")) {
-                // unmark an item
-                System.out.println("unmark " + words[1]);
-                int index = Integer.parseInt(words[1]);
-                // we need to check if its out of bounds first
-                if (index > numOfItems || index <= 0) {
-                    System.out.println("index out of bounds, try again later.");
-                } else {
-                    Task currTask = storageArray[index - 1];
-                    currTask.unmarkStatus();
-                    System.out.println("Nice! I've marked this task as done: ");
-                    String message = String.format("[%s] %s", currTask.getStatusIcon(), currTask.getDescription());
-                    System.out.println(message);
                 }
+                case "todo" -> {
+                    String newWord = userInput.replace("todo ", "");
+                    ToDos t = new ToDos(newWord);
+                    storageArray[numOfItems] = t;
+                    System.out.println("added: " + newWord);
+                    numOfItems += 1;
+                }
+                case "deadline" -> {
+                    String newWord = userInput.replace("deadline ", "");
+                    String[] deadlineArray = newWord.split("/");
+                    Deadlines t = new Deadlines(deadlineArray[0], deadlineArray[1]);
+                    storageArray[numOfItems] = t;
+                    System.out.println("added: " + deadlineArray[0]);
+                    numOfItems += 1;
 
-            } else if (initialWord.equals("todo")) {
-                String newWord = userInput.replace("todo ", "");
-                ToDos t = new ToDos(newWord);
-                storageArray[numOfItems] = t;
-                System.out.println("added: " + newWord);
-                numOfItems += 1;
-            } else if (initialWord.equals("deadline")) {
-                String newWord = userInput.replace("deadline ", "");
-                String[] deadlineArray = newWord.split("/");
-                Deadlines t = new Deadlines(deadlineArray[0],deadlineArray[1]);
-                storageArray[numOfItems] = t;
-                System.out.println("added: " + deadlineArray[0]);
-                numOfItems += 1;
+                }
+                case "event" -> {
+                    String newWord = userInput.replace("event ", "");
+                    String[] firstSplit = newWord.split("/");
+                    System.out.println(newWord);
+                    Events t = new Events(firstSplit[0], firstSplit[1], firstSplit[2]);
+                    storageArray[numOfItems] = t;
 
-            } else if (initialWord.equals("event")) {
-                String newWord = userInput.replace("event ", "");
-                String[] firstSplit = newWord.split("/");
-                System.out.println(newWord);
-                Events t = new Events(firstSplit[0],firstSplit[1], firstSplit[2]);
-                storageArray[numOfItems] = t;
-                
-                numOfItems += 1;
-                System.out.println("added: " + firstSplit[0] + ", you have now " + numOfItems + " tasks");
-                
+                    numOfItems += 1;
+                    System.out.println("added: " + firstSplit[0] + ", you have now " + numOfItems + " tasks");
 
-            } else {
-                Task t = new Task(userInput);
 
-                storageArray[numOfItems] = t;
-                System.out.println("added: " + userInput);
-                numOfItems += 1;
+                }
+                default -> {
+                    Task t = new Task(userInput);
+                    storageArray[numOfItems] = t;
+                    System.out.println("added: " + userInput);
+                    numOfItems += 1;
+                }
             }
 
             userInput = scanner.nextLine();
