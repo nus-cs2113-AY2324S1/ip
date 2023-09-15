9e8fb857cc8084c030950d5e3af01add976bca8a
NeoMinWei <>
2023-09-16 01:56:51 +0800

    Delete unused code

--- src/main/java/neo/task/Task.java
+++ src/main/java/neo/task/Task.java
@@ -3,16 +3,6 @@ package neo.task;
 public class Task {
     protected String description;
     protected boolean isDone;
-    private static int totalTasks = 0;
-
-
-    public static int getTotalTasks() {
-        return totalTasks;
-    }
-
-    public static void incrementTotalTasks() {
-        Task.totalTasks++;
-    }
 
     public void setDone(boolean done) {
         isDone = done;
@@ -21,7 +11,6 @@ public class Task {
     public Task(String description) {
         this.description = description;
         this.isDone = false;
-        incrementTotalTasks();
     }
 
     public String getStatusIcon() {
@@ -33,14 +22,5 @@ public class Task {
         return "[" + this.getStatusIcon() + "] " + this.description;
     }
 
-    public void printAddedTask() {
-        System.out.println("Got it. I've added this task:");
-        System.out.println("    " + this);
-        System.out.print("Now you have " + getTotalTasks());
-        if (getTotalTasks() == 1) {
-            System.out.println(" task in the list.");
-        } else {
-            System.out.println(" tasks in the list.");
-        }
-    }
+
 }
