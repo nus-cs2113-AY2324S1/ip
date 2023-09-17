4ba5ceb95f6cc39e4aee80658a70e7348bd6a2d3
itayrefaely <refaelyitay@gmail.com>
2023-09-17 23:47:31 +0800

    Level-7

--- src/main/java/duke/Itay.java
+++ src/main/java/duke/Itay.java
@@ -1,10 +1,16 @@
 package duke;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Itay {
-
     static ArrayList<Task> tasks = new ArrayList<>(100);
     static int numTasks = 0;
     static String DIVIDER = "------------------------------------------------------------";
@@ -16,7 +22,7 @@ public class Itay {
 
         switch(indicator) {
             case("list"):
-                printList();
+                printList(false);
                 break;
             case("mark"):
                 handleMark(splitInput);
@@ -41,9 +47,9 @@ public class Itay {
         }
     }
 
-    public static void printList() {
+    public static void printList(boolean isIntroduction) {
         if(numTasks == 0) {
-           System.out.println("Wohoo! You have no tasks in your list:"); 
+           System.out.println("You have no tasks in your list."); 
         }
         else {
             System.out.println("Here are the tasks in your list:");
@@ -51,12 +57,13 @@ public class Itay {
                 System.out.println(tasks.get(i).toString());
             }
         }
-        System.out.println(DIVIDER);
+        if(!isIntroduction) System.out.println(DIVIDER);
     }
 
     public static void handleMark(String[] splitInput) throws DukeException {
         int taskIdx = getTaskIndex(splitInput);
         tasks.get(taskIdx).setStatus(true);
+        saveTasksToFile(tasks);
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(tasks.get(taskIdx).toString());
         System.out.println(DIVIDER);
@@ -65,6 +72,7 @@ public class Itay {
     public static void handleUnmark(String[] splitInput) throws DukeException {
         int taskIdx = getTaskIndex(splitInput);
         tasks.get(taskIdx).setStatus(false);
+        saveTasksToFile(tasks);
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println(tasks.get(taskIdx).toString());
         System.out.println(DIVIDER);
@@ -80,7 +88,7 @@ public class Itay {
                 throw new DukeException(errorMessage);
             }
         } catch (ArrayIndexOutOfBoundsException | NumberFormatException inputEx) {
-            throw new DukeException(errorMessage);
+            throw new DukeException(errorMessage + inputEx.getMessage());
         }
         return taskIdx;
     }
@@ -90,6 +98,7 @@ public class Itay {
         Task toDelete = tasks.get(taskIdx);
         tasks.remove(taskIdx);
         numTasks--;
+        saveTasksToFile(tasks);
 
         System.out.println("Got it. I've removed this task:");
         System.out.println(toDelete.toString());
@@ -146,9 +155,10 @@ public class Itay {
         } 
     }
     
-    public static void addTask(Task task) {
+    public static void addTask(Task task) throws DukeException {
         tasks.add(task);
         numTasks++;
+        saveTasksToFile(tasks);
 
         System.out.println("Got it. I've added this task:");   
         System.out.println(task.toString());   
@@ -156,8 +166,61 @@ public class Itay {
         System.out.println(DIVIDER);   
     }
 
+    public static void saveTasksToFile(ArrayList<Task> tasks) throws DukeException {
+        try {
+            FileOutputStream fos = new FileOutputStream("./data/duke.txt");
+            PrintWriter writer = new PrintWriter(fos);
+
+            for (Task task : tasks) {
+                String taskString = task.serializeToString(); 
+                writer.println(taskString);
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            throw new DukeException("OOPS!!! Error saving tasks to file: " + e.getMessage(), e);
+        }
+    }
+
+    public static ArrayList<Task> loadTasksFromFile() throws DukeException {
+        try {
+            File folder = new File("./data");
+            if (!folder.exists()) {
+                folder.mkdirs(); // Create the "data" folder if it doesn't exist
+            }
+
+            File file = new File("./data/duke.txt");
+            if (!file.exists()) {
+                file.createNewFile(); // Create the file "duke.txt" if it doesn't exist
+            }
+
+            FileInputStream fis = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Task task = Task.deserializeFromString(line); // Implement deserialization
+                if (task != null) {
+                    tasks.add(task);
+                }
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            throw new DukeException("OOPS!!! Error loading tasks from file: " + e.getMessage(), e);
+        }
+
+        // Update static variable numTasks 
+        numTasks = tasks.size();
+        return tasks;
+    }
+
     public static void main(String[] args) throws DukeException {
-        System.out.println("Hello! I'm Itay");
+        // Load tasks from the data file at startup
+        loadTasksFromFile();
+        
+        System.out.println("Hello! I'm Itay!");
+        printList(true);
         System.out.println("What can I do for you?");
         System.out.println(DIVIDER);
 
@@ -176,6 +239,9 @@ public class Itay {
                 input = input.trim();
             }
         }
+
+        // Save tasks to the data file before exiting
+        saveTasksToFile(tasks);
 
         System.out.println(DIVIDER);
         System.out.println("Bye. Hope to see you again soon!");
