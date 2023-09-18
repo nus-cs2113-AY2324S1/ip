658e449b82358c0361ecaf8169b30a71c68e3581
Faris Sirraj <farish.sirraj@gmail.com>
2023-09-19 00:05:11 +0800

    Implement initial feature to read stored task from a text file

--- src/main/java/duke/FileManager.java
+++ src/main/java/duke/FileManager.java
@@ -4,37 +4,50 @@ import java.io.IOException;
 import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class FileManager {
 
     public FileWriter writer;
     public File file;
+    private static final ArrayList<duke.Task> taskList = new ArrayList<>();
 
     public FileManager() throws IOException {
         this.writer = new FileWriter("./test.txt", true);
         this.file = new File("./test.txt");
     }
 
-    public void read(){
+    public ArrayList<Task> read(){
         try {
-            FileReader fileReader = new FileReader("test.txt");
+            FileReader fileReader = new FileReader("./test.txt");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
 
             while ((line = bufferedReader.readLine()) != null) {
-                // Split the line using ":" as the delimiter
                 String[] parts = line.split("\\|");
                 String keyword = parts[0].trim();
-                String status = parts[1].trim();
-
-                String key = keyword + " - " + status;
-                System.out.println(key);
-
+                switch (keyword){
+                case "T":
+                    Task todo = new ToDo(parts[2].trim());
+                    todo.isDone = Boolean.parseBoolean(parts[1].trim());
+                    taskList.add(todo);
+                    break;
+                case "D":
+                    Deadline deadline = new Deadline(parts[2].trim(), parts[3].trim());
+                    deadline.isDone = Boolean.parseBoolean(parts[1].trim());
+                    taskList.add(deadline);
+                    break;
+                case "E":
+                    Task event = new Event(parts[2].trim(), parts[3].trim(), parts[4].trim());
+                    event.isDone = Boolean.parseBoolean(parts[1].trim());
+                    taskList.add(event);
+                    break;
+                }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return taskList;
     }
 
     public void write(String text){
