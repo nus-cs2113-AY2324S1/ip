624f2b4bf1cecbb7b2e5fcb813253c00fafbef27
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-09-19 10:07:00 +0800

    Implemented A-Collections

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,64 +1,15 @@
 import java.util.Scanner;  // Import the Scanner class
 import java.io.File; // Import the File class
+import java.util.ArrayList; // Import the ArrayList class
 
 public class Duke {
 
     //Initialize create a list of tasks
-    public static Task[] tasks = new Task[100];
-    public static int taskCount = 0;
+    public static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        //check if there is a file to read from
-        try{
-            File file = new File("data/duke.txt");
-            Scanner fileScan = new Scanner(file);
-            while(fileScan.hasNextLine()){
-                String line = fileScan.nextLine();
-                String[] lineSplit = line.split(" \\| ");
-                if(lineSplit[0].equals("T")){
-                    tasks[taskCount] = new Todo(lineSplit[2]);
-                    if(lineSplit.length > 3){
-                        throw new DukeException("Your data file is corrupted");
-                    }
-                    if(lineSplit[1].equals("true")){
-                        tasks[taskCount].markAsDone();
-                    }
-                    taskCount++;
-                }else if(lineSplit[0].equals("D")){
-                    if(lineSplit.length > 4){
-                        throw new DukeException("Your data file is corrupted");
-                    }
-                    tasks[taskCount] = new Deadline(lineSplit[2], lineSplit[3]);
-                    if(lineSplit[1].equals("true")){
-                        tasks[taskCount].markAsDone();
-                    }
-                    taskCount++;
-                }else if(lineSplit[0].equals("E")){
-                    if(lineSplit.length > 4){
-                        throw new DukeException("Your data file is corrupted");
-                    }
-                    tasks[taskCount] = new Event(lineSplit[2], lineSplit[3]);
-                    if(lineSplit[1].equals("true")){
-                        tasks[taskCount].markAsDone();
-                    }
-                    taskCount++;
-                }
-            }
-            //print the tasks in the file
-            
-            fileScan.close();
-        //otherwise create a new file
-        } catch(Exception e){
-            //Create a new file and directory
-            try{
-                File file = new File("data/duke.txt");
-                file.getParentFile().mkdirs();
-                file.createNewFile();
-            }catch(Exception e1){
-                e1.printStackTrace();
-            }
-        }
+        FileRW.readFromFile(tasks);
 
         //Print out the logo
         String logo = " ____        _        \n"
@@ -84,8 +35,8 @@ public class Duke {
                 if(Check.isList(userInput)){
                     //Print out the list of tasks
                     System.out.println("Here are the tasks in your list:");
-                    for(int i=0;i<taskCount;i++){
-                        System.out.println((i+1) + ". " + tasks[i]);
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
                     }
                     //Get user input again
                     userInput = userScan.nextLine();  
@@ -96,7 +47,7 @@ public class Duke {
                     //get the task number
                     int taskNumber = Integer.parseInt(userInput.substring(7));
                     //if the task number is greater than the task count throw an exception
-                    if(taskNumber>taskCount){
+                    if(taskNumber>tasks.size()){
                         throw new IllegalArgumentException("The task number is greater than the number of tasks.");
                     }
                     //if the task number is less than 1 throw an exception
@@ -104,18 +55,18 @@ public class Duke {
                         throw new IllegalArgumentException("The task number is less than 1.");
                     }
                     //if there is no task at the task number throw an exception
-                    if(tasks[taskNumber-1]==null){
+                    if(tasks.get(taskNumber-1)==null){
                         throw new IllegalArgumentException("There is no task at the task number.");
                     }
                     //if the task is already not done throw an exception
-                    if(tasks[taskNumber-1].isDone==false){
+                    if(tasks.get(taskNumber-1).isDone==false){
                         throw new IllegalArgumentException("The task is already not done.");
                     }
                     //mark the task as done
-                    tasks[taskNumber-1].markAsNotDone();
+                    tasks.get(taskNumber-1).markAsNotDone();
                     //print out the task that was marked as done
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println(tasks[taskNumber-1]);
+                    System.out.println(tasks.get(taskNumber-1));
                     //Get user input again
                     userInput = userScan.nextLine();          
                 }
@@ -125,7 +76,7 @@ public class Duke {
                     //get the task number
                     int taskNumber = Integer.parseInt(userInput.substring(5));
                     //if the task number is greater than the task count throw an exception
-                    if(taskNumber>taskCount){
+                    if(taskNumber>tasks.size()){
                         throw new IllegalArgumentException("The task number is greater than the number of tasks.");
                     }
                     //if the task number is less than 1 throw an exception
@@ -133,18 +84,18 @@ public class Duke {
                         throw new IllegalArgumentException("The task number is less than 1.");
                     }
                     //if there is no task at the task number throw an exception
-                    if(tasks[taskNumber-1]==null){
+                    if(tasks.get(taskNumber-1)==null){
                         throw new IllegalArgumentException("There is no task at the task number.");
                     }
                     //if the task is already done throw an exception
-                    if(tasks[taskNumber-1].isDone==true){
+                    if(tasks.get(taskNumber-1).isDone==true){
                         throw new IllegalArgumentException("The task is already done.");
                     }
                     //mark the task as done
-                    tasks[taskNumber-1].markAsDone();
+                    tasks.get(taskNumber-1).markAsDone();
                     //print out the task that was marked as done
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(tasks[taskNumber-1]);
+                    System.out.println(tasks.get(taskNumber-1));
                     //Get user input again
                     userInput = userScan.nextLine();              
                 }
@@ -158,13 +109,11 @@ public class Duke {
                         throw new IllegalArgumentException("The description of a todo cannot be empty.");
                     }
                     //create a todo task
-                    tasks[taskCount] = new Todo(taskName);
-                    //increment the task count
-                    taskCount++;
+                    tasks.add(new Todo (taskName));
                     //print out the task that was added
                     System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks[taskCount-1]);
-                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(tasks.get(tasks.size()-1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     //Get user input again
                     userInput = userScan.nextLine();  
                 }
@@ -184,14 +133,11 @@ public class Duke {
                         throw new IllegalArgumentException("The deadline of a deadline cannot be empty. Add a / argument to specify time.");
                     }
                     //create a deadline task
-                    tasks[taskCount] = new Deadline(taskName,deadline);
-                    //increment the task count
-                    taskCount++;
+                    tasks.add(new Deadline(taskName,deadline));
                     //print out the task that was added
                     System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks[taskCount-1]);
-                    System.out.println("Now you have " + taskCount + " tasks in the list.");
-                    //Get user input again
+                    System.out.println(tasks.get(tasks.size()-1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     userInput = userScan.nextLine();  
                 }
 
@@ -210,13 +156,12 @@ public class Duke {
                     if(eventTime.equals("")){
                         throw new IllegalArgumentException("The event time of an event cannot be empty. Add a / argument to specify time.");
                     }
-                    tasks[taskCount] = new Event(taskName,eventTime);
-                    //increment the task count
-                    taskCount++;
+                    //create an event task
+                    tasks.add(new Event(taskName,eventTime));
                     //print out the task that was added
                     System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks[taskCount-1]);
-                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(tasks.get(tasks.size()-1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     //Get user input again
                     userInput = userScan.nextLine();  
                 }
@@ -248,7 +193,7 @@ public class Duke {
         userScan.close();
 
         //Write the tasks to the file
-        FileRW.writeToFile(tasks, taskCount);
+        FileRW.writeToFile(tasks);
         
     }
 
--- src/main/java/FileRW.java
+++ src/main/java/FileRW.java
@@ -1,108 +1,69 @@
-import java.util.Scanner;  // Import the Scanner class
-import java.io.File; // Import the File class
-import java.io.FileWriter; // Import the FileWriter class
-import java.io.BufferedWriter; // Import the BufferedWriter class
-import java.io.IOException; // Import the IOException class to handle errors
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 public class FileRW {
-   
-    //method that takes in input tasks and taskCount and reads the file and writes the tasks into the array
-    public static void readFromFile(Task[] tasks, int taskCount)  {
-        //check if there is a file to read from
-        try{
+
+    public static void readFromFile(ArrayList<Task> tasks) {
+        try {
             File file = new File("data/duke.txt");
             Scanner fileScan = new Scanner(file);
-            while(fileScan.hasNextLine()){
+            while (fileScan.hasNextLine()) {
                 String line = fileScan.nextLine();
                 String[] lineSplit = line.split(" \\| ");
-                if(lineSplit[0].equals("T")){
-                    tasks[taskCount] = new Todo(lineSplit[2]);
-                    if(lineSplit.length > 3){
-                        throw new DukeException("Your data file is corrupted");
+                Task newTask = null;
+
+                if (lineSplit[0].equals("T")) {
+                    newTask = new Todo(lineSplit[2]);
+                } else if (lineSplit[0].equals("D")) {
+                    newTask = new Deadline(lineSplit[2], lineSplit[3]);
+                } else if (lineSplit[0].equals("E")) {
+                    newTask = new Event(lineSplit[2], lineSplit[3]);
+                }
+
+                if (newTask != null) {
+                    if (lineSplit[1].equals("true")) {
+                        newTask.markAsDone();
                     }
-                    if(lineSplit[1].equals("true")){
-                        tasks[taskCount].markAsDone();
-                    }
-                    taskCount++;
-                }else if(lineSplit[0].equals("D")){
-                    if(lineSplit.length > 4){
-                        throw new DukeException("Your data file is corrupted");
-                    }
-                    tasks[taskCount] = new Deadline(lineSplit[2], lineSplit[3]);
-                    if(lineSplit[1].equals("true")){
-                        tasks[taskCount].markAsDone();
-                    }
-                    taskCount++;
-                }else if(lineSplit[0].equals("E")){
-                    if(lineSplit.length > 4){
-                        throw new DukeException("Your data file is corrupted");
-                    }
-                    tasks[taskCount] = new Event(lineSplit[2], lineSplit[3]);
-                    if(lineSplit[1].equals("true")){
-                        tasks[taskCount].markAsDone();
-                    }
-                    taskCount++;
+                    tasks.add(newTask);
                 }
             }
-            //print the tasks in the file
-            
             fileScan.close();
-        //otherwise create a new file
-        } catch(Exception e){
-            //Create a new file and directory
-            try{
+        } catch (Exception e) {
+            try {
                 File file = new File("data/duke.txt");
                 file.getParentFile().mkdirs();
                 file.createNewFile();
-            }catch(Exception e1){
+            } catch (Exception e1) {
                 e1.printStackTrace();
             }
         }
-        
     }
 
-    //method that takes in input tasks and taskCount and writes the tasks into the file
-    public static void writeToFile(Task[] tasks, int taskCount)  {
-        //Save the tasks to the file
-        try{
-            //Create a file writer
+    public static void writeToFile(ArrayList<Task> tasks) {
+        try {
             FileWriter fw = new FileWriter("data/duke.txt");
-            //Erase the file
-            fw.write("");
-            //Create a buffered writer
             BufferedWriter bw = new BufferedWriter(fw);
-            //Write the tasks to the file
-            for(int i=0;i<taskCount;i++){
-                //if the task is a todo
-                if(tasks[i] instanceof Todo){
-                    //write the task to the file
-                    bw.write("T | " + tasks[i].isDone + " | " + tasks[i].taskName);
-                    bw.newLine();
-                }
-                //if the task is a deadline
-                else if(tasks[i] instanceof Deadline){
-                    //write the task to the file
-                    bw.write("D | " + tasks[i].isDone + " | " + tasks[i].taskName + " | " + ((Deadline) tasks[i]).deadline);
-                    bw.newLine();
-                }
-                //if the task is an event
-                else if(tasks[i] instanceof Event){
-                    //write the task to the file
-                    bw.write("E | " + tasks[i].isDone + " | " + tasks[i].taskName + " | " + ((Event) tasks[i]).eventTime);
-                    bw.newLine();
-                }
-            } 
 
-            //Close the buffered writer
+            for (Task task : tasks) {
+                if (task instanceof Todo) {
+                    bw.write("T | " + task.isDone + " | " + task.taskName);
+                } else if (task instanceof Deadline) {
+                    bw.write("D | " + task.isDone + " | " + task.taskName + " | " + ((Deadline) task).deadline);
+                } else if (task instanceof Event) {
+                    bw.write("E | " + task.isDone + " | " + task.taskName + " | " + ((Event) task).eventTime);
+                }
+                bw.newLine();
+            }
+
             bw.close();
-    
-            //Close the file writer
             fw.close();
-
-        } catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Error writing to file.");
         }
-        
     }
 
 }
