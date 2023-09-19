d71609ec2af41742575b370dd66c9e1b55da56da
Benedict Chan <benedictchan247@gmail.com>
2023-09-20 01:03:46 +0800

    Add error handling for reading and writing to file

---
+++ src/main/java/Exceptions/KenReadFromFileException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class KenReadFromFileException extends KenException {
+    public KenReadFromFileException() {
+        super("Oops, darling! Reading from the file is like, so not happening right now!");
+    }
+}
