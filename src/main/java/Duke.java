7087e36f3109d1f4bed4ce38e7282eb624d6340c
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-09-19 17:23:36 +0800

    IntelliJ recommendations

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Duke {
     private static final String BYE_COMMAND = "bye";
     public static void main(String[] args) {
-        String logo = "\t _           _\n"
-                + "\t| |    _   _| | _____\n"
-                + "\t| |   | | | | |/ / _ \\\n"
-                + "\t| |___| |_| |   <  __/\n"
-                + "\t|_____|\\__,_|_|\\_\\___|\n";
+        String logo = """
+                \t _           _
+                \t| |    _   _| | _____
+                \t| |   | | | | |/ / _ \\
+                \t| |___| |_| |   <  __/
+                \t|_____|\\__,_|_|\\_\\___|
+                """;
         System.out.println("\t" + "Hello! I'm\n" + logo);
         System.out.println("\t" + "What can I do for you?");
 
@@ -25,57 +27,53 @@ public class Duke {
             ActionType action = ActionType.valueOf(words[0].toUpperCase());
 
             switch (action) {
-                case LIST:
+                case LIST -> {
                     System.out.println("\tHere are the tasks in your list:");
                     for (int i = 0; i < counter; i += 1) {
                         System.out.println("\t" + (i + 1) + "." + taskList[i]);
                     }
-                    break;
-                case MARK:
+                }
+                case MARK -> {
                     taskNumber = Integer.parseInt(words[1]) - 1;
                     System.out.println("\tWoohoo! You have accomplished:");
                     taskList[taskNumber].setDone(true);
                     System.out.println(taskList[taskNumber]);
-                    break;
-                case UNMARK:
+                }
+                case UNMARK -> {
                     taskNumber = Integer.parseInt(words[1]) - 1;
                     System.out.println("\tHA! You still have to complete:");
                     taskList[taskNumber].setDone(false);
                     System.out.println(taskList[taskNumber]);
-                    break;
-                case TODO:
+                }
+                case TODO -> {
                     taskDescription = echo.substring(4);
                     taskList[counter] = new Todo(taskDescription);
                     System.out.println("\tGot it. I've added this task:" + "\n" + taskList[counter]);
                     counter += 1;
                     System.out.println("\tNow you have " + counter + " tasks in the list.");
-                    break;
-                case DEADLINE:
+                }
+                case DEADLINE -> {
                     taskDescription = echo.substring(8);
                     slashCut = taskDescription.indexOf("/");
-
                     String taskDeadline = taskDescription.substring(slashCut + 1);
                     taskDescription = taskDescription.substring(0, slashCut);
-
                     taskList[counter] = new Deadline(taskDescription, taskDeadline);
                     System.out.println("\tGot it. I've added this task:" + "\n" + taskList[counter]);
                     counter += 1;
                     System.out.println("\tNow you have " + counter + " tasks in the list.");
-                    break;
-                case EVENT:
+                }
+                case EVENT -> {
                     taskDescription = echo.substring(5);
                     slashCut = taskDescription.indexOf("/");
-
                     String taskDuration = taskDescription.substring(slashCut);
                     taskDescription = taskDescription.substring(0, slashCut);
-
                     taskList[counter] = new Event(taskDescription, taskDuration);
                     System.out.println("\tGot it. I've added this task:" + "\n" + taskList[counter]);
                     counter += 1;
                     System.out.println("\tNow you have " + counter + " tasks in the list.");
-                    break;
-                default:
-                    break;
+                }
+                default -> {
+                }
             }
             echo = userInput.nextLine();
         }
