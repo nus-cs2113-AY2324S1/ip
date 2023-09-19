8254280a81a5436b10243b3932cf4fceac7d2a8b
lckjosh <lckjosh@gmail.com>
2023-09-19 23:21:56 +0800

    add ability to save and read from file

--- src/main/java/BotBuddy/Event.java
+++ src/main/java/BotBuddy/Event.java
@@ -3,16 +3,24 @@ package BotBuddy;
 public class Event extends Task {
 
     protected String from;
-    protected String by;
+    protected String to;
 
-    public Event(String description, String from, String by) {
+    public Event(String description, String from, String to) {
         super(description);
         this.from = from;
-        this.by = by;
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + by + ")";
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
