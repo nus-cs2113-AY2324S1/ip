41ce6df4089cdbcc836a9f00285cf3a2a19deb98
Dexter <dexmatt99@gmail.com>
2023-09-20 22:01:56 +0800

    Update tasks to set and get task contents

--- src/main/java/alan/task/Event.java
+++ src/main/java/alan/task/Event.java
@@ -5,13 +5,29 @@ public class Event extends Task {
     protected String to;
 
     public Event(String description, String from, String to) {
-        super(description);
+        super(description, TaskType.E);
         this.from  = from;
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
         this.to = to;
     }
 
     @Override
     public String toString() {
-        return "[E]" + "[" + super.getStatusIcon() + "] " + super.toString() + " (from: " + from + " | to: "+ to + ")";
+        return "[" + super.getTaskType() + "]" + "[" + super.getStatusIcon() + "] " + super.toString() + " (from: " + from + " | to: "+ to + ")";
     }
 }
