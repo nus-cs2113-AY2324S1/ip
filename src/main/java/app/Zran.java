d30b5ff17ba4410eea9aa9c8dd61dddfc5ab81bb
NaychiMin <naychimin02@gmail.com>
2023-09-21 09:19:27 +0800

    Implement creating of text file to store data, load data from text file and save data to text file in a new class `TaskListData`.

--- src/main/java/app/Zran.java
+++ src/main/java/app/Zran.java
@@ -2,16 +2,20 @@ package app;
 
 import exceptions.ZranExceptions;
 import taskmanagement.TaskItems;
+import taskmanagement.TaskListData;
+
+import java.io.IOException;
 
 public class Zran {
     static String outputFormat = "    ____________________________________________________________\n" +
             "    %s\n    ____________________________________________________________";
 
-    public static void main(String[] args) throws ZranExceptions {
+    public static void main(String[] args) throws ZranExceptions, IOException {
         System.out.printf((outputFormat) + "%n",
                 "hello! I'm app.Zran, your personal assistant:)! \n" +
                 "    Type in your to dos for the day and press enter! \n " +
                 "   Type 'help' to view to list of commands the bot accepts! ");
+        TaskListData.init();
         TaskItems.addItems();
     }
 
