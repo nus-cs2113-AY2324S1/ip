9a658fe741979d96351e7f6a5dfe46f9847d184f
NgLixuanNixon <e0726667@u.nus.edu>
2023-09-22 01:02:56 +0800

    Level 7 branch completed

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,8 +1,10 @@
+import org.json.JSONObject;
+
 public class Deadlines extends Task{
     private String dueDate;
 
-    public Deadlines(String taskName, String dueDate) {
-        super(taskName);
+    public Deadlines(String taskName, String dueDate, Boolean isDone) {
+        super(taskName, isDone);
         this.dueDate = dueDate;
     }
 
@@ -14,5 +16,15 @@ public class Deadlines extends Task{
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + this.dueDate + ")";
+    }
+
+    @Override
+    public JSONObject toJSONObject() {
+        JSONObject jo = new JSONObject();
+        jo.put("taskType", "D");
+        jo.put("isDone", this.getIsDone());
+        jo.put("taskName", this.getTaskName());
+        jo.put("dueDate", this.dueDate);
+        return jo;
     }
 }
--- src/main/java/ToDos.java
+++ src/main/java/ToDos.java
@@ -1,11 +1,22 @@
+import org.json.JSONObject;
+
 public class ToDos extends Task{
 
-    public ToDos(String toDoName) {
-        super(toDoName);
+    public ToDos(String toDoName, Boolean isDone) {
+        super(toDoName, isDone);
     }
 
     @Override
     public String toString() {
         return "[T]" + super.toString();
+    }
+
+    @Override
+    public JSONObject toJSONObject() {
+        JSONObject jo = new JSONObject();
+        jo.put("taskType", "T");
+        jo.put("isDone", this.getIsDone());
+        jo.put("taskName", this.getTaskName());
+        return jo;
     }
 }
