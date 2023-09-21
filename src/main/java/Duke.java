dd60c2ea1c95dde7a6377319bb4282718b96bd9f
ken_ruster <ken.ruster@gmail.com>
2023-09-21 11:34:43 +0800

    Add save and load functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,11 +4,80 @@ import Task.Task.Deadline;
 import Task.Task.Event;
 import Task.EmptyDescriptionException;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
 
 public class Duke {
 
     public static final String LINE_DIVIDER = "    _____________________________________";
+
+    public static void save(Task[] tasks) throws IOException {
+        Files.delete(Paths.get("./data/duke.txt"));
+        try {
+            FileWriter writer = new FileWriter("./data/duke.txt");
+            for (Task task: tasks) {
+                writer.write(task.toFileLine()
+                        + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            System.out.println(LINE_DIVIDER
+                    + "\n    Error saving file!\n"
+                    + LINE_DIVIDER);
+        }
+    }
+
+    public static Task[] load() {
+        Task[] out = new Task[100];
+        int i = 0;
+        try {
+            File in = new File("./data/duke.txt");
+            Scanner scanner = new Scanner(in);
+            while(scanner.hasNextLine()){
+                String[] params = scanner
+                        .nextLine()
+                        .split(" /");
+
+                switch(params[2]){
+                    case "TODO":
+                        ToDo todo = new ToDo(params[0],
+                                (params[1] == "true"));
+                        out[i] = todo;
+                        break;
+
+                    case "EVENT":
+                        Event event = new Event(params[0],
+                                (params[1] == "true"),
+                                params[3],
+                                params[4]);
+                        out[i] = event;
+                        break;
+
+                    case "DEADLINE":
+                        Deadline deadline = new Deadline(params[0],
+                                (params[1] == "true"),
+                                params[3]);
+                        out[i] = deadline;
+                        break;
+                    default:
+                }
+
+                i += 1;
+            }
+        } catch(NullPointerException e) {
+            System.out.println(LINE_DIVIDER
+                    + "\n    Where's the file, yo? I couldn't find it!\n"
+                    + LINE_DIVIDER);
+        } catch(FileNotFoundException e) {
+            return out;
+        }
+
+        return out;
+    }
 
     public static String RemoveCommandWord(String in) throws EmptyDescriptionException {
         int firstSpaceIndex = in.indexOf(" ") + 1;
@@ -119,7 +188,7 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         String buf = in.nextLine();
-        Task[] itemList = new Task[100];
+        Task[] itemList = load();
         int i = 0; // current index in itemList
 
         while(!buf.equalsIgnoreCase("bye")){
@@ -161,6 +230,13 @@ public class Duke {
                 break;
             }
             buf = in.nextLine();
+
+            try {
+                save(itemList);
+            } catch (IOException e) {
+                System.out.println("    I couldn't save your file, try again?\n"
+                        + LINE_DIVIDER);
+            }
         }
 
         System.out.println(LINE_DIVIDER +
