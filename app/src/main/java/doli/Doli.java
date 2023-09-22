2857bd3f50d95e015bc55192576b753af40d3a32
danielpappa <daniel.pappalardo@icloud.com>
2023-09-22 22:12:02 +0800

    Refactor Doli class

--- app/src/main/java/doli/Doli.java
+++ app/src/main/java/doli/Doli.java
@@ -1,411 +1,51 @@
 package doli;
 
-import doli.GUI.DialogBox;
-import doli.files.FileHandler;
+import doli.GUI.Ui;
+import doli.commands.Command;
+import doli.commands.Parser;
 import doli.exceptions.DoliExceptions;
-import doli.tasks.Deadline;
-import doli.tasks.Event;
-import doli.tasks.Task;
-import doli.tasks.ToDo;
+import doli.files.Storage;
+import doli.tasks.TaskList;
 
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.io.IOException;
 
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.Region;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+public class Doli {
 
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
 
-
-/** Very simple chatbot that sets up an agenda with various assignments and tasks
- *  Able to add, delete, view, clear, mark, unmark etc.
- *  Agenda is stored in working directory as Agenda.txt and retrieved when the bot is initialised
- */
-public class Doli extends Application {
-    private VBox dialogContainer;
-    private ScrollPane scrollPane;
-    private Button enterButton;
-    private TextField userInput;
-    private Scene scene;
-    private Image userProfile = new Image(this.getClass().getResourceAsStream("/images/user.png"));
-    private Image doliProfile = new Image(this.getClass().getResourceAsStream("/images/doli.png"));
-    private static final String LOGO = " ____       _\n" +
-            "|  _  \\    | | [_]\n" +
-            "| | | |____| |  _\n" +
-            "| |_| | [] | | | |\n" +
-            "|____/|____|__||_|\n";
-    private static final String BULLET_POINT = "\n\t\u2022 ";
-    private static final String INTRO = "I can help you create an agenda to manage your tasks.\n" +
-            "Simply use one of the below listed commands to proceed:" +
-            BULLET_POINT + "todo: adds a task with a DESCRIPTION" +
-            BULLET_POINT + "deadline /: adds a task with DESCRIPTION and /DEADLINE" +
-            BULLET_POINT + "event / /: adds a task with DESCRIPTION, /STARTDATE and /ENDDATE" +
-            BULLET_POINT + "mark: marks the task with the provided INDEX as done" +
-            BULLET_POINT + "unmark: resets the task with the provided INDEX as not done" +
-            BULLET_POINT + "count: returns the total number of tasks in the agenda" +
-            BULLET_POINT + "delete: deletes the task with the provided INDEX" +
-            BULLET_POINT + "clear: deletes all tasks within the agenda" +
-            BULLET_POINT + "list: lists all current entries in the agenda" +
-            BULLET_POINT + "bye: exits the program\n";
-    private static final String USERNAME = "User:";
-    private static final String CHATBOT = "Doli:";
-    private static final String WELCOME_MESSAGE = "Hello, my name is\n%s\n" + INTRO + "How can I help you?";
-    private static final String EXIT = "It was a pleasure, bye. See you later!";
-    private static final String HORIZONTAL_LINE = "________________________________";
-    private static final String ADDED_TASK_SUCCESSFULLY = "Got it! I've added the following task to your agenda:";
-    private static final String SUMMARIZING_CURRENT_AGENDA_ENTRIES = "Now you have a total of %d tasks in your agenda.";
-    private static final String ENCOURAGE_TO_MARK = "Would you like to mark/unmark something else?";
-    private static final String AGENDA_OVERVIEW = "Here is an overview of your agenda:";
-    private static final String FAILED_TO_FIND_TASK = "I couldn't find the task in your agenda, try another one.";
-    private static final String MARKED_SUCCESSFULLY = "I've successfully marked task %d as done.";
-    private static final String UNMARKED_SUCCESSFULLY = "I've unmarked task %d. You better get it done soon!";
-    private static final String TODO_COMMAND = "todo";
-    private static final String DEADLINE_COMMAND = "deadline";
-    private static final String EVENT_COMMAND = "event";
-    private static final String LIST_COMMAND = "list";
-    private static final String COUNT_COMMAND = "count";
-    private static final String DELETE_COMMAND = "delete";
-    private static final String CLEAR_COMMAND = "clear";
-    private static final String MARK_COMMAND = "mark";
-    private static final String UNMARK_COMMAND = "unmark";
-    private static final String EXIT_COMMAND = "bye";
-    private static final int NR_EVENT_ARGS = 3;
-    private static final int NR_DEADLINE_ARGS = 2;
-    private static final String UNRECOGNIZED_COMMAND = "I am so sorry, but I do not recognize this command. " +
-            "Please try typing something else.";
-    private static final Scanner IN = new Scanner(System.in);
-    public static ArrayList<Task> tasks;
-
-    public static void main(String[] args) throws DoliExceptions, IOException {
-
-        initializeAgenda();
-        welcomeUser();
-
-        while (true) {
-            String input = getInput();
-            handleCommand(input);
-            FileHandler.modifyFile(tasks);
-        }
-    }
-    public static void initializeAgenda() throws IOException, DoliExceptions {
-        tasks = new ArrayList<>();
-        /** Check if a file named Agenda.txt already exists in the wd
-         *  if yes, read its content and add it to the ArrayList tasks
-         *  if not, create a new empty text file
-         */
-        FileHandler.initializeFile(tasks);
-    }
-    public static void welcomeUser() {
-        printOutput(buildWelcomeMessage());
-    }
-    public static String buildWelcomeMessage() {
-        return String.format(WELCOME_MESSAGE, LOGO);
-    }
-    private static String buildCurrentSummary() {
-        return String.format(SUMMARIZING_CURRENT_AGENDA_ENTRIES, tasks.size());
-    }
-    public static String getInput() {
-        System.out.println(USERNAME);
-        String input = IN.nextLine();
-        return input;
-    }
-    public static void printOutput(String... arguments) {
-        System.out.println(CHATBOT);
-        for (String arg: arguments) {
-            System.out.println(arg);
-        }
-        System.out.println(HORIZONTAL_LINE);
-    }
-    public static void greetBye() {
-        printOutput(EXIT);
-        System.exit(0);
-    }
-
-    /** Extract both the command and if it exists also its input arguments
-     *  for example "list" is a single word, whereas a "todo" also expects a description
-     */
-    private static String[] extractCommandInfo(String input) {
-        String[] commandAndArgs = input.toLowerCase().split(" ", 2);
-        if (commandAndArgs.length == 2) {
-            return commandAndArgs;
-        } else {
-            return commandAndArgs = new String[]{commandAndArgs[0], ""};
-        }
-    }
-    private static boolean invalidEvent(String[] args) {
-        return args.length < NR_EVENT_ARGS;
-    }
-    private static boolean invalidDeadline(String[] args) {
-        return args.length < NR_DEADLINE_ARGS;
-    }
-    private static ToDo initializeNewTodo(String args) throws DoliExceptions {
-        if (args.trim().isEmpty()) {
-            throw new DoliExceptions("The description of a todo cannot be blank!");
-        }
-        ToDo newTodo = new ToDo(args);
-        return newTodo;
-    }
-    private static Deadline initializeNewDeadline(String args) throws DoliExceptions {
-        final String[] descriptionAndTiming = args.trim().split("/");
-        if (invalidDeadline(descriptionAndTiming)) {
-            throw new DoliExceptions("A deadline needs both a description and a time reference!");
-        }
-        final String description = descriptionAndTiming[0];
-        final String timing = descriptionAndTiming[1];
-        Deadline newDeadline = new Deadline(description, timing);
-        return newDeadline;
-    }
-    private static Event initializeNewEvent(String args) throws DoliExceptions {
-        final String[] descriptionAndTiming = args.trim().split("/");
-        if (invalidEvent(descriptionAndTiming)) {
-            throw new DoliExceptions("An event needs to contain a description, a start and an end date");
-        }
-        final String description = descriptionAndTiming[0];
-        final String startTime = descriptionAndTiming[1];
-        final String endTime = descriptionAndTiming[2];
-        Event newEvent = new Event(description, startTime, endTime);
-        return newEvent;
-    }
-    private static String addTodo(String arguments) throws DoliExceptions {
-        ToDo newTodo = initializeNewTodo(arguments);
-        tasks.add(newTodo);
-        final String TODO_DESCRIPTION = newTodo.toString();
-        return indentText(TODO_DESCRIPTION);
-    }
-    private static String addDeadline(String arguments) throws DoliExceptions {
-        Deadline newDeadline = initializeNewDeadline(arguments);
-        tasks.add(newDeadline);
-        final String DEADLINE_DESCRIPTION = newDeadline.toString();
-        return indentText(DEADLINE_DESCRIPTION);
-    }
-    private static String addEvent(String arguments) throws DoliExceptions {
-        Event newEvent = initializeNewEvent(arguments);
-        tasks.add(newEvent);
-        final String EVENT_DESCRIPTION = newEvent.toString();
-        return indentText(EVENT_DESCRIPTION);
-    }
-    private static boolean verifyValidTaskNumber(String number) {
-        int taskNumber = Integer.parseInt(number);
-        return taskNumber <= tasks.size();
-    }
-    private static String markedTaskSuccessfully(int taskNumber) {
-        return String.format(MARKED_SUCCESSFULLY, taskNumber);
-    }
-    private static String unmarkedTaskSuccessfully(int taskNumber) {
-        return String.format(UNMARKED_SUCCESSFULLY, taskNumber);
-    }
-    private static String markTask(String arg) {
-        int taskNumber = Integer.parseInt(arg);
-        tasks.get(taskNumber - 1).markTaskAsDone();
-        return markedTaskSuccessfully(taskNumber);
-    }
-    private static String unmarkTask(String arg) {
-        int taskNumber = Integer.parseInt(arg);
-        tasks.get(taskNumber - 1).markTaskAsNotDone();
-        return unmarkedTaskSuccessfully(taskNumber);
-    }
-    private static String indentText(String text) {
-        return "\t" + text;
-    }
-    private static String listTasks() {
-        StringBuilder agenda = new StringBuilder();
-        for (int i = 0; i < tasks.size(); i++) {
-            agenda.append(String.format("   %d. %s\n", i + 1, tasks.get(i).toString()));
-        }
-        return agenda.toString();
-    }
-    private static void deleteTask(String numberOfTaskToDelete) {
+    public Doli(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
         try {
-            removedTaskSuccessfully(Integer.parseInt(numberOfTaskToDelete) - 1);
-            tasks.remove(Integer.parseInt(numberOfTaskToDelete) - 1);
-        } catch(IndexOutOfBoundsException e) {
-            System.out.println(String.format("Watch out, there is no task number %s in your agenda.\n" +
-                    "Try a different task or add one", numberOfTaskToDelete));
+            tasks = new TaskList(storage.initializeFile());
+        } catch (DoliExceptions e) {
+            ui.showLoadingError();
+            tasks = new TaskList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
-    private static void removedTaskSuccessfully(int numberOfTask) {
-        String outputMessage = "Sure, I've removed this task:\n\t" +
-                tasks.get(numberOfTask).toString() +
-                String.format("Now you have a total of %d tasks in your agenda", tasks.size() - 1);
-        System.out.println(outputMessage);
-    }
-    private static String addMark(String arguments) {
-        if (verifyValidTaskNumber(arguments)) {
-            return markTask(arguments);
-        } else {
-            return FAILED_TO_FIND_TASK;
+    public void run() {
+        ui.welcomeUser();
+        boolean isExit = false;
+        while (!isExit) {
+            try {
+                String fullCommand = ui.getInput();
+                ui.printLine();
+                Command c = Parser.parseInputIntoCommand(fullCommand);
+                c.handleCommand(tasks, ui, storage);
+                c.getResponse();
+                isExit = c.isExit();
+            } catch (DoliExceptions e) {
+                ui.showError(e.getMessage());
+            } finally {
+                ui.printLine();
+            }
         }
     }
-    private static String undoMark(String arguments) {
-        if (verifyValidTaskNumber(arguments)) {
-            return unmarkTask(arguments);
-        } else {
-            return FAILED_TO_FIND_TASK;
-        }
-    }
-    private static void executeTodo(String task) {
-        try {
-            printOutput(ADDED_TASK_SUCCESSFULLY, addTodo(task), buildCurrentSummary());
-        } catch(DoliExceptions e) {
-            System.out.println("Could not add the todo task.");
-        }
-    }
-    private static void executeDeadline(String task) {
-        try {
-            printOutput(ADDED_TASK_SUCCESSFULLY, addDeadline(task), buildCurrentSummary());
-        } catch(DoliExceptions e) {
-            System.out.println("Could not add the deadline.");
-        }
-    }
-    private static void executeEvent(String task) {
-        try {
-            printOutput(ADDED_TASK_SUCCESSFULLY, addEvent(task), buildCurrentSummary());
-        } catch(DoliExceptions e) {
-            System.out.println("Could not add the event.");
-        }
-    }
-    private static void deleteAll() {
-        tasks = new ArrayList<>();
-    }
-    private static void printList(String list) throws IOException {
-        printOutput(AGENDA_OVERVIEW, list);
-    }
-    private static void executeMark(String arguments) {
-        printOutput(addMark(arguments), ENCOURAGE_TO_MARK);
-    }
-    private static void executeUnmark(String arguments) {
-        printOutput(undoMark(arguments), ENCOURAGE_TO_MARK);
-    }
-
-    /** Most important method of the both, responsible for identifying the specific command
-     * and carrying out the respective action. As default, the method does not know the command and
-     * encourages the user to type a new one.
-     * @param input
-     * @throws DoliExceptions
-     * @throws IOException
-     */
-    public static void handleCommand(String input) throws DoliExceptions, IOException {
-        final String command = extractCommandInfo(input)[0];
-        final String args = extractCommandInfo(input)[1];
-        switch (command) {
-        case TODO_COMMAND:
-            executeTodo(args);
-            break;
-        case DEADLINE_COMMAND:
-            executeDeadline(args);
-            break;
-        case EVENT_COMMAND:
-            executeEvent(args);
-            break;
-        case LIST_COMMAND:
-            printList(listTasks());
-            break;
-        case COUNT_COMMAND:
-            printOutput(String.valueOf(tasks.size()));
-            break;
-        case DELETE_COMMAND:
-            deleteTask(args);
-            break;
-        case CLEAR_COMMAND:
-            deleteAll();
-            break;
-        case MARK_COMMAND:
-            executeMark(args);
-            break;
-        case UNMARK_COMMAND:
-            executeUnmark(args);
-            break;
-        case EXIT_COMMAND:
-            greetBye();
-            break;
-        default:
-            printOutput(UNRECOGNIZED_COMMAND);
-            break;
-        }
-    }
-    @Override
-    public void start(Stage stage) {
-        Label hw = new Label("HW");
-        Scene sc = new Scene(hw);
-        stage.setScene(sc);
-        stage.show();
-
-        scrollPane = new ScrollPane();
-        userInput = new TextField();
-        enterButton = new Button("Enter");
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(
-                scrollPane,
-                userInput,
-                enterButton
-        );
-        scene = new Scene(mainLayout);
-        stage.setScene(scene);
-        stage.show();
-
-        stage.setTitle("Doli");
-        stage.setResizable(false);
-        stage.setMinWidth(600);
-        stage.setMinHeight(400);
-
-        mainLayout.setPrefSize(600, 400);
-        scrollPane.setPrefSize(585, 335);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPadding(new Insets(20, 20, 20, 20));
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-        userInput.setPrefWidth(505);
-        enterButton.setPrefWidth(85);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(enterButton, 1.0);
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setRightAnchor(enterButton, 1.0);
-
-        enterButton.setOnMouseClicked((event -> {
-            handleUserInput();
-        }));
-
-        userInput.setOnAction((event -> {
-            handleUserInput();
-        }));
-        dialogContainer.heightProperty().addListener(observable -> {
-            scrollPane.setVvalue(1.0);
-        });
-    }
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label doliText = new Label(getRespone(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialogBox(userText, new ImageView(userProfile)),
-                DialogBox.getDoliDialogBox(doliText, new ImageView(doliProfile))
-        );
-        userInput.clear();
-    }
-    private String getRespone(String input) {
-        return "Doli says: " + input;
-    }
-    public Doli() {
-
+    public static void main(String[] args) {
+        new Doli("Agenda.txt").run();
     }
 }
