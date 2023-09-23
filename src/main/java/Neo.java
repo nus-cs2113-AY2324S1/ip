740fcd6424565f8bcd2611b15226bb2151197dd6
NeoMinWei <>
2023-09-23 23:33:07 +0800

    Extract Parser related method to Parser class

--- src/main/java/Neo.java
+++ src/main/java/Neo.java
@@ -1,37 +1,15 @@
-import neo.task.Task;
 import java.util.ArrayList;
+import neo.task.Task;
 
 public class Neo {
-
-    private static void handleInput(String line, ArrayList<Task> list) {
-        while (!line.equals("bye")) {
-            if (line.equals("list")) {
-                TaskList.printList();
-            } else if (line.startsWith("mark")) {
-                TaskList.markTask(line);
-            } else if (line.startsWith("unmark")) {
-                TaskList.unmarkTask(line);
-            } else if (line.startsWith("delete")) {
-                TaskList.deleteTask(line);
-            } else if (line.startsWith("event")) {
-                TaskList.handleEvent(line);
-            } else if (line.startsWith("deadline")) {
-                TaskList.handleDeadline(line);
-            } else if (line.startsWith("todo")){
-                TaskList.handleTodo(line);
-            } else {
-                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
-            }
-            Storage.updateFile(list);
-            line = Ui.readInput();
-        }
-    }
-
     public static void main(String[] args) {
         Storage.findFile(TaskList.getList());
         Ui.welcomeMessage();
+
         String input = Ui.readInput();
-        handleInput(input, TaskList.getList());
+        ArrayList<Task> list = TaskList.getList();
+
+        Parser.handleInput(input, list);
         Ui.byeMessage();
     }
 }
