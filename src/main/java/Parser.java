e1ed451d278863dfb453df115397cccb69e8f948
NeoMinWei <>
2023-09-23 23:32:42 +0800

    Create Parser class

---
+++ src/main/java/Parser.java
@@ -0,0 +1,28 @@
+import neo.task.Task;
+import java.util.ArrayList;
+
+public abstract class Parser {
+    public static void handleInput(String input, ArrayList<Task> list) {
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                TaskList.printList();
+            } else if (input.startsWith("mark")) {
+                TaskList.markTask(input);
+            } else if (input.startsWith("unmark")) {
+                TaskList.unmarkTask(input);
+            } else if (input.startsWith("delete")) {
+                TaskList.deleteTask(input);
+            } else if (input.startsWith("event")) {
+                TaskList.handleEvent(input);
+            } else if (input.startsWith("deadline")) {
+                TaskList.handleDeadline(input);
+            } else if (input.startsWith("todo")){
+                TaskList.handleTodo(input);
+            } else {
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+            }
+            Storage.updateFile(list);
+            input = Ui.readInput();
+        }
+    }
+}
