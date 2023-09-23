e3c5e6f7655b8e8cad0ea56fad68e2df7aae672c
NeoMinWei <>
2023-09-23 23:24:38 +0800

    Extract methods related to TaskList into TaskList class

--- src/main/java/Neo.java
+++ src/main/java/Neo.java
@@ -1,256 +1,37 @@
-import neo.exception.NeoException;
-import neo.task.Deadline;
-import neo.task.Event;
 import neo.task.Task;
-import neo.task.Todo;
-import neo.type.CommandType;
-import neo.type.ErrorType;
 import java.util.ArrayList;
 
 public class Neo {
-    private final Storage storage;
-    private final ArrayList<Task> list = new ArrayList<>();
 
-    public static void printList(ArrayList<Task> list) {
-        int listIndex = 1;
-
-        System.out.println("Here are the tasks in your list:");
-        for (Task task : list) {
-            System.out.print((listIndex) + ". ");
-            System.out.println(task);
-            listIndex++;
-        }
-    }
-
-    public static void printAddedTask(ArrayList<Task> list) {
-        int listSize = list.size();
-        int arrayListIndex = listSize - 1;
-
-        System.out.println("Got it. I've added this task:");
-        System.out.println("    " + list.get(arrayListIndex));
-        System.out.print("Now you have " + listSize);
-        if (listSize == 1) {
-            System.out.println(" task in the list.");
-        } else {
-            System.out.println(" tasks in the list.");
-        }
-    }
-
-    public static void markTask(String line, ArrayList<Task> list) {
-        try {
-            String[] words = line.split(" ");
-            int listIndex = Integer.parseInt(words[1]);
-            int arrayListIndex = listIndex - 1;
-
-            Task toMark = list.get(arrayListIndex);
-            toMark.setDone(true);
-            list.set(arrayListIndex, toMark);
-            System.out.println("Nice! I've marked this task as done: ");
-            System.out.println("    " + list.get(arrayListIndex));
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("OOPS!!! Please give the index of which task to mark.");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("OOPS!!! Please make sure the index given is within the current list.");
-        }
-    }
-
-    public static void unmarkTask(String line, ArrayList<Task> list) {
-        try {
-            String[] words = line.split(" ");
-            int listIndex = Integer.parseInt(words[1]);
-            int arrayListIndex = listIndex - 1;
-
-            Task toUnmark = list.get(arrayListIndex);
-            toUnmark.setDone(false);
-            list.set(arrayListIndex, toUnmark);
-            System.out.println("OK, I've marked this task as not done yet: ");
-            System.out.println("    " + list.get(arrayListIndex));
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("OOPS!!! Please give the index of which task to unmark.");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("OOPS!!! Please make sure the index given is within the current list.");
-        }
-    }
-
-    public static void deleteTask(String line, ArrayList<Task> list) {
-        try {
-            String[] words = line.split(" ");
-            int listIndex = Integer.parseInt(words[1]);
-            int arrayListIndex = listIndex - 1;
-
-            Task toRemove = list.get(arrayListIndex);
-            list.remove(arrayListIndex);
-            int listSize = list.size();
-
-            System.out.println("Noted. I've removed this task:");
-            System.out.println("    " + toRemove);
-            System.out.print("Now you have ");
-            if (listSize == 1) {
-                System.out.println(listSize + " task in the list.");
-            } else {
-                System.out.println(listSize + " tasks in the list.");
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("OOPS!!! Please give the index of which task to delete.");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("OOPS!!! Please make sure the index given is within the current list.");
-        }
-    }
-
-    public static void catchFormatError(CommandType type, String line) throws NeoException {
-        switch (type) {
-        case TODO:
-            if (line.contains("/from") || (line.contains("/to"))) {
-                throw new NeoException("/from", ErrorType.MISUSE);
-            }
-            if (line.contains("/by")) {
-                throw new NeoException("/by", ErrorType.MISUSE);
-            }
-            break;
-        case DEADLINE:
-            if (line.contains("/from") || (line.contains("/to"))) {
-                throw new NeoException("/from", ErrorType.MISUSE);
-            }
-            if (!line.contains("/by")) {
-                throw new NeoException("/by", ErrorType.FORMAT);
-            }
-            break;
-        case EVENT:
-            if (line.contains("/by")) {
-                throw new NeoException("/by", ErrorType.MISUSE);
-            }
-            if (!line.contains("/from")) {
-                throw new NeoException("/from", ErrorType.FORMAT);
-            }
-            if (!line.contains("/to")) {
-                throw new NeoException("/to", ErrorType.FORMAT);
-            }
-            break;
-        }
-    }
-
-    public static void catchEmptyDescription(String field, String description) throws NeoException {
-        if (description.isBlank()) {
-            throw new NeoException(field, ErrorType.EMPTY);
-        }
-    }
-
-    public static void handleEvent(String line, ArrayList<Task> list) {
-        try {
-            addEvent(line, list);
-        } catch (NeoException e) {
-            e.printException();
-        }
-    }
-
-    public static void addEvent(String line, ArrayList<Task> list) throws NeoException {
-        catchFormatError(CommandType.EVENT, line);
-
-        int fromIndex = line.indexOf("/from");
-        int toIndex = line.indexOf("/to");
-        int fromStringLength = 5;
-        int toStringLength = 3;
-        int eventStringLength = 5;
-
-        String description = line.substring(eventStringLength, fromIndex).trim();
-        String from = line.substring(fromIndex + fromStringLength, toIndex).trim();
-        String to = line.substring(toIndex + toStringLength).trim();
-
-        catchEmptyDescription("description", description);
-        catchEmptyDescription("/from", from);
-        catchEmptyDescription("/to", to);
-
-        Event toAdd = new Event(description, from, to);
-        list.add(toAdd);
-        printAddedTask(list);
-    }
-
-    public static void handleDeadline(String line, ArrayList<Task> list) {
-        try {
-            addDeadline(line, list);
-        } catch (NeoException e) {
-            e.printException();
-        }
-    }
-
-    public static void addDeadline(String line, ArrayList<Task> list) throws NeoException {
-        catchFormatError(CommandType.DEADLINE, line);
-
-        int byIndex = line.indexOf("/by");
-        int byStringLength = 3;
-        int deadlineStringLength = 8;
-
-        String description = line.substring(deadlineStringLength, byIndex).trim();
-        String by = line.substring(byIndex + byStringLength).trim();
-
-        catchEmptyDescription("description", description);
-        catchEmptyDescription("/by", by);
-
-        Deadline toAdd = new Deadline(description, by);
-        list.add(toAdd);
-        printAddedTask(list);
-    }
-
-    public static void handleTodo(String line, ArrayList<Task> list) {
-        try {
-            addTodo(line,list);
-        } catch (NeoException e) {
-            e.printException();
-        }
-    }
-    public static void addTodo(String line, ArrayList<Task> list) throws NeoException {
-        catchFormatError(CommandType.TODO, line);
-
-        int todoStringLength = 4;
-
-        String description = line.substring(todoStringLength).trim();
-
-        catchEmptyDescription("description", description);
-
-        Todo toAdd = new Todo(description);
-        list.add(toAdd);
-        printAddedTask(list);
-    }
-
-
-    private void handleInput(String line, ArrayList<Task> list) {
+    private static void handleInput(String line, ArrayList<Task> list) {
         while (!line.equals("bye")) {
             if (line.equals("list")) {
-                printList(list);
+                TaskList.printList();
             } else if (line.startsWith("mark")) {
-                markTask(line, list);
+                TaskList.markTask(line);
             } else if (line.startsWith("unmark")) {
-                unmarkTask(line, list);
+                TaskList.unmarkTask(line);
             } else if (line.startsWith("delete")) {
-                deleteTask(line, list);
+                TaskList.deleteTask(line);
             } else if (line.startsWith("event")) {
-                handleEvent(line, list);
+                TaskList.handleEvent(line);
             } else if (line.startsWith("deadline")) {
-                handleDeadline(line, list);
+                TaskList.handleDeadline(line);
             } else if (line.startsWith("todo")){
-                handleTodo(line, list);
+                TaskList.handleTodo(line);
             } else {
                 System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
-            storage.updateFile(list);
+            Storage.updateFile(list);
             line = Ui.readInput();
         }
     }
 
-
-
-    public Neo(String filePath, String fileDirectory) {
-        storage = new Storage(filePath, fileDirectory);
-        storage.findFile(list);
-    }
-
-    public void run() {
+    public static void main(String[] args) {
+        Storage.findFile(TaskList.getList());
         Ui.welcomeMessage();
         String input = Ui.readInput();
-        handleInput(input, list);
+        handleInput(input, TaskList.getList());
         Ui.byeMessage();
-    }
-    public static void main(String[] args) {
-        new Neo("data/data.txt", "data").run();
     }
 }
