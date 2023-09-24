f4829494c3b823e249f1c151784a04ff469adcd5
ryan1604 <ryanchua1604@gmail.com>
2023-09-24 19:04:18 +0800

    Add Find command

---
+++ src/main/java/RC/command/Find.java
@@ -0,0 +1,31 @@
+package RC.command;
+
+import RC.RCException;
+import RC.TaskList;
+import RC.UI.Ui;
+import RC.task.Task;
+
+public class Find extends RCCommand {
+    private String keyword;
+    private static final String MESSAGE_FIND_TASKS = "\tHere are the matching tasks in your list:";
+    private static final String MESSAGE_EMPTY = "\tOOPS!!! Please ensure the search keyword isn't empty.";
+
+    public Find(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui) throws RCException {
+        if (keyword.isEmpty()) {
+            throw new RCException(MESSAGE_EMPTY);
+        }
+
+        ui.showMessage(MESSAGE_FIND_TASKS);
+        for (Task task : taskList.tasks) {
+            String taskDesc = task.toString();
+            if (taskDesc.contains(keyword)) {
+                ui.showMessage("\t  " + taskDesc);
+            }
+        }
+    }
+}
