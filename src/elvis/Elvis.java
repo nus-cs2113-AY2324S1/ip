23974f1040faff1f8cbff3f7db22459457e4f66d
woodenclock <luckyisland3710@gmail.com>
2023-09-24 21:01:02 +0800

    Update main

--- src/elvis/Elvis.java
+++ src/elvis/Elvis.java
@@ -1,8 +1,10 @@
 package elvis;
+
+import elvis.operation.Storage;
+
 public class Elvis {
 
     public static void main(String[] args) {
-        //Functionalities of ChatBot
-        FileManager.fileManager();
+        Storage.fileManager();
     }
 }
