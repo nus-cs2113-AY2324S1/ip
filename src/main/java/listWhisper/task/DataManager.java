ade98f6dc61a44dca770e08a9c2f22eee2968214
ziyi105 <ahziyi@gmail.com>
2023-09-24 22:20:11 +0800

    Modify Saver.java according to TaskS and rename it to DataManager.java

--- src/main/java/listWhisper/task/DataManager.java
+++ src/main/java/listWhisper/task/DataManager.java
@@ -1,24 +1,26 @@
 package listWhisper.task;
 
+// Inspired by TaskS
 import java.io.*;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 public abstract class Saver {
     private final static String userWorkingDirectory = System.getProperty("user.dir");
     private final static  java.nio.file.Path FILE_PATH = java.nio.file.Paths.get(userWorkingDirectory, "data.txt");
 
-    public static void saveList(List list) throws IOException {
-        clearFile();
-        Files.writeString(FILE_PATH, list.toString());
+    public static void saveList(ListOfTasks listOfTasks) throws IOException {
+        Files.delete(FILE_PATH);
+        Files.writeString(FILE_PATH, listOfTasks.toString());
     }
 
-    private static void clearFile() throws IOException {
+    private static ArrayList<String> readFile() throws IOException {
         boolean directoryExists = java.nio.file.Files.exists(FILE_PATH);
 
         if (!directoryExists) {
             Files.createFile(FILE_PATH);
-        } else {
-            Files.delete(FILE_PATH);
-            Files.createFile(FILE_PATH);
         }
+
+        return (ArrayList<String>) Files.readAllLines(FILE_PATH);
     }
 }
