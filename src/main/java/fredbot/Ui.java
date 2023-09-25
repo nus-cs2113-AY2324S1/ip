42acd6111065f4178a4f19173dc59db463185ace
wwweert123 <puachengxuan.fp@gmail.com>
2023-09-25 22:32:14 +0800

    Edit Ui class to print welcome message

--- src/main/java/fredbot/Ui.java
+++ src/main/java/fredbot/Ui.java
@@ -1,8 +1,18 @@
 package fredbot;
 
 public class Ui {
+    public static final String INDENT = "    ";
+    public static final String DIVIDER = "    ____________________________________________________________\n";
+    public static final String GREETING = "     Hello! I'm Fredbot\n" +
+            "     What can I do for you?";
+
+    public void printMessage(String message) {
+        System.out.print(DIVIDER);
+        System.out.println(message);
+        System.out.println(DIVIDER);
+    }
     public void showWelcome() {
-        System.out.println();
+        printMessage(INDENT + GREETING);
     }
 
 }
