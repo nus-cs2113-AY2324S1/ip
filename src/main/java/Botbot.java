25b91a05d1590a31c1f8fd27acee01732fb65169
TriciaBK <e1032477@u.nus.edu>
2023-09-25 20:53:31 +0800

    added saving and loading data feature

--- src/main/java/Botbot.java
+++ src/main/java/Botbot.java
@@ -1,6 +1,14 @@
 //import scanner
-import javax.lang.model.type.NullType;
 import java.util.Scanner;
+import javax.lang.model.type.NullType;
+//import File I/O class
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 public class Botbot {
     public static String line = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
@@ -129,9 +137,93 @@ public class Botbot {
         System.out.println(line);
     }
 
+//    //method to write to file
+//    public static void writeToFile(String filePath, String textToAdd) throws IOException {
+//        FileWriter fileWriter = new FileWriter("./data/botbot.txt");
+//        fileWriter.write(textToAdd);
+//        fileWriter.close();
+//    }
+    //method to save list to file
+    public static void saveListToFile() throws IOException {
+        try (FileWriter fileWriter = new FileWriter("data/botbot.txt")){
+            for (int i = 0; i < listSize; i++) {
+                Task task = list[i];
+                fileWriter.write(task.toString() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+    }
+
+    //method to extract todo task
+    public static void extractTodo(String savedTask){
+        createTodoTasks(savedTask.substring(8));
+    }
+
+    //method to extract deadline task
+    public static void extractDeadline(String savedTask) throws DukeException {
+        String[] parts = savedTask.split(" \\(by: " );
+        String task = parts[0].substring(8);
+        String deadline = parts[1].substring(0, parts[1].length() - 1);
+        if (task.isEmpty() || deadline.isEmpty()) {
+                throw new DukeException("Loaded task or deadline is empty... Please check your saved list again~");
+        } else {
+            //instantiate new deadline object
+            Deadline deadlineTask = new Deadline(task, deadline);
+            //add to array
+            list[listSize] = deadlineTask;
+            listSize++;
+        }
+    }
+
+    //method to extract event task
+    public static void extractEvent(String savedTask) throws DukeException {
+        String[] parts = savedTask.split(" \\(");
+        String task = parts[0].substring(8);
+        String[] timeSplit = parts[1].split(" to: ");
+        String from = timeSplit[0].substring(6);
+        String to = timeSplit[1].substring(0, timeSplit[1].length() - 1);
+        if (task.isEmpty() || from.isEmpty() || to.isEmpty()) {
+            throw new DukeException("Loaded task or time period is empty... Please check your saved list again~");
+        } else {
+            //instantiate new event object
+            Event eventTask = new Event(task, from, to);
+            //add to array
+            list[listSize] = eventTask;
+            listSize++;
+        }
+    }
+
+    // method to load list from file
+    public static void loadListFromFile() throws IOException, DukeException {
+        File file = new File("data/botbot.txt");
+        if (!file.exists()){
+            try {
+                boolean createdDirectory = file.mkdirs();
+                boolean createdFile = file.createNewFile();
+            } catch (IOException e){
+                System.out.println("Something went wrong: " + e.getMessage());
+            }
+        } else {
+            Scanner fileScanner = new Scanner(file);
+            while (fileScanner.hasNext()) {
+                String savedTask = fileScanner.nextLine();
+                if (savedTask.contains("T")) {
+                    extractTodo(savedTask);
+                } else if (savedTask.contains("D")) {
+                    extractDeadline(savedTask);
+                } else if (savedTask.contains("E")) {
+                    extractEvent(savedTask);
+                }
+            }
+        }
+    }
+
 
     //main method
-    public static void main(String[] args){
+    public static void main(String[] args) throws DukeException, IOException {
+        //load file
+        loadListFromFile();
         //message
         System.out.println("Hello! I'm Botbot \n" +
                 "───────────────────────────────────────────────────────────────────────────────────────────────\n" +
@@ -176,20 +268,24 @@ public class Botbot {
                         break;
                     case "mark":
                         markUnmarkTask(input);
+                        saveListToFile();
                         break;
                     case "todo":
                         createTodoTasks(input.substring(5));
+                        saveListToFile();
                         break;
                     case "deadline":
                         createDeadlineTasks(input);
+                        saveListToFile();
                         break;
                     case "event":
                         createEventTask(input);
+                        saveListToFile();
                         break;
                     default:
                         return;
                 }
-            } catch (DukeException e) {
+            } catch (DukeException | IOException e) {
                 System.out.println(e.getMessage() + "\n" + line);
             }
         }
