b970da98b5912e092c1eecb36a7f0f18a1f91403
spinoandraptos <spinoandraptos@gmail.com>
2023-09-26 22:28:04 +0800

    Fixed UI commands display

--- src/main/java/rene/parser/Parser.java
+++ src/main/java/rene/parser/Parser.java
@@ -46,12 +46,14 @@ public class Parser {
             if (exceptionMessage.equals("Invalid Input")) {
                 System.out.println("    Pwease enter a valid command :0");
                 System.out.println("    Valid commands are: todo,\n" +
-                        "                        deadline: /by [time],\n" +
-                        "                        event: /from [start] /to [end],\n" +
+                        "                        deadline /by [time],\n" +
+                        "                        event /from [start] /to [end],\n" +
                         "                        list,\n" +
                         "                        mark [task number],\n" +
                         "                        unmark [task number],\n" +
                         "                        delete [task number],\n" +
+                        "                        find /description [description]\n" +
+                        "                        find /time [time]\n" +
                         "                        bye");
             }
         }
