1332c3b58de35c394cad74e81e59cd25021b54ec
ziyi105 <ahziyi@gmail.com>
2023-09-27 17:08:10 +0800

    Move DataManager.java from package listWhisper.task to storage

---
+++ src/main/java/storage/DataManager.java
@@ -0,0 +1,48 @@
+package storage;
+
+// Inspired by TaskS
+import listWhisper.task.Parser;
+import listWhisper.task.TaskList;
+import ui.Ui;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+public class DataManager {
+    private final static String userWorkingDirectory = System.getProperty("user.dir");
+    private final static  java.nio.file.Path FILE_PATH = java.nio.file.Paths.get(userWorkingDirectory, "data.txt");
+    private final Ui ui;
+
+    public DataManager(Ui ui) {
+        this.ui = ui;
+    }
+
+    public static void saveList(TaskList taskList) {
+        try {
+            Files.delete(FILE_PATH);
+            Files.writeString(FILE_PATH, taskList.prepareSaveList());
+        } catch (IOException e) {
+            Ui.showError(e);
+        }
+    }
+
+    public TaskList load(Parser parser) throws IOException {
+        TaskList taskList = new TaskList();
+        ArrayList<String> data = this.readFile();
+        for (String line : data) {
+            parser.parseLoadingData(taskList, line);
+        }
+        return taskList;
+    }
+
+    public ArrayList<String> readFile() throws IOException {
+        boolean directoryExists = java.nio.file.Files.exists(FILE_PATH);
+
+        if (!directoryExists) {
+            Files.createFile(FILE_PATH);
+        }
+
+        return (ArrayList<String>) Files.readAllLines(FILE_PATH);
+    }
+}
