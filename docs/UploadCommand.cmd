5268bee0a7ed47a7d8dbd8c0d3b11b6783db56c1
MrOPPA1 <3105784412@qq.com>
2023-09-28 02:32:21 +0800

    A-CodingStandard

--- docs/UploadCommand.cmd
+++ docs/UploadCommand.cmd
@@ -1,6 +1,6 @@
 rem customize the tag name and message here
-set TAG_NAME=Level-3
-set MESSAGE="Mark as Done"
+set TAG_NAME=A-CodingStandard
+set MESSAGE="A-CodingStandard"
 
 rem create a branch
 git branch branch-%TAG_NAME%
