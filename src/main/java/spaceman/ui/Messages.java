ba17a8daf871f96b1840ed95539d8a0d5b6c130e
ChungZhiXuan <zhixuanchung03@gmail.com>
2023-09-27 14:23:35 +0800

    Bug fix: Remove typo

--- src/main/java/spaceman/ui/Messages.java
+++ src/main/java/spaceman/ui/Messages.java
@@ -1,5 +1,9 @@
-package ui;
+package spaceman.ui;
 
+/**
+ * Container for user-visible messages used in the application.
+ * These messages are used to provide feedback and information to the user.
+ */
 public class Messages {
     public static final String LINE = "------------------------------------------------------------";
     public static final String LOGO = "  ____  _____   ___    _____ _____ __    __   ___   __   __\n"
@@ -10,8 +14,8 @@ public class Messages {
             + "|____ /|_|   |__| |__|\\_____|_____|__|  |__|__| |__|__|\\___|\n";
     public static final String MESSAGE_BYE = "Bye. Hope to see you again soon!";
     public static final String MESSAGE_UNKNOWN = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
-    public static final String MESSAGE_MARK = "Nice! I've marked this data.task as done:";
-    public static final String MESSAGE_UNMARK = "OK, I've marked this data.task as not done yet:";
+    public static final String MESSAGE_MARK = "Nice! I've marked this task as done:";
+    public static final String MESSAGE_UNMARK = "OK, I've marked this task as not done yet:";
     public static final String MESSAGE_EMPTY_TODO = "☹ OOPS!!! The description of a todo cannot be empty.";
     public static final String MESSAGE_EMPTY_DEADLINE = "☹ OOPS!!! The description of a deadline cannot be empty.";
     public static final String MESSAGE_EMPTY_EVENT = "☹ OOPS!!! The description of an event cannot be empty.";
