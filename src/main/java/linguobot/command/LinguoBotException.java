08360a42f5143d3ab4e9ae6958ad6636b2abc9bf
WooKaiNing <e0968128@u.nus.edu>
2023-09-27 12:52:32 +0800

    Add javaDoc comments for all classes

--- src/main/java/linguobot/command/LinguoBotException.java
+++ src/main/java/linguobot/command/LinguoBotException.java
@@ -1,5 +1,14 @@
 package linguobot.command;
+/**
+ * The <code>LinguoBotException</code> class represents a custom exception used in the LinguoBot application.
+ * It extends the standard Java `Exception` class and is used to handle specific application-related exceptions.
+ */
 public class LinguoBotException extends Exception {
+
+    /**
+     * Constructs a new <code>LinguoBotException</code> with the specified error message to be displayed.
+     * @param message the error message associated with the exception.
+     */
     public LinguoBotException(String message) {
         super(message);
     }
