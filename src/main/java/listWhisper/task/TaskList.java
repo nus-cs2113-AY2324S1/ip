495ae3a2fe51f1f9d5fdd57cba78af74dac04afd
ziyi105 <ahziyi@gmail.com>
2023-09-27 17:09:10 +0800

    Rename List.java toTaskList.java

--- src/main/java/listWhisper/task/TaskList.java
+++ src/main/java/listWhisper/task/TaskList.java
@@ -1,9 +1,7 @@
 package listWhisper.task;
 import storage.DataManager;
-import common.StringSplitter;
 import listWhisper.exceptions.DescriptionFormatException;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class TaskList {
@@ -18,89 +16,50 @@ public class TaskList {
     }
 
     public Task getTask(int taskId) {
-        return this.tasks.get(taskId - 1);
+        return this.tasks.get(taskId);
     }
 
     //Actions
-    public Todo addTodo(String description) throws DescriptionFormatException {
-        if (description.equals("")) {
-            throw new DescriptionFormatException(
-                    "Wrong todo format. Follow this format to add a todo: "
-                            + "todo [todo description]");
-        }
+    public Todo addTodo(String description) {
         Todo todo = new Todo(description);
         this.tasks.add(todo);
         return todo;
     }
 
-    public Deadline addDeadline(String description) throws DescriptionFormatException {
-        try {
-            String[] descriptionAndTime = StringSplitter.splitInputIntoDeadlineFormat(description);
-            Deadline deadline = new Deadline(descriptionAndTime[0], descriptionAndTime[1]);
-            this.tasks.add(deadline);
-            return deadline;
-        } catch (DescriptionFormatException e) {
-            throw e;
-        }
+    public Deadline addDeadline(String description, String by) {
+        Deadline deadline = new Deadline(description, by);
+        this.tasks.add(deadline);
+        return deadline;
     }
 
-    public Event addEvent(String description) throws DescriptionFormatException {
-        try {
-            String[] descriptionAndTime = StringSplitter.splitInputIntoEventFormat(description);
-            Event event = new Event(descriptionAndTime[0],
-                    descriptionAndTime[1], descriptionAndTime[2]);
+    public Event addEvent(String description, String startTime, String endTime) {
+            Event event = new Event(description, startTime, endTime);
             this.tasks.add(event);
             return event;
-        } catch (DescriptionFormatException e) {
-            throw e;
-        }
     }
 
-    public Task mark(String input) throws DescriptionFormatException {
-        try {
-            int taskId = getTaskId(input);
-            Task task = this.tasks.get(taskId - 1);
-            task.setDone(true);
-            return task;
-        } catch (DescriptionFormatException e) {
-            throw e;
-        }
+    public void mark(int taskId) {
+        Task task = this.tasks.get(taskId);
+        task.setDone(true);
     }
 
-    public Task unmark(String input) throws DescriptionFormatException {
-        try {
-            int taskId = getTaskId(input);
-            this.tasks.get(taskId - 1).setDone(false);
-            return this.tasks.get(taskId - 1);
-        } catch (DescriptionFormatException e) {
-            throw e;
-        }
+    public void unmark(int taskId) {
+        Task task = this.tasks.get(taskId);
+        task.setDone(true);
     }
 
-    public Task delete(String input) throws DescriptionFormatException {
-        try {
-            int taskId = getTaskId(input);
-            Task task = this.tasks.get(taskId - 1);
+    public Task delete(int taskId) {
+            Task task = this.tasks.get(taskId);
             this.tasks.remove(taskId - 1);
             return task;
-        } catch (DescriptionFormatException e) {
-            throw e;
-        }
     }
 
-    // Methods related to data management
-    public void load(ArrayList<String> data) throws DescriptionFormatException {
-        for (String line : data) {
-            TaskClassifier.classifyTaskForLoading(this, line);
-        }
-    }
-
-    public void saveList() throws IOException {
+    public String prepareSaveList() {
         StringBuilder listOfFormattedTasks = new StringBuilder();
         for (Task task : this.tasks) {
             listOfFormattedTasks.append(task.formatAsInput()).append("\n");
         }
-        DataManager.saveList(listOfFormattedTasks.toString());
+        return listOfFormattedTasks.toString();
     }
 
     // Miscellaneous methods
