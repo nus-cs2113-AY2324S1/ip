e6efffc9b57e7f196535401a80f55322ee71afb8
ChungZhiXuan <zhixuanchung03@gmail.com>
2023-09-27 14:22:53 +0800

    Extract methods

--- src/main/java/spaceman/storage/Storage.java
+++ src/main/java/spaceman/storage/Storage.java
@@ -35,6 +35,17 @@ public class Storage {
     public ArrayList<Task> readDataFromFile() throws FileNotFoundException {
         ArrayList<Task> tasks = new ArrayList<>();
         File file = new File(filePath);
+        taskDecoder(file, tasks);
+        return tasks;
+    }
+
+    /**
+     * Decodes the storage data file and store it into the arraylist of tasks.
+     * @param file storage data file
+     * @param tasks arraylist of tasks
+     * @throws FileNotFoundException If the storage data file does not exist.
+     */
+    private static void taskDecoder(File file, ArrayList<Task> tasks) throws FileNotFoundException {
         Scanner data = new Scanner(file);
         while (data.hasNext()) {
             String taskDetails = data.nextLine();
@@ -43,25 +54,21 @@ public class Storage {
             int isMarked = Integer.parseInt(descriptions[1].trim());
             switch (taskType) {
             case 'T':
-                Task todo = new Todo(descriptions[2].trim(), isMarked);
-                tasks.add(todo);
+                tasks.add(new Todo(descriptions[2].trim(), isMarked));
                 break;
             case 'D':
                 LocalDateTime formattedDate = LocalDateTime.parse(descriptions[3].trim());
-                Task deadline = new Deadline(descriptions[2].trim(), formattedDate, isMarked);
-                tasks.add(deadline);
+                tasks.add(new Deadline(descriptions[2].trim(), formattedDate, isMarked));
                 break;
             case 'E':
                 LocalDateTime formattedStart= LocalDateTime.parse(descriptions[3].trim());
                 LocalTime formattedEnd = LocalTime.parse(descriptions[4].trim());
-                Task event = new Event(descriptions[2].trim(), formattedStart, formattedEnd, isMarked);
-                tasks.add(event);
+                tasks.add(new Event(descriptions[2].trim(), formattedStart, formattedEnd, isMarked));
                 break;
             default:
                 break;
             }
         }
-        return tasks;
     }
 
     /**
@@ -76,31 +83,39 @@ public class Storage {
             file.getParentFile().mkdirs();
             file.createNewFile();
         }
+        FileWriter fw = taskEncoder(taskList, file);
+        fw.close();
+    }
+
+    private static FileWriter taskEncoder(TaskList taskList, File file) throws IOException {
         FileWriter fw = new FileWriter(file);
         ArrayList<Task> tasks = taskList.getTasks();
         for (Task task : tasks) {
-            String output = null;
-            boolean isMarked = task.getTaskStatus();
-            int markedIndex;
-
-            if (isMarked) {
-                markedIndex = 1;
-            } else {
-                markedIndex = 0;
-            }
-
-            if (task instanceof Todo) {
-                output = "T | " + Integer.toString(markedIndex) + " | " + task.getDescription();
-            } else if (task instanceof Deadline) {
-                output = "D | " + Integer.toString(markedIndex) + " | " + task.getDescription()
-                        + " | " + ((Deadline) task).getBy();
-            } else if (task instanceof Event) {
-                output = "E | " + Integer.toString(markedIndex) + " | " + task.getDescription()
-                        + " | " + ((Event) task).getStart() + " | " + ((Event) task).getEnd();
-            }
+            int markedIndex = encodeTaskStatus(task.getTaskStatus());
+            String output = toString(task, markedIndex);
             fw.write(output);
             fw.write("\n");
         }
-        fw.close();
+        return fw;
+    }
+
+    private static int encodeTaskStatus(boolean isMarked) {
+        if (isMarked) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private static String toString(Task task, int markedIndex) {
+        if (task instanceof Todo) {
+            return "T | " + markedIndex + " | " + task.getDescription();
+        } else if (task instanceof Deadline) {
+            return "D | " + markedIndex + " | " + task.getDescription() + " | " + ((Deadline) task).getBy();
+        } else if (task instanceof Event) {
+            return "E | " + markedIndex + " | " + task.getDescription() + " | " + ((Event) task).getStart()
+                    + " | " + ((Event) task).getEnd();
+        }
+        return null;
     }
 }
