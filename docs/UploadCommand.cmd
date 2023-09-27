3255293c1e5745f9f4bd79dfcdf8390d63c2c275
MrOPPA1 <3105784412@qq.com>
2023-09-28 01:24:33 +0800

    Rename, Greet, Exit

--- docs/UploadCommand.cmd
+++ docs/UploadCommand.cmd
@@ -1,10 +1,10 @@
 set TAG_NAME=Level-0
-set MESSAGE=125
+set MESSAGE="Rename, Greet, Exit"
 
 git branch branch-%TAG_NAME%
 git switch branch-%TAG_NAME%
 git add .
-git commit -m "%MESSAGE%"
+git commit -m %MESSAGE%
 git push origin branch-%TAG_NAME%
 git tag %TAG_NAME% branch-%TAG_NAME%
 git push origin %TAG_NAME%
