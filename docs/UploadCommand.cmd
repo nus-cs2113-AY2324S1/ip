a4059f78b083d9953b10944b319dd689dfaa98b2
MrOPPA1 <3105784412@qq.com>
2023-09-28 01:33:23 +0800

    Rename, Greet, Exit

--- docs/UploadCommand.cmd
+++ docs/UploadCommand.cmd
@@ -10,6 +10,7 @@ git commit -m %MESSAGE%
 git push origin branch-%TAG_NAME%
 
 rem delete the tag if there exist
+git tag -d %TAG_NAME%
 git push origin --delete %TAG_NAME%
 git tag %TAG_NAME% branch-%TAG_NAME%
 git push origin %TAG_NAME%
