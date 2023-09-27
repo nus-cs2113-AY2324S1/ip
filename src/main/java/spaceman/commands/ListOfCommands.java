cda867fe830df5c23b7e30cc0e7bca8de303b747
ChungZhiXuan <zhixuanchung03@gmail.com>
2023-09-27 14:48:53 +0800

    Extract ListOfCommands class to contain a list of valid commands

---
+++ src/main/java/spaceman/commands/ListOfCommands.java
@@ -0,0 +1,16 @@
+package spaceman.commands;
+
+/**
+ * Container for valid commands executed by the user in the application.
+ */
+public class ListOfCommands {
+    public static final String COMMAND_BYE = "bye";
+    public static final String COMMAND_LIST = "list";
+    public static final String COMMAND_MARK = "mark";
+    public static final String COMMAND_UNMARK = "unmark";
+    public static final String COMMAND_TODO = "todo";
+    public static final String COMMAND_DEADLINE = "deadline";
+    public static final String COMMAND_EVENT = "event";
+    public static final String COMMAND_DELETE = "delete";
+    public static final String COMMAND_FIND = "find";
+}
