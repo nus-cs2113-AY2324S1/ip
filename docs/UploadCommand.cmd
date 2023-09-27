9d0d22d20984e354e1d13d45d82c858bed5776dc
MrOPPA1 <3105784412@qq.com>
2023-09-28 01:29:53 +0800

    Rename, Greet, Exit

--- docs/UploadCommand.cmd
+++ docs/UploadCommand.cmd
@@ -1,10 +1,15 @@
+rem customize the tag name and message here
 set TAG_NAME=Level-0
 set MESSAGE="Rename, Greet, Exit"
 
+rem create a branch
 git branch branch-%TAG_NAME%
 git switch branch-%TAG_NAME%
 git add .
 git commit -m %MESSAGE%
 git push origin branch-%TAG_NAME%
+
+rem delete the tag if there exist
+git push origin --delete %TAG_NAME%
 git tag %TAG_NAME% branch-%TAG_NAME%
 git push origin %TAG_NAME%
