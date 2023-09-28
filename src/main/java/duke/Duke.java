b960141c4c972237b94e6d1cf1ef5720982b0dfd
Stanley Wijaya <wijayastanley00@gmail.com>
2023-09-28 20:58:06 +0800

    Remove unused lines

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -7,73 +7,12 @@ import duke.tasklist.TaskList;
 import duke.ui.TextUi;
 import duke.parser.Parser;
 
-import java.util.Scanner;
-
 public class Duke {
 
     private TextUi ui;
     private Storage storage;
     private TaskList tasks;
     private Parser parser;
-
-//    public void executeCommand(String input) {
-//        String[] parsedInput = input.split(" ", 2);
-//        String command = parsedInput[0];
-//        int selectedIndex = 0;
-//        String dataString;
-//        input = parsedInput.length == 1 ? " " : parsedInput[1].trim();
-//
-//        try {
-//            switch (command) {
-//            case "list":
-//                ui.printTasks(tasks);
-//                break;
-//            case "mark":
-//                selectedIndex = tasks.setMarkAsDone(input);
-//                storage.updateTaskDatabase(selectedIndex, true);
-//                ui.printModifiedTask(tasks.getTask(selectedIndex), true);
-//                break;
-//            case "unmark":
-//                selectedIndex = tasks.setUnmarkAsDone(input);
-//                storage.updateTaskDatabase(selectedIndex, false);
-//                ui.printModifiedTask(tasks.getTask(selectedIndex), false);
-//                break;
-//            case "delete":
-//                selectedIndex = tasks.deleteTask(input);
-//                storage.deleteTaskData(selectedIndex);
-//                break;
-//            case "todo":
-//                Command c = parser.parseCommand(input);
-//                c.executeCommand(tasks, ui, storage);
-//                break;
-//            case "deadline":
-//                dataString = tasks.addDeadline(input);
-//                storage.addNewData(dataString, tasks.getTasksCount());
-//                ui.printRecentTask(tasks);
-//                break;
-//            case "event":
-//                dataString = tasks.addEvent(input);
-//                storage.addNewData(dataString, tasks.getTasksCount());
-//                ui.printRecentTask(tasks);
-//                break;
-//            case "bye":
-//                ui.tellGoodbye();
-//                System.exit(0);
-//            default:
-//                throw new DukeCommandException();
-//            }
-//        } catch (IndexOutOfBoundsException exception) {
-//            ui.handleIndexOutOfBoundsException(tasks.getTasksCount());
-//        } catch (NumberFormatException exception) {
-//            sdasd
-//        } catch (DukeCommandException exception) {
-//            exception.handleDukeCommandException(command);
-//        } catch (DukeTaskException exception) {
-//            exception.handleDukeTaskException(command, input);
-//        } catch (IOException exception) {
-//            ui.handleIOException(exception);
-//        }
-//    }
 
     private void setup() {
         ui = new TextUi();
