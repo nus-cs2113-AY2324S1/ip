ae3eb951b6c2563f646cd2c7a8b077203bd182cb
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-09-29 23:56:43 +0800

    Implemented Ui classes

---
+++ src/main/java/duke/ui/MessageConstants.java
@@ -0,0 +1,29 @@
+package duke.ui;
+
+/**
+ * Contains all the messages that Duke will display to the user.
+ */
+
+public class MessageConstants {
+    public static final String LINE = "____________________________________________________________";
+    public static final String MESSAGE_WELCOME = "Hello! I'm Duke\nWhat can I do for you?";
+    public static final String MESSAGE_GOODBYE = "Bye. Hope to see you again soon!";
+    
+    public static final String MESSAGE_LIST = "Here are the tasks in your list:";
+    public static final String MESSAGE_DONE = "Nice! I've marked this task as done:";
+    public static final String MESSAGE_DELETE = "Noted. I've removed this task:";
+    public static final String MESSAGE_ADD = "Got it. I've added this task:";
+    public static final String MESSAGE_FIND = "Here are the matching tasks in your list:";
+    
+    public static final String MESSAGE_ERROR = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+    public static final String MESSAGE_ERROR_EMPTY_DESCRIPTION = "☹ OOPS!!! The description of a %s cannot be empty.";
+    public static final String MESSAGE_ERROR_EMPTY_DEADLINE = "☹ OOPS!!! The deadline of a %s cannot be empty.";
+    public static final String MESSAGE_ERROR_EMPTY_EVENT = "☹ OOPS!!! The event time of a %s cannot be empty.";
+    public static final String MESSAGE_ERROR_INVALID_COMMAND = "☹ OOPS!!! I'm sorry, but the command is invalid.";
+    public static final String MESSAGE_ERROR_INVALID_TASK_NUMBER = "☹ OOPS!!! The task number is invalid.";
+    public static final String MESSAGE_ERROR_INVALID_TASK_TYPE = "☹ OOPS!!! The task type is invalid.";
+    public static final String MESSAGE_ERROR_INVALID_DEADLINE_FORMAT = "☹ OOPS!!! The deadline format is invalid.";
+    public static final String MESSAGE_ERROR_INVALID_EVENT_FORMAT = "☹ OOPS!!! The event format is invalid.";
+    public static final String MESSAGE_ERROR_INVALID_DATE_FORMAT = "☹ OOPS!!! The date format is invalid.";
+    public static final String MESSAGE_ERROR_INVALID_DATE = "☹ OOPS!!! The date is invalid.";
+}
---
+++ src/main/java/duke/ui/Ui.java
@@ -0,0 +1,56 @@
+package duke.ui;
+
+import static duke.ui.MessageConstants.LINE;
+import static duke.ui.MessageConstants.MESSAGE_WELCOME;
+import static duke.ui.MessageConstants.MESSAGE_GOODBYE;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Ui {
+    private final Scanner sc;
+    private final PrintStream printer;
+
+    public Ui() {
+        this.sc = new Scanner(System.in);
+        this.printer = System.out;
+    }
+
+    /**
+     * Prints the welcome message.
+     */
+
+    public void showWelcome() {
+        printer.println(LINE);
+        printer.println(MESSAGE_WELCOME);
+        printer.println(LINE);
+    }
+
+    /**
+     * Prints the goodbye message.
+     */
+
+    public void showGoodbye() {
+        printer.println(LINE);
+        printer.println(MESSAGE_GOODBYE);
+        printer.println(LINE);
+    }
+
+    /**
+     * Prints the given message.
+     */
+    public void showMessage(String message) {
+        printer.println(LINE);
+        printer.println(message);
+        printer.println(LINE);
+    }
+ 
+    /**
+     * Reads the next line of input.
+     */
+    public String readCommand() {
+        printer.print("Enter command: ");
+        return sc.nextLine();
+    }
+    
+}
