c6327290b64671020a402319f558a3fc1a6f2e31
Stanley Wijaya <wijayastanley00@gmail.com>
2023-10-01 00:05:17 +0800

    Add JavaDoc to Storage and TaskList class

--- src/main/java/duke/tasklist/TaskList.java
+++ src/main/java/duke/tasklist/TaskList.java
@@ -8,12 +8,14 @@ import duke.task.Task;
 import duke.task.Todo;
 import duke.ui.TextUi;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import static java.util.stream.Collectors.toList;
 
+/**
+ * Represents a list of tasks and contains all tasks available in the program.
+ */
 public class TaskList {
     private final String TODO_DATA_TEMPLATE = "T | false | " ;
     private final String DEADLINE_DATA_TEMPLATE = "D | false | ";
@@ -27,6 +29,11 @@ public class TaskList {
     private Parser parser;
     private int tasksCount;
 
+    /**
+     * Creates a task list with saved tasks.
+     *
+     * @param tasks The saved tasks from previous session.
+     */
     public TaskList(ArrayList<Task> tasks)  {
         ui = new TextUi();
         parser = new Parser();
@@ -39,31 +46,53 @@ public class TaskList {
         return tasksCount;
     }
 
+    /**
+     * Gets a task from the list with a specified index.
+     *
+     * @param index The specified index.
+     * @return The task with the specified index.
+     */
     public Task getTask(int index) {
         return tasks.get(index);
     }
 
+    /**
+     * Gets the most recent added task from the list.
+     *
+     * @return The recent task.
+     */
     public Task getRecentTask() {
         return tasks.get(tasksCount - 1);
     }
 
-    public int setMarkAsDone(String input) {
+    /**
+     * Marks a task with the specified index.
+     *
+     * @param input The selected index.
+     */
+    public void setMarkAsDone(String input) {
         int index = parser.parseIndex(input);
 
         tasks.get(index).markAsDone();
-
-        return index;
     }
 
-    public int setUnmarkAsDone(String input) {
+    /**
+     * Unmarks a task with the specified index.
+     *
+     * @param input The selected index.
+     */
+    public void setUnmarkAsDone(String input) {
         int index = parser.parseIndex(input);
 
         tasks.get(index).unmarkAsDone();
-
-        return index;
     }
 
-    public int deleteTask(String input) throws IOException {
+    /**
+     * Deletes a task with the given index in the list.
+     *
+     * @param input The selected index.
+     */
+    public void deleteTask(String input) {
         int index = parser.parseIndex(input);
         Task removedTask = tasks.remove(index);
 
@@ -72,15 +101,17 @@ public class TaskList {
         tasksCount--;
 
         ui.printNumOfTasks(tasksCount);
-        return index;
     }
 
-    public boolean checkEmptyTodoInput(String input) {
-        return input.trim().isEmpty();
-    }
-
+    /**
+     * Adds a todo into the task list.
+     *
+     * @param input The task input.
+     * @return A data string that will be stored in the database.
+     * @throws DukeTaskException If an input has an invalid format.
+     */
     public String addTodo(String input) throws DukeTaskException {
-        if (checkEmptyTodoInput(input)) {
+        if (input.trim().isEmpty()) {
             throw new DukeTaskException();
         }
 
@@ -90,6 +121,13 @@ public class TaskList {
         return TODO_DATA_TEMPLATE + input.trim();
     }
 
+    /**
+     * Adds an event into the task list.
+     *
+     * @param input The task input.
+     * @return A data string that will be stored in the database.
+     * @throws DukeTaskException If an input has an invalid format.
+     */
     public String addDeadline(String input) throws DukeTaskException {
         String[] parsedInput = parser.parseTask(input, BY_KEYWORD);
 
@@ -107,18 +145,37 @@ public class TaskList {
         return DEADLINE_DATA_TEMPLATE + description + " | "  + byBeforeParse;
     }
 
-    public boolean checkNumOfEventKeywords(String input) {
+    /**
+     * Checks the number of each event keywords in the input.
+     *
+     * @param input The task input.
+     * @return Confirmation if the number of keywords are correct.
+     */
+    private boolean checkNumOfEventKeywords(String input) {
         int numOfFromKeyword = input.split(FROM_KEYWORD).length - 1;
         int numOfToKeyword = input.split(TO_KEYWORD).length - 1;
 
         return numOfFromKeyword == 1 && numOfToKeyword == 1;
     }
 
-    public boolean checkPosOfEventKeywords(String input) {
+    /**
+     * Checks the input's event keywords are in order.
+     *
+     * @param input The task input.
+     * @return Confirmation if the input's keywords are in the right sequence.
+     */
+    private boolean checkPosOfEventKeywords(String input) {
         return input.indexOf(FROM_KEYWORD) < input.indexOf(TO_KEYWORD);
     }
 
-    public String addEvent(String input) throws DukeTaskException, IOException {
+    /**
+     * Adds an event into the task list.
+     *
+     * @param input The task input.
+     * @return A data string that will be stored in the database.
+     * @throws DukeTaskException If an input has an invalid format.
+     */
+    public String addEvent(String input) throws DukeTaskException {
         if (!checkNumOfEventKeywords(input) || !checkPosOfEventKeywords(input)) {
             throw new DukeTaskException();
         }
@@ -141,6 +198,12 @@ public class TaskList {
         return dataString;
     }
 
+    /**
+     * Filters out tasks that do not have their description matches with the keyword.
+     *
+     * @param keyword The selected keyword to match.
+     * @return The filtered list.
+     */
     public ArrayList<Task> filterTasks(String keyword) {
          ArrayList<Task> filteredList = (ArrayList<Task>) tasks.stream()
                  .filter((task) -> task.getDescription().contains(keyword))
