8a723aec03f683ae3995d162e2185bd9d35c9923
Faris Sirraj <faris@curium.sg>
2023-10-02 00:03:46 +0800

    Refactor to handle specefic task related operations within the subclass

--- src/main/java/duke/ToDo.java
+++ src/main/java/duke/ToDo.java
@@ -1,9 +1,19 @@
 package duke;
 
+import java.util.Arrays;
+
 public class ToDo extends Task{
 
-    public ToDo(String description) {
-        super(description);
+    public ToDo(String[] userInput, boolean read) {
+        super(getDescription(userInput, read));
         super.taskType = "todo";
+    }
+
+    private static String getDescription(String[] userInput, boolean read){
+        int startIndex = 1;
+        if (read){
+            startIndex = 2;
+        }
+        return String.join(" ", Arrays.copyOfRange(userInput, startIndex, userInput.length));
     }
 }
