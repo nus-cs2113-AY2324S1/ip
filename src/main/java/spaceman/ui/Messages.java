c98515f8ab4c8dc9f8d8d28dd067f3588483cf83
ChungZhiXuan <zhixuanchung03@gmail.com>
2023-10-02 01:31:27 +0800

    Remove emoji in messages

--- src/main/java/spaceman/ui/Messages.java
+++ src/main/java/spaceman/ui/Messages.java
@@ -13,18 +13,18 @@ public class Messages {
             + "/      | |   |   _   |   \\__|     |   __   |   _   |       |\n"
             + "|____ /|_|   |__| |__|\\_____|_____|__|  |__|__| |__|__|\\___|\n";
     public static final String MESSAGE_BYE = "Bye. Hope to see you again soon!";
-    public static final String MESSAGE_UNKNOWN = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+    public static final String MESSAGE_UNKNOWN = "OOPS!!! I'm sorry, but I don't know what that means :-(";
     public static final String MESSAGE_MARK = "Nice! I've marked this task as done:";
     public static final String MESSAGE_UNMARK = "OK, I've marked this task as not done yet:";
-    public static final String MESSAGE_EMPTY_TODO = "☹ OOPS!!! The description of a todo cannot be empty.";
-    public static final String MESSAGE_EMPTY_DEADLINE = "☹ OOPS!!! The description of a deadline cannot be empty.";
-    public static final String MESSAGE_EMPTY_BY = "☹ OOPS!!! The /by parameter of a deadline cannot be empty.";
-    public static final String MESSAGE_EMPTY_EVENT = "☹ OOPS!!! The description of an event cannot be empty.";
-    public static final String MESSAGE_EMPTY_FROM = "☹ OOPS!!! The /from parameter of an event cannot be empty.";
-    public static final String MESSAGE_EMPTY_TO = "☹ OOPS!!! The /to parameter of an event cannot be empty.";
-    public static final String MESSAGE_INVALID_DATE = "☹ OOPS!!! The format of the date is invalid.";
-    public static final String MESSAGE_INVALID_INDEX = "☹ OOPS!!! This is an invalid task index.";
+    public static final String MESSAGE_EMPTY_TODO = "OOPS!!! The description of a todo cannot be empty.";
+    public static final String MESSAGE_EMPTY_DEADLINE = "OOPS!!! The description of a deadline cannot be empty.";
+    public static final String MESSAGE_EMPTY_BY = "OOPS!!! The /by parameter of a deadline cannot be empty.";
+    public static final String MESSAGE_EMPTY_EVENT = "OOPS!!! The description of an event cannot be empty.";
+    public static final String MESSAGE_EMPTY_FROM = "OOPS!!! The /from parameter of an event cannot be empty.";
+    public static final String MESSAGE_EMPTY_TO = "OOPS!!! The /to parameter of an event cannot be empty.";
+    public static final String MESSAGE_INVALID_DATE = "OOPS!!! The format of the date is invalid.";
+    public static final String MESSAGE_INVALID_INDEX = "OOPS!!! This is an invalid task index.";
     public static final String MESSAGE_EMPTY_LIST = "Yay! You currently have no tasks!";
-    public static final String MESSAGE_EMPTY_KEYWORD = "☹ OOPS!!! The keyword of a find command cannot be empty.";
-    public static final String MESSAGE_EMPTY_INDEX = "☹ OOPS!!! The index of a mark/unmark command cannot be empty.";
+    public static final String MESSAGE_EMPTY_KEYWORD = "OOPS!!! The keyword of a find command cannot be empty.";
+    public static final String MESSAGE_EMPTY_INDEX = "OOPS!!! The index of a mark/unmark command cannot be empty.";
 }
