894b25a85687f70166f3d90ba811b69c2f39a20a
Faris Sirraj <faris@curium.sg>
2023-10-02 00:01:13 +0800

    New class to handle the inputs obtained from the user

---
+++ src/main/java/duke/Parser.java
@@ -0,0 +1,45 @@
+package duke;
+
+import java.io.IOException;
+
+
+public class Parser extends DukeException{
+
+    private final TaskList taskList;
+    private final FileManager writer;
+
+    public Parser(String[] userInput, TaskList taskList, FileManager writer) {
+        super(userInput);
+        this.taskList = taskList;
+        this.writer = writer;
+    }
+
+    public void parseInput() throws IOException {
+        super.checkInput(taskList.getSize());
+        if (!super.exception){
+            switch (userInput[0]) {
+            case "mark":
+                taskList.markItem(Integer.parseInt(userInput[1]) - 1, true);
+                break;
+            case "unmark":
+                taskList.markItem(Integer.parseInt(userInput[1]) - 1, false);
+                break;
+            case "bye":
+                writer.save(TaskList.taskList);
+                writer.closeFile();
+                new Ui().printBye();
+                System.exit(0);
+                break;
+            case "list":
+                taskList.listTask();
+                break;
+            case "delete":
+                taskList.deleteTask(Integer.parseInt(userInput[1]) - 1);
+                break;
+            default:
+                taskList.addTasks(userInput);
+                break;
+            }
+        }
+    }
+}
