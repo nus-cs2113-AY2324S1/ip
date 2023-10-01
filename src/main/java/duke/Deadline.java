9bd126e0abf115c90c8c7e8ff2ade8040d34e201
Faris Sirraj <faris@curium.sg>
2023-10-02 00:07:53 +0800

    Refactor code to handle all Deadline task related operations within the subclass

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -1,9 +1,31 @@
 package duke;
+
+import java.util.Arrays;
+
 public class Deadline extends Task {
 
-    public Deadline(String description, String by) {
-        super(description);
+
+    public Deadline(String[] userInput, boolean read) {
+        super(getDescription(userInput, read));
         super.taskType = "deadline";
-        super.deadline = by;
+        super.deadline = getDeadline(userInput, read);
+    }
+
+    private static String getDescription(String[] userInput, boolean read){
+        int byIndex = Arrays.asList(userInput).indexOf("/by");
+        int startIndex = 1;
+        if (read){
+            return userInput[2];
+        }
+
+        return String.join(" ", Arrays.copyOfRange(userInput, startIndex, byIndex)) + " ";
+    }
+
+    private static String getDeadline(String[] userInput, boolean read){
+        if (read) {
+            return userInput[3];
+        }
+        int byIndex = Arrays.asList(userInput).indexOf("/by");
+        return String.join(" ", Arrays.copyOfRange(userInput, byIndex + 1, userInput.length));
     }
 }
