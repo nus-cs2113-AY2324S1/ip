21745369bdfe1e08c387a038e516ca004a11c774
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-10-02 23:53:59 +0800

    Fixed formatting error

--- src/main/java/duke/commands/ListCommand.java
+++ src/main/java/duke/commands/ListCommand.java
@@ -3,12 +3,13 @@ package duke.commands;
 import duke.tasks.Tasklist;
 
 import duke.ui.Ui;
+import static duke.ui.MessageConstants.MESSAGE_LIST;
 
 public class ListCommand extends Command {
     
     @Override
     public void execute(Tasklist tasks) {
-        Ui.showMessage(tasks.tasksToString());
+        Ui.showMessage(MESSAGE_LIST + "\n" + tasks.tasksToString());
     }
     
 }
