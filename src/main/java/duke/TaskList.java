ff30817b32df3fe44720febb1c63c8e86d1cdd5a
Faris Sirraj <faris@curium.sg>
2023-10-02 13:25:59 +0800

    Javadocs added for the TaskList Class

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -2,22 +2,35 @@ package duke;
 
 import java.util.ArrayList;
 
+/**
+ * Class to handle operations regarding the TaskList as a whole without
+ * specific manipulations to the Task objects itself.
+ */
 public class TaskList {
 
     public static ArrayList<Task> taskList = new ArrayList<>();
     private final Ui ui = new Ui();
 
+    /**
+     * @param task Task object to add to the taskList
+     */
     public void add(Task task){
         taskList.add(task);
         addTaskCallback(task);
     }
 
+    /**
+     * @param taskCount Index of Task object to be deleted from the Task list.
+     */
     public void deleteTask(int taskCount){
         Task oldTask = TaskList.taskList.get(taskCount);
         taskList.remove(taskCount);
         deleteTaskCallback(oldTask);
     }
 
+    /**
+     * Method to list all the tasks in the list.
+     */
     public void listTask(){
         ui.printLine();
         if (!taskList.isEmpty()) {
@@ -29,6 +42,10 @@ public class TaskList {
         ui.printLine();
     }
 
+    /**
+     * @param userInput User input string to be assigned to the respective subclasses based on the
+     *                  task type command by the user.
+     */
     public void addTasks(String[] userInput){
         switch (userInput[0]) {
         case "todo":
@@ -47,6 +64,10 @@ public class TaskList {
         }
     }
 
+    /**
+     * @param taskCount Index of task object to mark/unmark.
+     * @param isMark boolean to diffentiate between marking and unmarking.
+     */
     public void markItem(int taskCount, boolean isMark) {
         ui.printLine();
         if (isMark) {
@@ -63,6 +84,9 @@ public class TaskList {
         ui.printLine();
     }
 
+    /**
+     * @param task Task object added to be reflected in the callback message.
+     */
     public void addTaskCallback(Task task){
         ui.printLine();
         ui.echo("    Got it. I've added this task:\n");
@@ -73,6 +97,9 @@ public class TaskList {
         ui.printLine();
     }
 
+    /**
+     * @param task Task object added to be reflected in the callback message.
+     */
     public void deleteTaskCallback(Task task){
         ui.printLine();
         ui.echo("    Noted. I've removed this task:\n");
@@ -83,6 +110,9 @@ public class TaskList {
         ui.printLine();
     }
 
+    /**
+     * @return Size of the taskList.
+     */
     public int getSize(){
         return taskList.size();
     }
