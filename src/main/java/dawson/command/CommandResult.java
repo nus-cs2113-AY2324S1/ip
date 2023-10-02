0dcf2b48974afb48be7279c5b65a07674fba4821
TongZhengHong <zhtong315@gmail.com>
2023-10-02 22:29:11 +0800

    Change CommandResult to use ... operator

--- src/main/java/dawson/command/CommandResult.java
+++ src/main/java/dawson/command/CommandResult.java
@@ -4,12 +4,7 @@ public class CommandResult {
     
     private final String[] message;
 
-    public CommandResult(String message) {
-        String[] newMessage = { message };
-        this.message = newMessage;
-    }
-
-    public CommandResult(String[] message) {
+    public CommandResult(String... message) {
         this.message = message;
     }
 
