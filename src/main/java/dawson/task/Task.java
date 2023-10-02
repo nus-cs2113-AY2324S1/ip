6ca798164f3872b87cdca1ad9d24ac7472cb10e8
TongZhengHong <zhtong315@gmail.com>
2023-10-03 00:46:32 +0800

    Add dateTime format in Task to parse dateTime info

--- src/main/java/dawson/task/Task.java
+++ src/main/java/dawson/task/Task.java
@@ -1,11 +1,19 @@
 package dawson.task;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+
 public abstract class Task {
 
     protected String description;
     protected boolean isDone;
 
     public abstract String encode();
+
+    protected final DateTimeFormatter showDateTimeFormat = DateTimeFormatter
+        .ofPattern("dd MMM yyyy, HH:mm");
 
     public Task(String description) {
         this.description = description;
@@ -35,6 +43,23 @@ public abstract class Task {
     @Override
     public String toString() {
         return String.format("[%s] %s", getStatusIcon(), description);
+    }
+
+    protected LocalDateTime parseDateTime(String dateTimeString, boolean isStart) {
+        DateTimeFormatter parseFormat = new DateTimeFormatterBuilder()
+            .appendPattern("d/M/yyyy")
+            .optionalStart()
+            .appendPattern(" HHmm")
+            .optionalEnd()
+            .parseDefaulting(ChronoField.HOUR_OF_DAY, isStart ? 0 : 23)
+            .parseDefaulting(ChronoField.MINUTE_OF_HOUR, isStart ? 0 : 59)
+            .toFormatter();
+        
+        try {
+            return LocalDateTime.parse(dateTimeString, parseFormat);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
 }
