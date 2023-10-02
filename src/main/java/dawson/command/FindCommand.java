ad86e419a7f7656d378ab5b2a38db12b33e3bcd9
TongZhengHong <zhtong315@gmail.com>
2023-10-02 22:42:01 +0800

    Add empty query check

--- src/main/java/dawson/command/FindCommand.java
+++ src/main/java/dawson/command/FindCommand.java
@@ -7,7 +7,9 @@ import dawson.task.TaskList;
 
 public class FindCommand extends Command {
 
-    private static final String MESSAGE_FIND_PROMPT = "Here are the matching tasks in your list";
+    private static final String MESSAGE_EMPTY_QUERY = "Empty query found! Please enter non-empty search query";
+    private static final String MESSAGE_CANNOT_FIND = "No task found with: %s";
+    private static final String MESSAGE_FIND_PROMPT = "Here are the tasks matching: %s";
 
     private String payload;
 
@@ -19,12 +21,18 @@ public class FindCommand extends Command {
     public CommandResult execute(TaskList list) throws DawsonException {
         ArrayList<String> result = list.findTasks(payload);
 
-        if (result.isEmpty()) {
-            return new CommandResult("No task found with: " + payload);
+        if (isQueryEmpty(payload)) {
+            return new CommandResult(MESSAGE_EMPTY_QUERY);
+        } else if (result.isEmpty()) {
+            return new CommandResult(String.format(MESSAGE_CANNOT_FIND, payload));
         }
 
-        result.add(0, MESSAGE_FIND_PROMPT);
-        return new CommandResult(result.toArray(new String[result.size()]));
+        result.add(0, String.format(MESSAGE_FIND_PROMPT, payload));
+        return new CommandResult(result.toArray(new String[0]));
+    }
+
+    private boolean isQueryEmpty(String query) {
+        return query.trim().length() == 0;
     }
     
 }
