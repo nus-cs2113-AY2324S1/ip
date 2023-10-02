f6c2b11e9552f3cd84b49bbce034e07059e7e6eb
Benedict Chan <benedictchan247@gmail.com>
2023-10-03 01:40:06 +0800

    Add JavaDoc for UI package

--- src/main/java/UI/Ui.java
+++ src/main/java/UI/Ui.java
@@ -2,11 +2,22 @@ package UI;
 
 import java.util.ArrayList;
 
+/**
+ * Provides user interface functionality for the Barbie-themed task manager.
+ */
 public class Ui {
+    /**
+     * Prints a horizontal line separator to the console.
+     */
     public static void printLine() {
         System.out.println("_____________________________________________________________");
     }
 
+    /**
+     * Prints an array of text messages to the console.
+     *
+     * @param texts An array of text messages to be printed.
+     */
     public static void printTexts(String[] texts) {
         printLine();
         for (String text : texts) {
@@ -15,6 +26,12 @@ public class Ui {
         printLine();
     }
 
+    /**
+     * Prints an array of task-related text messages to the console.
+     *
+     * @param startLine The initial line of text to be printed.
+     * @param taskTexts An ArrayList of task-related text messages to be printed.
+     */
     public static void printTexts(String startLine, ArrayList<String> taskTexts) {
         printLine();
         System.out.println("\t" + startLine);
@@ -24,6 +41,9 @@ public class Ui {
         printLine();
     }
 
+    /**
+     * Displays a greeting message with a Barbie-themed logo to console.
+     */
     public static void greetUser() {
         String hiLogo = " ____  __.___________ _______\n"
                 + "\t|    |/ _|\\_   _____/ \\      \\\n"
@@ -39,6 +59,9 @@ public class Ui {
         });
     }
 
+    /**
+     * Displays a farewell message with a Barbie-themed logo to the console.
+     */
     public static void byeUser() {
         String byeLogo = "  ___________________  _____ __________ ____  __.____    .___ _______    ________\n"
                 + "\t/   _____/\\______   \\/  _  \\\\______   \\    |/ _|    |   |   |\\      \\  /  _____/\n"
