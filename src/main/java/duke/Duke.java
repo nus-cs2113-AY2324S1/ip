f77382bad4c9de15a9ad848f59f7e470077f9fe5
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-10-02 23:37:42 +0800

    Optimized readability for main duke class

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,62 +1,38 @@
 package duke;
 
-import duke.ui.Ui;
-
-import duke.storage.FileRW;
-
-import duke.tasks.*;
-
-import java.io.IOException;
-
 import duke.commands.Command;
-
 import duke.exception.DukeException;
-
 import duke.parser.Parser;
-
+import duke.tasks.Tasklist;
+import duke.storage.FileRW;
+import duke.ui.Ui;
 
 public class Duke {
 
-   
-    
-
-    public static void main(String[] args) throws DukeException {
+    public static void main(String[] args) {
         try {
-
             Tasklist tasks = new Tasklist();
-
             FileRW.readFromFile(tasks);
-            
-    
+
             Ui.showWelcome();
-    
+
             Command nextCommand = new Command();
-    
+
             while (!nextCommand.isExit()) {
                 try {
-                    //Read in the user input
                     String input = Ui.readCommand();
-                    //Parse the user input
                     nextCommand = Parser.parse(input);
-                    //Execute the command
                     nextCommand.execute(tasks);
                 } catch (DukeException e) {
-                    //Handle DukeException
                     Ui.showMessage(e.getMessage());
-                } 
-            
-                
+                }
             }
-    
-            //Print out a goodbye message
+            
             Ui.showGoodbye();
-    
-            //Write the tasks to the file
+
             FileRW.writeToFile(tasks);
         } catch (DukeException e) {
-            //Handle DukeException
             Ui.showMessage(e.getMessage());
         }
     }
-
 }
