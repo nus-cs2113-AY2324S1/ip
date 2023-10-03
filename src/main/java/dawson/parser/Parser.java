019c982067813e6b28bfc8f19f49b6ed09160ac9
TongZhengHong <zhtong315@gmail.com>
2023-10-03 16:54:18 +0800

    Add DateCommand to find tasks with date

--- src/main/java/dawson/parser/Parser.java
+++ src/main/java/dawson/parser/Parser.java
@@ -1,8 +1,10 @@
 package dawson.parser;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
 
 import dawson.command.Command;
@@ -13,7 +15,7 @@ import dawson.command.ExitCommand;
 import dawson.command.InvalidCommand;
 import dawson.command.ListCommand;
 import dawson.command.MarkCommand;
-import dawson.command.TimeCommand;
+import dawson.command.DateCommand;
 import dawson.command.TodoCommand;
 import dawson.command.UnmarkCommand;
 import dawson.exception.DawsonException;
@@ -21,6 +23,12 @@ import dawson.task.*;
 import dawson.ui.Messages;
 
 public class Parser {
+
+    public static final DateTimeFormatter userDateFormat = 
+        DateTimeFormatter.ofPattern("dd MMM yyyy");
+
+    public static final DateTimeFormatter userDateTimeFormat = DateTimeFormatter
+        .ofPattern("dd MMM yyyy, HH:mm");
 
     public static final String COMMAND_DELIMITER = "\\s+";
     public static final String TASK_DELIMITER = "\\|";
@@ -39,8 +47,8 @@ public class Parser {
                 return new EventCommand(payload);
             case Command.LIST_COMMAND:
                 return new ListCommand();
-            case Command.TIME_COMMAND:
-                return new TimeCommand(payload);
+            case Command.DATE_COMMAND:
+                return new DateCommand(payload);
             case Command.DELETE_COMMAND:
                 return new DeleteCommand(payload);
             case Command.MARK_COMMAND:
@@ -86,6 +94,11 @@ public class Parser {
         } catch (Exception e) {
             return null;
         }
+    }
+
+    public static LocalDate parseDate(String dateString) throws DateTimeParseException {
+        DateTimeFormatter parseFormat = DateTimeFormatter.ofPattern("d/M/yyyy");
+        return LocalDate.parse(dateString, parseFormat);
     }
 
 }
