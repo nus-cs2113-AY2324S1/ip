4c595bc345cbaeae4261a913fc4c482539be5007
Wendelin Wemhoener <wendelin.wemhoener@gmail.com>
2023-10-05 04:35:48 +0800

    Add JavaDoc

--- src/main/java/careo/Storage.java
+++ src/main/java/careo/Storage.java
@@ -3,12 +3,24 @@ package careo;
 import java.util.ArrayList;
 
 public class Storage {
+    /** Where the persistence file (relative path) is stored */
     private String filePath;
 
+    /**
+     * Instantiates a Storage instance with access to a persistence file.
+     *
+     * @param filePath Where the persistence file (relative path) is stored.
+     */
     public Storage(String filePath) {
         this.filePath = filePath;
     }
 
+    /**
+     * Loads and returns stored tasks from the persistence file.
+     *
+     * @return The tasks stored in the persistence file.
+     * @throws StorageLoadException When filePath doesn't point to a file.
+     */
     public ArrayList<Task> load() throws StorageLoadException {
         ArrayList<Task> tasks = new ArrayList<>();
 
@@ -17,6 +29,11 @@ public class Storage {
         // return tasks;
     }
 
+    /**
+     * Saves tasks to a persitence file.
+     *
+     * @param tasks The TaskList containing the tasks to be saved.
+     */
     public void save(TaskList tasks) {
 
     }
