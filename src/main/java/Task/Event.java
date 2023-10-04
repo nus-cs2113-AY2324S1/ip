3c60761b5d1a2132c0285347b342c4e55c14bc6e
TriciaBK <e1032477@u.nus.edu>
2023-10-04 21:55:09 +0800

    edited formatting of toString

--- src/main/java/Task/Event.java
+++ src/main/java/Task/Event.java
@@ -12,6 +12,6 @@ public class Event extends Task{
 
     @Override
     public String toString(){
-        return "[E] " + super.toString() + "(from: " + startTime + " to: " + endTime + ")";
+        return "[E] " + super.toString() + " (from: " + startTime + " to: " + endTime + ")";
     }
 }
