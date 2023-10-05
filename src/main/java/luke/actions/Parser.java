49d2924c3b085d2987fa24233c762dd60a441370
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-10-05 18:56:49 +0800

    Fix Objects (Parser), to include instantiating child classes of Command

--- src/main/java/luke/actions/Parser.java
+++ src/main/java/luke/actions/Parser.java
@@ -14,50 +14,63 @@ public class Parser {
         String parameters = "";
 
         String[] words = fullCommand.split(" "); //to identify usage of features "mark" & "unmark"
-
+        Command c = null;
         try {
             ActionType action = ActionType.valueOf(words[0].toUpperCase());
             theAction = action;
+            //Command c = null;
 
             try {
                 switch (action) {
                     case LIST:
                         parameters = "";
+                        c = new ListCommand(theAction, parameters);
                         break;
 
                     case FIND:
                         parameters = fullCommand.substring(4);
+                        c = new FindCommand(theAction, parameters);
                         break;
 
-                    case MARK: case UNMARK: case DELETE:
+                    case MARK: case UNMARK:
                         parameters = words[1];
+                        c = new MarkCommand(theAction, parameters);
                         break;
 
                     case TODO:
                         parameters = fullCommand.substring(4);
+                        c = new AddCommand(theAction, parameters);
                         break;
 
                     case DEADLINE:
                         parameters = fullCommand.substring(8);
+                        c = new AddCommand(theAction, parameters);
                         break;
 
                     case EVENT:
                         parameters = fullCommand.substring(5);
+                        c = new AddCommand(theAction, parameters);
+                        break;
+
+                    case DELETE:
+                        parameters = words[1];
+                        c = new DeleteCommand(theAction, parameters);
                         break;
 
                     default:
+                        c = new AddCommand(theAction, "");
                         assert false: "This line should never be reached";
                         break;
                 }
             } catch (IndexOutOfBoundsException e) { //empty for MARK, UNMARK, TO DO description, DEADLINE description, EVENT description
                 System.out.println("\tOOPS!!! You have missing arguments for " + words[0] + ".");
             }
-            Command c = new Command(theAction, parameters);
-            return c;
+
+            //return c;
         } catch (IllegalArgumentException e) {
             System.out.println("\tOOPS!!! I'm sorry, but I don't know what that means :-(");
         }
-        return null;
+        return c;
 
     }
 }
