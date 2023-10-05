be0e4958d492f45f6fe65dffd9f2aea822bb2d36
MrOPPA1 <3105784412@qq.com>
2023-10-05 23:45:32 +0800

    Find

--- docs/UploadCommand.cmd
+++ docs/UploadCommand.cmd
@@ -1,6 +1,6 @@
 rem customize the tag name and message here
-set TAG_NAME=A-JavaDoc
-set MESSAGE="Add JavaDoc comments"
+set TAG_NAME=Level-9
+set MESSAGE="Find"
 
 rem create a branch
 git branch branch-%TAG_NAME%
