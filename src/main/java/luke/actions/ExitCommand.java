18a66435972120e4c088393dd6bb846fa3c60a83
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-10-05 16:58:42 +0800

    Add Objects (ExitCommand)

---
+++ src/main/java/luke/actions/ExitCommand.java
@@ -0,0 +1,20 @@
+package luke.actions;
+
+import luke.files.Storage;
+import luke.tasks.TaskList;
+import luke.user.Ui;
+
+public class ExitCommand extends Command {
+    //bye input
+    public ExitCommand(ActionType theAction, String parameters) {
+        super(theAction, parameters);
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        //ui has String echo, storage has ArrayList<Task> tasks, tasks has ArrayList<Task> mainTaskList;
+
+        storage.store(tasks);
+        ui.closeUi();
+    }
+}
