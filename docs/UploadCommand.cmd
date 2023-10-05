9fcfb94c3c306887b035e5a2080d4b34b6b5976c
MrOPPA1 <3105784412@qq.com>
2023-10-05 23:46:02 +0800

    Dates and Times

--- docs/UploadCommand.cmd
+++ docs/UploadCommand.cmd
@@ -1,6 +1,6 @@
 rem customize the tag name and message here
-set TAG_NAME=Level-9
-set MESSAGE="Find"
+set TAG_NAME=Level-8
+set MESSAGE="Dates and Times"
 
 rem create a branch
 git branch branch-%TAG_NAME%
