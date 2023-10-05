f1b66a6105e1053d5d51d783cf663a72457a2ec4
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-10-05 20:41:25 +0800

    Add Abstract Class (Command)

--- src/main/java/luke/actions/Command.java
+++ src/main/java/luke/actions/Command.java
@@ -8,40 +8,17 @@ import luke.user.Ui;
 public abstract class Command {
     protected ActionType theAction;
     protected String parameters; //no. or description
+    private boolean isExit;
+
     public Command(ActionType theAction, String parameters) {
         this.theAction = theAction;
         this.parameters = parameters;
     }
-    private boolean isExit;
-    public void execute(TaskList tasks, Ui ui, Storage storage) {
 
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
         //ui has String echo, storage has ArrayList<Task> tasks, tasks has ArrayList<Task> mainTaskList;
 
-        String[] words = echo.split(" "); //to identify usage of features "mark" & "unmark"
-
-        try {
-            ActionType action = ActionType.valueOf(words[0].toUpperCase());
-
-            try {
-                switch (action) {
-
-
-
-                    default:
-                        assert false: "This line should never be reached";
-                        break;
-
-
-                }
-            } catch (IndexOutOfBoundsException e) { //empty for MARK, UNMARK, TO DO description, DEADLINE description, EVENT description
-                System.out.println("\tOOPS!!! You have missing arguments for " + words[0] + ".");
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println("\tOOPS!!! I'm sorry, but I don't know what that means :-(");
-        }
-
-        echo = userInput.nextLine();
-
+        //do nothing? should not be executed
     }
 
     public boolean isExit() {
