f26d5a3ba93d52e114f65c76b0e4ac4273a5ef33
rohit <rohitdarnell@gmail.com>
2023-10-06 01:30:19 +0800

    JavaDoc Update 3

--- src/main/java/Tasks/Deadline.java
+++ src/main/java/Tasks/Deadline.java
@@ -2,9 +2,19 @@ package Tasks;
 
 import Tasks.Task;
 
+/**
+ * The Deadline class represents a task with a specific due date.
+ * It extends the Task class and inherits its properties and behavior.
+ */
 public class Deadline extends Task {
     private String due;
 
+    /**
+     * Constructs a Deadline object with the provided description and due date.
+     *
+     * @param description The description of the deadline.
+     * @param due         The due date of the deadline.
+     */
     public Deadline(String description, String due) {
         super(description);
         this.due = due;
@@ -13,6 +23,11 @@ public class Deadline extends Task {
     // appends "[D]" to the beginning of the string.
     // Then, it calls super.toString(), ie. it calls the toString() method of the superclass
     // then adds the due date
+    /**
+     * Returns a formatted string representation of the Deadline object.
+     *
+     * @return A string representing the Deadline.
+     */
     @Override
     public String toString() {
         return "[" + this.type + "]" + super.toString() + " (by:"+ this.due + ")";
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,27 +1,51 @@
 import Tasks.*;
 import java.util.ArrayList;
 
+/**
+ * The Ui class manages user interface interactions and messages.
+ */
 public class Ui {
+    /**
+     * Displays a welcome message to the user.
+     */
     public void showWelcomeMessage() {
         System.out.println("Hey! I'm Dukey, your virtual assistant!\nWhat can I do for you?\n");
     }
 
+    /**
+     * Displays an exit message to the user.
+     */
     public static void showExitMessage() {
         System.out.println("Bye. Hope to see you again soon!\n");
     }
 
+    /**
+     * Outputs a header for the task list.
+     */
     public static void outputHeader() {
         printLine();
         System.out.println("Here are the matching tasks in your list:\n");
     }
 
+    /**
+     * Prints a visual line separator.
+     */
     public static void printLine() {
         System.out.println("_____________________________________________________");
     }
 
+    /**
+     * Shows an error message when loading tasks from a file.
+     */
     public static void showLoadingError() {
         System.out.println("Error loading tasks from text file to Dukey");
     }
+
+    /**
+     * Displays the task list to the user.
+     *
+     * @param tasks The list of tasks to be displayed.
+     */
     public void showTaskList(ArrayList<Task> tasks) {
         printLine();
         System.out.println("Here are the tasks in your list:");
