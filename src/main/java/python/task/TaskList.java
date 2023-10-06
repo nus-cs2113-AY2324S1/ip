7bbab3b662d0feefef5e950a844d1687afa6a480
nihalzp <81457724+nihalzp@users.noreply.github.com>
2023-10-06 20:54:19 +0800

    Add TaskList class

---
+++ src/main/java/python/task/TaskList.java
@@ -0,0 +1,37 @@
+package python.task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    final static private List<Task> tasks = new ArrayList<>();
+
+    public static Task getTask(int taskNo) {
+        return tasks.get(taskNo);
+    }
+
+    public static List<Task> getTasks() {
+        return tasks;
+    }
+
+    public static int getNumberOfTasks() {
+        return tasks.size();
+    }
+
+    public static void markTask(int taskNo) {
+        tasks.get(taskNo).setDone(true);
+    }
+
+    public static void unmarkTask(int taskNo) {
+        tasks.get(taskNo).setDone(false);
+    }
+
+    public static void deleteTask(int taskNo) {
+        tasks.remove(taskNo);
+    }
+
+    public static void addTask(Task task) {
+        tasks.add(task);
+    }
+
+}
