7ee16942c8f97e17efd218792143aafc77ac2ba3
ZH <cazh1or2@gmail.com>
2023-10-06 21:39:09 +0800

    Update Code Quality

--- src/main/java/kenergeticbot/command/BooleanChecks.java
+++ src/main/java/kenergeticbot/command/BooleanChecks.java
@@ -1,16 +1,6 @@
 package kenergeticbot.command;
 
 public class BooleanChecks {
-    //Returns True if the text contains "mark", False if not
-    public static boolean checkTextForMark(String item) {
-        return item.length() > 4 && item.startsWith("mark");
-    }
-
-    //Returns True if the text contains "unmark", False if not
-    public static boolean checkTextForUnmark(String item) {
-        return item.length() > 6 && item.startsWith("unmark");
-    }
-
     //Returns True if the text contains "todo", False if not
     public static boolean checkTextForTodo(String item) {
         return item.startsWith("todo");
@@ -24,8 +14,5 @@ public class BooleanChecks {
     //Returns True if the text contains "event", False if not
     public static boolean checkTextForEvent(String item) {
         return item.startsWith("event");
-    }
-    public static boolean checkTextForDelete(String item) {
-        return item.startsWith("delete");
     }
 }
