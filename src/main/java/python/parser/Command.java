19bf31a199fc2156d4661a2e61bdced2385630a6
nihalzp <81457724+nihalzp@users.noreply.github.com>
2023-10-06 20:53:21 +0800

    Add Command class

---
+++ src/main/java/python/parser/Command.java
@@ -0,0 +1,16 @@
+package python.parser;
+
+public class Command {
+    public static final String COMMAND_BYE = "bye";
+    public static final String COMMAND_LIST = "list";
+    public static final String COMMAND_MARK = "mark";
+    public static final String COMMAND_UNMARK = "unmark";
+    public static final String COMMAND_DELETE = "delete";
+    public static final String COMMAND_TODO = "todo";
+    public static final String COMMAND_DEADLINE = "deadline";
+    public static final String COMMAND_EVENT = "event";
+
+    public static boolean isCommandBye(String command) {
+        return (command.equals(COMMAND_BYE));
+    }
+}
