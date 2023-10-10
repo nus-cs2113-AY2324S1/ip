f2429fa3f900255656231dc212108f9473633332
Bayasgalan Kherlen <e1248887@u.nus.edu>
2023-10-10 22:06:51 +0800

    Add More-OOP

---
+++ src/main/java/AMY/Parser.java
@@ -0,0 +1,118 @@
+package AMY;
+
+import AMY.Exceptions.EmptyDeadlineException;
+import AMY.Exceptions.EmptyDeleteException;
+import AMY.Exceptions.EmptyEventException;
+import AMY.Exceptions.EmptyInput;
+import AMY.Exceptions.EmptyMarkException;
+import AMY.Exceptions.EmptyToDoException;
+import AMY.Exceptions.EmptyUnmarkException;
+
+import AMY.command.Deadline;
+import AMY.command.Event;
+import AMY.command.Todo;
+
+import java.util.Scanner;
+
+public class Parser {
+
+    public static final String LINE = "____________________________________________________________";
+
+    public static void drawLine() {
+        System.out.println(LINE);
+    }
+
+    // Throw exceptions when exceptions are encountered to avoid an error
+    public static void manageException(String userInput) throws EmptyInput, EmptyToDoException,
+            EmptyMarkException, EmptyUnmarkException, EmptyDeadlineException, EmptyEventException,
+            EmptyDeleteException {
+
+        Scanner input = new Scanner(userInput);
+        String command;
+        if (!input.hasNext()) {
+            throw new EmptyInput();
+        } else {
+            command = input.next();
+        }
+        if (command.equals("todo") && !input.hasNext()) {
+            throw new EmptyToDoException();
+        }
+        if (command.equals("mark") && !input.hasNext()) {
+            throw new EmptyMarkException();
+        }
+        if (command.equals("unmark") && !input.hasNext()) {
+            throw new EmptyUnmarkException();
+        }
+        if (command.equals("deadline") && !input.hasNext()) {
+            throw new EmptyDeadlineException();
+        }
+        if (command.equals("event") && !input.hasNext()) {
+            throw new EmptyEventException();
+        }
+        if (command.equals("delete") && !input.hasNext()) {
+            throw new EmptyDeleteException();
+        }
+    }
+
+    // Manage input when input is given to follow order
+    public static void manageInput() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine(); // Read user input
+            System.out.println(LINE);
+            try {
+                manageException(userInput);
+                if (userInput.equals("bye")) {
+                    break; // Exit the loop if the user enters "bye"
+                } else if (userInput.equals("list")) {
+                    TaskList.listTasks(); // list tasks
+                } else if (userInput.startsWith("mark")) {
+                    // Extract the task index from the user input
+                    int taskIndex = Integer.parseInt(userInput.substring(5).trim());
+                    TaskList.markTaskAsDone(taskIndex);
+                } else if (userInput.startsWith("unmark")) {
+                    // Extract the task index from the user input
+                    int taskIndex = Integer.parseInt(userInput.substring(7).trim());
+                    TaskList.unmarkTask(taskIndex);
+                } else if (userInput.startsWith("todo")) {
+                    String description = userInput.substring(5).trim();
+                    Todo todo = new Todo(description);
+                    TaskList.addToList(todo);
+                } else if (userInput.startsWith("deadline")) {
+                    int inputIndex = userInput.indexOf(" /by ");
+                    String description = userInput.substring("deadline ".length(), inputIndex);
+                    String by = userInput.substring(inputIndex + " /by ".length());
+                    TaskList.addToList(new Deadline(description, by));
+                } else if (userInput.startsWith("event")) {
+                    String[] parts = userInput.substring(5).split("/from");
+                    String description = parts[0].trim();
+                    String[] dateTimeParts = parts[1].split("/to");
+                    String from = dateTimeParts[0].trim();
+                    String to = dateTimeParts[1].trim();
+                    Event event = new Event(description, from, to);
+                    TaskList.addToList(event);
+                } else if (userInput.startsWith("delete")) {
+                    int taskIndex = Integer.parseInt(userInput.substring(7).trim());
+                    TaskList.deleteTask(taskIndex);
+                } else {
+                    System.out.println("Invalid command. Please try again.");
+                }
+            } catch (EmptyInput exception) {
+                System.out.println("☹ OOPS!!! The description cannot be empty.");
+            } catch (EmptyToDoException exception) {
+                System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+            } catch (EmptyMarkException exception) {
+                System.out.println("☹ OOPS!!! The description of a mark cannot be empty.");
+            } catch (EmptyUnmarkException exception) {
+                System.out.println("☹ OOPS!!! The description of an unmark cannot be empty.");
+            } catch (EmptyDeadlineException exception) {
+                System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
+            } catch (EmptyEventException exception) {
+                System.out.println("☹ OOPS!!! The description of an event cannot be empty.");
+            } catch (EmptyDeleteException exception) {
+                System.out.println("☹ OOPS!!! The description of an delete cannot be empty.");
+            }
+            drawLine();
+        }
+    }
+}
