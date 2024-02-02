b0c925927157fe0963b54a8b2c8a0903e1dc0a44
Praneet-25 <e0959984@u.nus.edu>
2024-02-02 19:22:31 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,12 +8,17 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
 
+        String[] listOfItems = new String[100];
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Brennan!");
         System.out.println("What can I do for you?\n");
 
         //String that stores the input entered by the user
         String input;
+
+        //variable stores the number of tasks being added
+        int sizeOfAddedItems = 0;
 
         Scanner in = new Scanner(System.in);
         while (true) {
@@ -23,15 +28,24 @@ public class Duke {
                 break;
             }
 
-            else {
+            else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(input);
+                for (int i = 0; i < sizeOfAddedItems; i++) {
+                    System.out.println((i + 1) + ". " + listOfItems[i]);
+                }
                 System.out.println("____________________________________________________________");
             }
+            else {
+                listOfItems[sizeOfAddedItems] = input;
+
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
+
+                sizeOfAddedItems++;
+            }
+
         }
-
-
-
 
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
